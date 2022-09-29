01ec9aa5a05997babbdd58d5b9f727a174dbed29
MiraGeowerktstatt <mira.bleuler@geowerkstatt.ch>
2022-09-29 10:59:15 +0200

    Change connection string for context in tests

--- tests/ContextFactory.cs
+++ tests/ContextFactory.cs
@@ -3,10 +3,10 @@
 namespace BDMS;
 internal static class ContextFactory
 {
-    public static string ConnectionString { get; } = "Host=localhost;Username=SPAWNPLOW;Password=YELLOWSPATULA;Database=bdms";
+    public static string ConnectionString { get; } = "Host=db;Username=SPAWNPLOW;Password=YELLOWSPATULA;Database=bdms";
 
     /// <summary>
-    /// Creates an instance of <see cref="BdmsContext"/> with a database. The database is seeded with data.
+    /// Creates an instance of <see cref="BdmsContext"/>.
     /// </summary>
     /// <returns>The initialized <see cref="BdmsContext"/>.</returns>
     public static BdmsContext CreateContext()
