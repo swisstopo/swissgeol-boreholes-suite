888f1cd36e2b0c930d3671e1c9bfde586b0c43b6
Patrick Ackermann <patrick.ackermann@geowerkstatt.ch>
2022-10-10 08:30:17 +0200

    Add more bulk edit tests

--- src/client/cypress/e2e/editor/bulkedit.cy.js
+++ src/client/cypress/e2e/editor/bulkedit.cy.js
@@ -1,4 +1,54 @@
 describe("Test the borehole bulk edit feature.", () => {
+  const adminUserAuth = {
+    user: "admin",
+    password: "swissforages",
+  };
+
+  const createBorehole = values => {
+    return cy
+      .request({
+        method: "POST",
+        url: "/api/v1/borehole/edit",
+        body: {
+          action: "CREATE",
+          id: 1,
+        },
+        auth: adminUserAuth,
+      })
+      .then(res => {
+        expect(res.body).to.have.property("success", true);
+        let boreholeId = res.body.id;
+        let fields = Object.entries(values).map(([key, value]) => [key, value]);
+        if (fields.length > 0) {
+          cy.request({
+            method: "POST",
+            url: "/api/v1/borehole/edit",
+            body: {
+              action: "MULTIPATCH",
+              fields: fields,
+              ids: [boreholeId],
+            },
+            auth: adminUserAuth,
+          }).then(res => expect(res.body).to.have.property("success", true));
+        }
+        return cy.wrap(boreholeId);
+      });
+  };
+
+  const deleteBorehole = id => {
+    cy.request({
+      method: "POST",
+      url: "/api/v1/borehole/edit",
+      body: {
+        action: "DELETE",
+        id: id,
+      },
+      auth: adminUserAuth,
+    })
+      .its("body.success")
+      .should("eq", true);
+  };
+
   beforeEach(() => {
     cy.intercept("/api/v1/geoapi/canton").as("geoapi");
     cy.intercept("/api/v1/borehole").as("borehole");
@@ -12,9 +62,64 @@ describe("Test the borehole bulk edit feature.", () => {
     cy.wait("@geoapi");
   });
 
-  it("opens the bulk edit dialog", () => {
+  it("opens the bulk edit dialog with all boreholes selected", () => {
     cy.get('[data-cy="borehole-table"] thead .checkbox').click();
     cy.contains("button", "Bulk editing").click();
     cy.wait("@edit_ids");
+  });
+
+  it("checks if all toggle buttons do something", () => {
+    cy.get('[data-cy="borehole-table"] thead .checkbox').click();
+    cy.contains("button", "Bulk editing").click();
+
+    cy.get(".modal .toggle")
+      .should("have.length", 25)
+      .each((el, index, list) => {
+        cy.wrap(el).click();
+        cy.get("form .field").should("exist");
+        cy.wrap(el).click();
+        cy.get("form").children().should("not.exist");
+      });
+  });
+
+  it("fills all bulkedit fields and saves.", () => {
+    // create boreholes
+    createBorehole({ "extended.original_name": "NINTIC" }).as("borehole_id_1");
+    createBorehole({ "extended.original_name": "LOMONE" }).as("borehole_id_2");
+    cy.contains("a", "Refresh").click();
+    cy.wait("@borehole");
+
+    // select the boreholes for bulk edit
+    cy.get('[data-cy="borehole-table"]').within(() => {
+      cy.contains("NINTIC").parent().find(".checkbox").click();
+      cy.contains("LOMONE").parent().find(".checkbox").click();
+      cy.contains("button", "Bulk editing").click();
+    });
+
+    // select all bulk edit fields and insert values
+    cy.get(".modal .toggle").click({ multiple: true });
+    cy.get("form .field > .input > input")
+      .should("have.length", 8)
+      .each((el, index, list) => cy.wrap(el).type(`A${index}`));
+    cy.get("form .field > .react-datepicker-wrapper input")
+      .should("have.length", 3)
+      .each((el, index, list) => {
+        cy.wrap(el).click();
+        cy.get(`.react-datepicker__day--013`).click();
+      });
+    cy.get('form .field [role="combobox"]')
+      .should("have.length", 14)
+      .each((el, index, list) =>
+        cy.wrap(el).click().find('[role="option"]').last().click(),
+      );
+
+    // save
+    cy.contains("button", "Save").click();
+    cy.wait("@edit_multipatch").its("response.body.success").should("eq", true);
+    cy.wait("@edit_list");
+
+    // delete the boreholes
+    cy.get("@borehole_id_1").then(id => deleteBorehole(id));
+    cy.get("@borehole_id_2").then(id => deleteBorehole(id));
   });
 });
