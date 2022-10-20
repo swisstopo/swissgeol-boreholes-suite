754e791366f4aea681233ceb4ac44888a8f76d4f
danjov <daniel.jovanovic@geowerkstatt.ch>
2022-10-20 15:46:10 +0200

    Remove obsolete users from db

---
+++ src/api/Migrations/20221020133502_RemoveObsoleteUsers.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDMS.Migrations;
+
+public partial class RemoveObsoleteUsers : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(@"DELETE FROM bdms.users WHERE username IN ('BRS', 'editor', 'franzi', 'Nadine', 'Raphael', 'St. Gallen', 'test', 'test_VD', 'ZH_AWEL-Toth');");
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+    }
+}
