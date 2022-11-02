c6eb719d1c843b444e755b3db34f3fe6d63c3a2e
MiraGeowerktstatt <mira.bleuler@geowerkstatt.ch>
2022-11-02 10:36:30 +0100

    Improve wildcard

--- src/client/cypress/e2e/testHelpers.js
+++ src/client/cypress/e2e/testHelpers.js
@@ -9,7 +9,7 @@ const adminUserAuth = {
 export const interceptApiCalls = () => {
   cy.intercept("/api/v1/borehole").as("borehole");
   cy.intercept("/api/v1/borehole/profile/layer").as("layer");
-  cy.intercept("/api/v2/layer*").as("layer-v2");
+  cy.intercept("/api/v2/layer/**").as("layer-v2");
   cy.intercept("/api/v1/borehole/edit", req => {
     return (req.alias = `edit_${req.body.action.toLowerCase()}`);
   });
