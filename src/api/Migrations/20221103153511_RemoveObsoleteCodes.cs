d84e1d4b343ea300dbc4ba19fcf550d8b592b05a
MiraGeowerktstatt <mira.bleuler@geowerkstatt.ch>
2022-11-03 16:21:14 +0100

    Remove obsolete codelist entries

---
+++ src/api/Migrations/20221103153511_RemoveObsoleteCodes.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDMS.Migrations;
+
+public partial class RemoveObsoleteCodes : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(@"DELETE FROM bdms.codelist WHERE schema_cli = 'layer_form' OR schema_cli = 'mlpr117' ");
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+    }
+}
