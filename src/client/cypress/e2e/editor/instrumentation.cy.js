f0525e86587fe84e7570dec5795692ec35cb71fd
MiraGeowerktstatt <mira.bleuler@geowerkstatt.ch>
2022-11-07 17:14:26 +0100

    Revert "Enhance wait time and force all clicks"
    This reverts commit c0b0dc39671c532c44a95b9bb05fe68a7a2756ad.

--- src/client/cypress/e2e/editor/instrumentation.cy.js
+++ src/client/cypress/e2e/editor/instrumentation.cy.js
@@ -12,9 +12,9 @@ const delayedType = (selector, string) => {
 };
 
 const addInstrumentLayer = () => {
-  cy.get('[data-cy="add-instrumentation-button"]').click({ force: true });
+  cy.get('[data-cy="add-instrumentation-button"]').click();
   cy.wait("@layer");
-  cy.wait(6000);
+  cy.wait(5000);
 };
 
 describe("Instrumentation tests", () => {
@@ -24,8 +24,8 @@ describe("Instrumentation tests", () => {
     login("/editor");
 
     newUneditableBorehole().as("borehole_id");
-    cy.get('[data-cy="completion-menu-item"]').click({ force: true });
-    cy.get('[data-cy="instrument-menu-item"]').click({ force: true });
+    cy.get('[data-cy="completion-menu-item"]').click();
+    cy.get('[data-cy="instrument-menu-item"]').click();
   });
 
   afterEach(() => {
@@ -38,17 +38,17 @@ describe("Instrumentation tests", () => {
       "contain",
       "No instrumentation available",
     );
-    cy.contains("a", "Start editing").click({ force: true });
+    cy.contains("a", "Start editing").click();
     cy.wait("@edit_lock");
     cy.get('[data-cy="instrument-message"]').should(
       "contain",
       "For the recording of an instrument please click the plus symbol at the top left",
     );
-    cy.contains("a", "Stop editing").click({ force: true });
+    cy.contains("a", "Stop editing").click();
   });
 
   it("Can add and delete instrument without completion", () => {
-    cy.contains("a", "Start editing").click({ force: true });
+    cy.contains("a", "Start editing").click();
     cy.wait("@edit_lock");
 
     // Header should not contain tabs
@@ -64,12 +64,7 @@ describe("Instrumentation tests", () => {
       .children()
       .first()
       .each((el, index, list) =>
-        cy
-          .wrap(el)
-          .click({ force: true })
-          .find('[role="option"]')
-          .last()
-          .click({ force: true }),
+        cy.wrap(el).click().find('[role="option"]').last().click(),
       );
 
     // Header should contain one tab with name "No casing"
@@ -87,80 +82,76 @@ describe("Instrumentation tests", () => {
       .children()
       .first()
       .should("contain", "No casing")
-      .click({ force: true });
+      .click();
 
     // Only one instrument should be displayed
     cy.get('[data-cy="instrument-list"]').children().should("have.length", 1);
 
     // Show all instruments
-    cy.get('[data-cy="show-all-button"]').click({ force: true });
+    cy.get('[data-cy="show-all-button"]').click();
     cy.get('[data-cy="instrument-list"]').children().should("have.length", 2);
 
     // Delete instrument with "no completion" removes "No completion" tab.
-    cy.get('[data-cy="delete-instrument-button"]')
-      .first()
-      .click({ force: true });
+    cy.get('[data-cy="delete-instrument-button"]').first().click();
 
     // Header should not contain tabs
     cy.get('[data-cy="profile-header-list"]')
       .children()
       .should("have.length", 0);
 
-    cy.contains("a", "Stop editing").click({ force: true });
+    cy.contains("a", "Stop editing").click();
   });
 
   it("Can add casing layer to instrument", () => {
-    cy.contains("a", "Start editing").click({ force: true });
+    cy.contains("a", "Start editing").click();
     cy.wait("@edit_lock");
 
     // Add two casings with two layers
-    cy.get('[data-cy="casing-menu-item"]').click({ force: true });
+    cy.get('[data-cy="casing-menu-item"]').click();
 
     // First casing
-    cy.get('[data-cy="add-stratigraphy-button"]').click({ force: true });
+    cy.get('[data-cy="add-stratigraphy-button"]').click();
     cy.wait("@stratigraphy_edit_create");
     cy.wait("@layer");
     cy.contains("div", "This is the main completion");
 
     delayedType('[data-cy="name"]', "Moonshine Bike");
 
-    cy.get('[data-cy="add-layer-button"]').click({ force: true });
+    cy.get('[data-cy="add-layer-button"]').click();
     cy.wait("@layer");
-    cy.get('[data-cy="styled-layer-0"]').click({ force: true });
+    cy.get('[data-cy="styled-layer-0"]').click();
     delayedType('[data-cy="casing_id"]', "Moonshine Veal");
 
-    cy.get('[data-cy="add-layer-button"]').click({ force: true });
+    cy.get('[data-cy="add-layer-button"]').click();
     cy.wait("@layer");
-    cy.get('[data-cy="styled-layer-1"]').click({ force: true });
+    cy.get('[data-cy="styled-layer-1"]').click();
     delayedType('[data-cy="casing_id"]', "Moonshine Honey");
 
     // Second casing
-    cy.get('[data-cy="add-stratigraphy-button"]').click({ force: true });
+    cy.get('[data-cy="add-stratigraphy-button"]').click();
     cy.wait("@stratigraphy_edit_create");
     cy.wait("@layer");
 
     // Navigate to correct tab
-    cy.contains("div", "Unknown").click({ force: true });
+    cy.contains("div", "Unknown").click();
     delayedType('[data-cy="name"]', "Sunshine Bike");
 
-    cy.get('[data-cy="add-layer-button"]').click({ force: true });
+    cy.get('[data-cy="add-layer-button"]').click();
     cy.wait("@layer");
     cy.wait("@stratigraphy_layer_edit_create");
 
-    cy.get('[data-cy="styled-layer-0"]').click({ force: true });
+    cy.get('[data-cy="styled-layer-0"]').click();
     delayedType('[data-cy="casing_id"]', "Sunshine Veal");
 
-    cy.get('[data-cy="add-layer-button"]').click({ force: true });
+    cy.get('[data-cy="add-layer-button"]').click();
     cy.wait("@layer");
     cy.wait("@stratigraphy_layer_edit_create");
 
-    cy.get('[data-cy="styled-layer-1"]')
-      .scrollIntoView()
-      .click({ force: true });
+    cy.get('[data-cy="styled-layer-1"]').scrollIntoView().click();
     delayedType('[data-cy="casing_id"]', "Sunshine Honey");
 
     // Add instrument
-    cy.get('[data-cy="instrument-menu-item"]').click({ force: true });
+    cy.get('[data-cy="instrument-menu-item"]').click();
     cy.wait("@layer");
 
     addInstrumentLayer();
@@ -255,6 +246,6 @@ describe("Instrumentation tests", () => {
     cy.wait(3000);
 
     casingLayerDropDown.contains("Sunshine Honey");
-    cy.contains("a", "Stop editing").click({ force: true });
+    cy.contains("a", "Stop editing").click();
   });
 });
