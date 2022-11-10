d82edd3193b5ddac4b5a150294797b02d828fd33
Oliver Gut <oliver.gut@geowerkstatt.ch>
2022-11-10 07:02:42 +0100

    Add migration for new reference system
    - Added new columns in borehole table for LV03 coordinates.
    - Migrate existing coordinates to LV03 using shift method.
    - Preserve LV03 coordinates for some selected boreholes.

---
+++ src/api/Models/ReferenceSystem.cs
@@ -0,0 +1,17 @@
+﻿namespace BDMS.Models;
+
+/// <summary>
+/// Represents a reference system.
+/// </summary>
+public enum ReferenceSystem
+{
+    /// <summary>
+    /// CH1903+ LV 95 (EPSG:2056).
+    /// </summary>
+    LV95 = 20104001,
+
+    /// <summary>
+    /// CH1903 LV 03 (EPSG:21781).
+    /// </summary>
+    LV03 = 20104002,
+}
