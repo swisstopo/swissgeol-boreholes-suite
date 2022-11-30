8519fd4909d4145f228e35817a529b70e8b69751
MiraGeowerktstatt <mira.bleuler@geowerkstatt.ch>
2022-11-30 13:58:34 +0100

    Improve cy command chaining

--- src/client/cypress/e2e/editor/coordinates.cy.js
+++ src/client/cypress/e2e/editor/coordinates.cy.js
@@ -35,38 +35,44 @@ describe("Tests for editing coordinates of a borehole.", () => {
     delayedType(cy.get("@LV95Y-input"), "1245794.92348");
 
     // wait edits of all 4 inputs to complete
-    cy.wait("@location");
-    cy.wait("@edit_patch");
-    cy.wait("@edit_patch");
-    cy.wait("@edit_patch");
-    cy.wait("@edit_patch");
+    cy.wait([
+      "@location",
+      "@edit_patch",
+      "@edit_patch",
+      "@edit_patch",
+      "@edit_patch",
+    ]);
     // verify automatically filled inputs for LV03
-    cy.get("@LV95X-input").should("have.value", 2645123.12124);
     cy.get("@LV95Y-input").should("have.value", 1245794.92348);
-    cy.get("@LV03X-input").should("have.value", 645122.39962);
     cy.get("@LV03Y-input").should("have.value", 245794.77398);
+    cy.get("@LV03X-input").should("have.value", 645122.39962);
 
-    // clear and fill again with less decimals.
-    cy.get("@LV95X-input").clear().type("2645123.12", { delay: 10 });
+    // assert input, then clear and fill again with less decimals.
+    cy.get("@LV95X-input")
+      .should("have.value", 2645123.12124)
+      .clear()
+      .type("2645123.12", { delay: 10 })
+      .then(() => {
+        // wait edits of all 4 inputs to complete
+        cy.wait([
+          "@location",
+          "@edit_patch",
+          "@edit_patch",
+          "@edit_patch",
+          "@edit_patch",
+        ]);
+        // automatically filled LV03
+        cy.get("@LV03X-input").should("have.value", 645122.4);
+        cy.get("@LV03Y-input").should("have.value", 245794.77);
 
-    // wait edits of all 4 inputs to complete
-    cy.wait("@location");
-    cy.wait("@edit_patch");
-    cy.wait("@edit_patch");
-    cy.wait("@edit_patch");
-    cy.wait("@edit_patch");
-
-    // automatically filled LV03
-    cy.get("@LV03X-input").should("have.value", 645122.4);
-    cy.get("@LV03Y-input").should("have.value", 245794.77);
-
-    //switch reference system
-    cy.get("input[value=20104002]").click();
-    // verify all inputs are empty
-    cy.get("@LV95X-input").should("be.empty");
-    cy.get("@LV95Y-input").should("be.empty");
-    cy.get("@LV03X-input").should("be.empty");
-    cy.get("@LV03Y-input").should("be.empty");
+        //switch reference system
+        cy.get("input[value=20104002]").click();
+        // verify all inputs are empty
+        cy.get("@LV95X-input").should("be.empty");
+        cy.get("@LV95Y-input").should("be.empty");
+        cy.get("@LV03X-input").should("be.empty");
+        cy.get("@LV03Y-input").should("be.empty");
+      });
   });
 
   it("validates inputs", () => {
@@ -89,11 +95,13 @@ describe("Tests for editing coordinates of a borehole.", () => {
     cy.get("[name=location_y]").should("not.have.class", "error");
 
     // wait edits of all 4 inputs to complete
-    cy.wait("@location");
-    cy.wait("@edit_patch");
-    cy.wait("@edit_patch");
-    cy.wait("@edit_patch");
-    cy.wait("@edit_patch");
+    cy.wait([
+      "@location",
+      "@edit_patch",
+      "@edit_patch",
+      "@edit_patch",
+      "@edit_patch",
+    ]);
 
     // type coordinates that are out of bounds
     cy.get("@LV95X-input").clear();
