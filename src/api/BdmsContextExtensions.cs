ab0254ce50943f827610cb8c1c269ccb93d761c7
Philipp Lüthi <philipp.luethi@geowerkstatt.ch>
2023-01-09 13:32:13 +0100

    Fix codelist for restriction causing UI errors

--- src/api/BdmsContextExtensions.cs
+++ src/api/BdmsContextExtensions.cs
@@ -48,7 +48,7 @@ public static class BdmsContextExtensions
         List<int> kindIds = codelists.Where(c => c.Schema == "kind").Select(s => s.Id).ToList();
         List<int> srsIds = codelists.Where(c => c.Schema == "srs").Select(s => s.Id).ToList();
         List<int> hrsIds = codelists.Where(c => c.Schema == "hrs").Select(s => s.Id).ToList();
-        List<int> restrictionIds = codelists.Where(c => c.Schema == "qt_location").Select(s => s.Id).ToList();
+        List<int> restrictionIds = codelists.Where(c => c.Schema == "restriction").Select(s => s.Id).ToList();
         List<int> qtLocationnIds = codelists.Where(c => c.Schema == "qt_elevation").Select(s => s.Id).ToList();
         List<int> qtDescriptionIds = codelists.Where(c => c.Schema == "qt_description").Select(s => s.Id).ToList();
         List<int> drillingMethodIds = codelists.Where(c => c.Schema == "extended.drilling_method").Select(s => s.Id).ToList();
