7082c1575f5afd407c7d6fd1db6fd1509c0c9ffc
MiraGeowerktstatt <mira.bleuler@geowerkstatt.ch>
2023-01-18 14:52:10 +0100

    Sync db snapshot

---
+++ src/api/Migrations/20230118133836_SyncExistingTable.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDMS.Migrations;
+
+public partial class SyncExistingTable : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+    }
+}
