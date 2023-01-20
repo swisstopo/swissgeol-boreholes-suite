swisstopo/swissgeol-boreholes-suite

commit f49cca6f0e06de0f7eeb4a6e1be878bed4d5cc0e

Dispose object in LocationController

src/api/Controllers/LocationController.cs
@@ -33,14 +33,14 @@ public class LocationController : Controller
 
         try
         {
-            var httpResponse = await httpClient.GetAsync(requestUri).ConfigureAwait(false);
+            using var httpResponse = await httpClient.GetAsync(requestUri).ConfigureAwait(false);
             httpResponse.EnsureSuccessStatusCode();
 
-            var contentStream = await httpResponse.Content.ReadAsStreamAsync().ConfigureAwait(false);
-            var document = await JsonDocument.ParseAsync(contentStream).ConfigureAwait(false);
+            using var contentStream = await httpResponse.Content.ReadAsStreamAsync().ConfigureAwait(false);
+            using var document = await JsonDocument.ParseAsync(contentStream).ConfigureAwait(false);
 
             var result = new LocationInfo(
-                Country: GetAttributeValueForLayer(document, countryLayer, "bez"),
+                Country: GetAttributeValueForLayer(document, countryLayer, "bbez"),
                 Canton: GetAttributeValueForLayer(document, cantonLayer, "name"),
                 Municipality: GetAttributeValueForLayer(document, municipalityLayer, "gemname"));
 

commit d0620598eb00ddf250b93be330619fb7c0611eec

Fix typo introduced in f49cca6

src/api/Controllers/LocationController.cs
@@ -40,7 +40,7 @@ public class LocationController : Controller
             using var document = await JsonDocument.ParseAsync(contentStream).ConfigureAwait(false);
 
             var result = new LocationInfo(
-                Country: GetAttributeValueForLayer(document, countryLayer, "bbez"),
+                Country: GetAttributeValueForLayer(document, countryLayer, "bez"),
                 Canton: GetAttributeValueForLayer(document, cantonLayer, "name"),
                 Municipality: GetAttributeValueForLayer(document, municipalityLayer, "gemname"));
 