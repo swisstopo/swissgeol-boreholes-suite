62fcc5fbb37fad3a7dd160b6cb459d5fe416426b
MiraGeowerktstatt <mira.bleuler@geowerkstatt.ch>
2023-01-30 10:24:35 +0100

    Refactor profile components in viewer mode

--- src/client/src/commons/detail/stratigrafy/profile/profileContainer.js
+++ src/client/src/commons/detail/stratigrafy/profile/profileContainer.js
@@ -1,94 +1,35 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { loadLayers } from "../../../../api-lib/index";
+import React, { useState } from "react";
 import ProfileView from "./view/profileViewComponent";
-import { fetchLayerById } from "../../../../api/fetchApiV2";
+import { fetchLayerById, useLayers } from "../../../../api/fetchApiV2";
 
-class ProfileContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      layer: null,
-      isFetchingLayer: false,
-    };
-  }
-  componentDidMount() {
-    const { id } = this.props;
-    if (id !== null) {
-      this.props.loadLayers(id);
-    }
-  }
-  componentDidUpdate(prevProps) {
-    if (this.props.id !== prevProps.id) {
-      this.setState(
-        {
-          layer: null,
-        },
-        () => {
-          this.props.loadLayers(this.props.id);
-        },
-      );
-    }
-  }
-  render() {
-    const { layers, stratigraphy } = this.props;
-    if (layers.data.length === 0) {
-      return null;
-    }
-    return (
-      <ProfileView
-        data={layers.data}
-        handleSelected={selected => {
-          this.setState(
-            {
-              isFetchingLayer: true,
-              layer: null,
-            },
-            () => {
-              if (selected !== null) {
-                fetchLayerById(selected.id).then(result => {
-                  this.setState({
-                    isFetchingLayer: false,
-                    layer: result,
-                  });
-                });
-              }
-            },
-          );
-        }}
-        isFetchingLayer={this.state.isFetchingLayer}
-        kind={stratigraphy.kind}
-        kinds={stratigraphy.kinds}
-        layer={this.state.layer}
-      />
-    );
-  }
-}
+const ProfileContainer = props => {
+  const [selectedLayer, setSelectedLayer] = useState(null);
+  const [isFetching, setIsFetching] = useState(null);
+  const { id, stratigraphy } = props;
 
-ProfileContainer.propTypes = {
-  id: PropTypes.number,
-  layers: PropTypes.shape({
-    data: PropTypes.array,
-  }),
-  loadLayers: PropTypes.func,
-  stratigraphy: PropTypes.object,
-};
+  const layers = useLayers(id);
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-    layers: state.core_layers_list,
-    ...ownProps,
+  const selectLayer = async selected => {
+    setIsFetching(true);
+    setSelectedLayer(null);
+    const newSelectedLayer = await fetchLayerById(selected.id);
+    setIsFetching(false);
+    setSelectedLayer(newSelectedLayer);
   };
+
+  if (layers.isLoading || layers?.data?.length === 0) {
+    return null;
+  }
+  return (
+    <ProfileView
+      data={layers.data}
+      handleSelected={selectLayer}
+      isFetchingLayer={isFetching}
+      kind={stratigraphy.kind}
+      kinds={stratigraphy.kinds}
+      layer={selectedLayer}
+    />
+  );
 };
 
-const mapDispatchToProps = dispatch => {
-  return {
-    dispatch: dispatch,
-    loadLayers: id => {
-      dispatch(loadLayers(id));
-    },
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(ProfileContainer);
+export default ProfileContainer;
