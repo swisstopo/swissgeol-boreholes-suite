a7ee2d5c55e2268d558c05b61d7014edabb481fe
danjov <daniel.jovanovic@geowerkstatt.ch>
2023-02-15 17:03:42 +0100

    Merge similar tests in app.cy.js

--- src/client/cypress/e2e/app.cy.js
+++ src/client/cypress/e2e/app.cy.js
@@ -1,28 +1,23 @@
 import { loginAsAdmin, loginAsEditorInViewerMode } from "./testHelpers";
 
 describe("General app tests", () => {
-  it("Displays the login page in english by default", () => {
+  it("Displays the login page in the correct language", () => {
+    // default is english
     cy.visit("/");
     cy.contains("Sign in");
     cy.contains("Welcome to");
-  });
 
-  it("Displays the login page in german", () => {
-    cy.visit("/");
+    // german
     cy.contains("span", "DE").click();
     cy.contains("Sign in");
     cy.contains("Willkommen bei");
-  });
 
-  it("Displays the login page in french", () => {
-    cy.visit("/");
+    // french
     cy.contains("span", "FR").click();
     cy.contains("Sign in");
     cy.contains("Bienvenue sur");
-  });
 
-  it("Displays the login page in italian", () => {
-    cy.visit("/");
+    // italian
     cy.contains("span", "IT").click();
     cy.contains("Sign in");
     cy.contains("Benvenuti su");
