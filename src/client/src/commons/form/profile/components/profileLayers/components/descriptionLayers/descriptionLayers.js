aa530c1febb79c8da2af66c27794ff785ef9134d
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2023-02-22 17:45:57 +0100

    Display shrunken layers for gaps in lithology

--- src/client/src/commons/form/profile/components/profileLayers/components/descriptionLayers/descriptionLayers.js
+++ src/client/src/commons/form/profile/components/profileLayers/components/descriptionLayers/descriptionLayers.js
@@ -35,6 +35,8 @@ const DescriptionLayers = props => {
   const [displayDescriptions, setDisplayDescriptions] = useState(null);
   const [descriptionIdSelectedForDelete, setDescriptionIdSelectedForDelete] =
     useState(0);
+  const [selectableDepths, setSelectableDepth] = useState([]);
+  const [gaps, setGaps] = useState([]);
 
   const { t } = useTranslation();
   const theme = useTheme();
@@ -63,9 +65,6 @@ const DescriptionLayers = props => {
     // update the previous length
     prevLengthRef.current = displayDescriptions?.length;
   }, [displayDescriptions, lastDescriptionRef]);
-
-  const selectableFromDepths = layers?.data?.map(l => l.depth_from);
-  const selectableToDepths = layers?.data?.map(l => l.depth_to);
 
   useEffect(() => {
     // include empty items in description column to signal missing descriptions
@@ -160,23 +159,68 @@ const DescriptionLayers = props => {
     // eslint-disable-next-line
   }, [description, qtDescriptionId, toDepth, fromDepth, selectedDescription]);
 
-  const calculateLayerWidth = (fromDepth, toDepth) => {
+  // calculate selectable depths and gaps
+  useEffect(() => {
+    const selectableFromDepths = layers?.data?.map(l => l.depth_from);
+    const selectableToDepths = layers?.data?.map(l => l.depth_to);
     if (selectableFromDepths && selectableToDepths) {
-      const completeDepths = selectableToDepths
+      const selectableDepths = selectableToDepths
         .concat(
           selectableFromDepths.filter(
             item => selectableToDepths.indexOf(item) < 0,
           ),
         )
         .sort((a, b) => a - b);
-      const layerDistance =
-        (completeDepths.indexOf(toDepth) - completeDepths.indexOf(fromDepth)) *
-        10;
-      if (layerDistance < 10) return "10em";
-      return layerDistance + "em";
-    } else {
-      return "10em";
+
+      const missingFromDepths = displayDescriptions
+        ?.map(d => d.fromDepth)
+        .filter(d => !selectableFromDepths.includes(d));
+      const missingToDepths = displayDescriptions
+        ?.map(d => d.toDepth)
+        .filter(d => !selectableToDepths.includes(d));
+
+      const missingDepths = missingFromDepths?.filter(d =>
+        missingToDepths.includes(d),
+      );
+
+      const gaps = [];
+      for (let i = 0; i < selectableDepths.length; i++) {
+        const gap = missingDepths?.filter(
+          d => d > selectableDepths[i] && d < selectableDepths[i + 1],
+        );
+        gaps.push(gap);
+      }
+
+      setGaps(gaps);
+      setSelectableDepth(selectableDepths);
     }
+  }, [layers?.data, displayDescriptions]);
+
+  const calculateLayerHeight = (fromDepth, toDepth) => {
+    // case height must be reduced because of gaps in lithology.
+    if (
+      !selectableDepths.includes(fromDepth) ||
+      (!selectableDepths.includes(toDepth) && gaps?.length)
+    ) {
+      const gapLength = gaps.find(
+        g => g?.includes(fromDepth) || g?.includes(toDepth),
+      )?.length;
+
+      if (gapLength) {
+        return 10 / (gapLength + 1);
+      } else {
+        return 10;
+      }
+    }
+    // case height must be enhanced because descriptions stretching several lithology layers.
+
+    const layerDistance =
+      (selectableDepths.indexOf(toDepth) -
+        selectableDepths.indexOf(fromDepth)) *
+      10;
+
+    if (layerDistance < 10) return 10;
+    return layerDistance;
   };
 
   return (
@@ -184,80 +228,84 @@ const DescriptionLayers = props => {
       {displayDescriptions &&
         displayDescriptions
           ?.sort((a, b) => a.fromDepth - b.fromDepth)
-          .map((item, index) => (
-            <Stack
-              direction="row"
-              data-cy={`description-${index}`}
-              sx={{
-                boxShadow: "inset -1px 0 0 lightgrey, inset 0 -1px 0 lightgrey",
-                flex: "1 1 100%",
-                height:
-                  selectedDescription?.id === item.id
-                    ? "16em"
-                    : calculateLayerWidth(item?.fromDepth, item?.toDepth),
-                overflowY: "auto",
-                padding: "5px",
-                backgroundColor:
-                  item.id === null || item.id === descriptionIdSelectedForDelete
-                    ? theme.palette.error.background
-                    : selectedDescription?.id === item?.id && "lightgrey",
-                "&:hover": {
-                  backgroundColor: "#ebebeb",
-                },
-              }}
-              key={index}
-              ref={descriptionRefs[index]}
-              onClick={() => {
-                if (isEditable && selectedDescription?.id !== item?.id)
-                  selectItem(item);
-              }}
-              isFirst={index === 0 ? true : false}>
-              {descriptionIdSelectedForDelete !== item.id && (
-                <>
-                  {selectedDescription?.id !== item?.id && (
-                    <DescriptionDisplay item={item} />
-                  )}
-                  {selectedDescription?.id === item?.id && (
-                    <DescriptionInput
-                      setFromDepth={setFromDepth}
-                      setDescription={setDescription}
-                      setToDepth={setToDepth}
-                      setQtDescriptionId={setQtDescriptionId}
-                      selectableDepths={selectableFromDepths.concat(
-                        selectableToDepths.filter(
-                          d => !selectableFromDepths.includes(d),
-                        ),
-                      )}
-                      lithologicalDescriptions={descriptions}
-                      submitUpdate={submitUpdate}
-                      item={item}
-                    />
-                  )}
-                  {isEditable && (
-                    <ActionButtons
-                      item={item}
-                      selectItem={selectItem}
-                      setDescriptionIdSelectedForDelete={
-                        setDescriptionIdSelectedForDelete
-                      }
-                      addMutation={addMutation}
-                      selectedDescription={selectedDescription}
-                      selectedStratigraphyID={selectedStratigraphyID}
-                    />
-                  )}
-                </>
-              )}
-              {descriptionIdSelectedForDelete === item.id && (
-                <DescriptionDeleteDialog
-                  item={item}
-                  setDescriptionIdSelectedForDelete={
-                    setDescriptionIdSelectedForDelete
-                  }
-                  deleteMutation={deleteMutation}
-                />
-              )}
-            </Stack>
-          ))}
+          .map((item, index) => {
+            const calculatedHeight = calculateLayerHeight(
+              item?.fromDepth,
+              item?.toDepth,
+            );
+            const isItemSelected = selectedDescription?.id === item?.id;
+            const isItemToDelete = item.id === descriptionIdSelectedForDelete;
+            return (
+              <Stack
+                direction="row"
+                data-cy={`description-${index}`}
+                sx={{
+                  boxShadow:
+                    "inset -1px 0 0 lightgrey, inset 0 -1px 0 lightgrey",
+                  flex: "1 1 100%",
+                  height: isItemSelected ? "16em" : calculatedHeight + "em",
+                  overflowY: "auto",
+                  padding: "5px",
+                  backgroundColor:
+                    item.id === null || isItemToDelete
+                      ? theme.palette.error.background
+                      : isItemSelected && "lightgrey",
+                  "&:hover": {
+                    backgroundColor: "#ebebeb",
+                  },
+                }}
+                key={index}
+                ref={descriptionRefs[index]}
+                onClick={() => {
+                  if (isEditable && !isItemSelected) selectItem(item);
+                }}
+                isFirst={index === 0 ? true : false}>
+                {!isItemToDelete && (
+                  <>
+                    {!isItemSelected && (
+                      <DescriptionDisplay
+                        item={item}
+                        layerHeight={calculatedHeight}
+                      />
+                    )}
+                    {isItemSelected && (
+                      <DescriptionInput
+                        setFromDepth={setFromDepth}
+                        setDescription={setDescription}
+                        setToDepth={setToDepth}
+                        setQtDescriptionId={setQtDescriptionId}
+                        selectableDepths={selectableDepths}
+                        lithologicalDescriptions={descriptions}
+                        submitUpdate={submitUpdate}
+                        item={item}
+                      />
+                    )}
+                    {isEditable && (
+                      <ActionButtons
+                        item={item}
+                        selectItem={selectItem}
+                        setDescriptionIdSelectedForDelete={
+                          setDescriptionIdSelectedForDelete
+                        }
+                        addMutation={addMutation}
+                        selectedDescription={selectedDescription}
+                        selectedStratigraphyID={selectedStratigraphyID}
+                      />
+                    )}
+                  </>
+                )}
+                {isItemToDelete && (
+                  <DescriptionDeleteDialog
+                    item={item}
+                    setDescriptionIdSelectedForDelete={
+                      setDescriptionIdSelectedForDelete
+                    }
+                    deleteMutation={deleteMutation}
+                  />
+                )}
+              </Stack>
+            );
+          })}
     </Box>
   );
 };
