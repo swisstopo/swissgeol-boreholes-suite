6ef87fa69d384410e11fd2bca6015fadf40669a8
danjov <daniel.jovanovic@geowerkstatt.ch>
2023-02-22 12:18:39 +0100

    Use react-query for fetching instruments
    Quick fix as too many (redundant) API calls are made in this component.

--- src/client/src/commons/form/profile/hooks/useCasingList.js
+++ src/client/src/commons/form/profile/hooks/useCasingList.js
@@ -2,20 +2,26 @@ import { useState, useEffect, useRef } from "react";
 import { profileKind } from "../constance";
 import { useTranslation } from "react-i18next";
 import { getProfile } from "../components/profileInstrument/api";
+import { useQuery } from "react-query";
 
 export default function useCasingList(boreholeID) {
   const { t } = useTranslation();
 
   const [casing, setCasing] = useState([]);
   const mounted = useRef(false);
+  const { data: profiles } = useQuery(
+    ["profile", boreholeID, profileKind.CASING],
+    () => getProfile(boreholeID, profileKind.CASING),
+    { staleTime: 5000 },
+  );
 
   useEffect(() => {
     mounted.current = true;
 
-    getProfile(boreholeID, profileKind.CASING).then(response => {
+    if (profiles) {
       const temp = [{ key: 1, value: 0, text: t("common:no_casing") }];
-      if (response.length > 0) {
-        response.forEach(e => {
+      if (profiles.length > 0) {
+        profiles.forEach(e => {
           temp.push({
             key: e.id,
             value: e.id,
@@ -26,12 +32,12 @@ export default function useCasingList(boreholeID) {
       if (mounted.current) {
         setCasing(temp);
       }
-    });
+    }
 
     return () => {
       mounted.current = false;
     };
-  }, [boreholeID, t]);
+  }, [t, profiles]);
 
   return { casing };
 }
