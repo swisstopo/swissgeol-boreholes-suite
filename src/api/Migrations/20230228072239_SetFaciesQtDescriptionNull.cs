0218274a78ab211691bf31974d25a1d5e550eb8c
danjov <daniel.jovanovic@geowerkstatt.ch>
2023-02-28 08:28:36 +0100

    Set qt_description_id of facies descriptions to NULL

---
+++ src/api/Migrations/20230228072239_SetFaciesQtDescriptionNull.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDMS.Migrations;
+
+public partial class SetFaciesQtDescriptionNull : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        // Set quality of description of Facies Description to NULL
+        migrationBuilder.Sql("UPDATE bdms.facies_description SET qt_description_id = NULL;");
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+    }
+}
