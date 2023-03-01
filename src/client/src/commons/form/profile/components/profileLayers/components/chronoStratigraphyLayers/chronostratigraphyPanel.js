b5ff72c18ac125b5b0d4497911d8101c06840302
Patrick Ackermann <patrick.ackermann@geowerkstatt.ch>
2023-03-01 14:34:16 +0100

    Remove unused code from chronostratigraphyPanel

--- src/client/src/commons/form/profile/components/profileLayers/components/chronoStratigraphyLayers/chronostratigraphyPanel.js
+++ src/client/src/commons/form/profile/components/profileLayers/components/chronoStratigraphyLayers/chronostratigraphyPanel.js
@@ -1,23 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Stack, Typography, CircularProgress, Box } from "@mui/material";
-import LayerCard from "./layerCard";
-import LayerGap from "./layerGap";
 import { useTranslation } from "react-i18next";
 import ChronostratigraphyLayers from "./chronostratigraphyLayers";
 import LithologyLayers from "./lithologyLayers";
 import { useChronostratigraphies } from "../../../../../../../api/fetchApiV2";
-
-const getHeader = t => {
-  return [
-    { title: t("eon"), isVisible: true },
-    { title: t("era"), isVisible: true },
-    { title: t("period"), isVisible: true },
-    { title: t("epoch"), isVisible: true },
-    { title: t("subepoch"), isVisible: true },
-    { title: t("age"), isVisible: true },
-    { title: t("subage"), isVisible: true },
-  ];
-};
 
 const ChronostratigraphyPanel = ({ selectedStratigraphyID, isEditable }) => {
   const { data: chronostratigraphyQueryData } = useChronostratigraphies(
@@ -25,7 +11,6 @@ const ChronostratigraphyPanel = ({ selectedStratigraphyID, isEditable }) => {
   );
   const { t } = useTranslation();
 
-  const [header, setHeader] = useState(getHeader(t));
   const [navigationState, setNavigationState] = useState({
     minimapCursor: "grab",
     scale: 1,
@@ -36,43 +21,6 @@ const ChronostratigraphyPanel = ({ selectedStratigraphyID, isEditable }) => {
     // height of this component in pixels
     height: 0,
   });
-
-  useEffect(() => {
-    return setHeader(getHeader(t));
-  }, [t]);
-
-  const layerDisplayStack = [];
-  const layers = chronostratigraphyQueryData || [];
-  layers
-    .sort((a, b) => a.fromDepth - b.fromDepth || a.toDepth - b.toDepth)
-    .forEach((layer, index) => {
-      const previousLayerToDepth = index === 0 ? 0 : layers[index - 1]?.toDepth;
-      const nextLayerFromDepth =
-        index === layers.length - 1
-          ? Number.MAX_VALUE
-          : layers[index + 1]?.fromDepth;
-
-      if (layer.fromDepth > previousLayerToDepth) {
-        layerDisplayStack.push(
-          <LayerGap
-            key={-index}
-            previousLayer={layers[index - 1]}
-            nextLayer={layers[index]}
-            isEditable={isEditable}
-          />,
-        );
-      }
-      layerDisplayStack.push(
-        <LayerCard
-          key={layer.id}
-          layer={layer}
-          minFromDepth={previousLayerToDepth}
-          maxToDepth={nextLayerFromDepth}
-          header={header}
-          isEditable={isEditable}
-        />,
-      );
-    });
 
   if (!chronostratigraphyQueryData) {
     return <CircularProgress />;
