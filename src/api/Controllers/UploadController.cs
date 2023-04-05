3b5742d6c1280ba95979c3852f66d5def2059573
Frederic Stahel <frederic.stahel@geowerkstatt.ch>
2023-04-05 13:10:25 +0200

    Add validation to borehole csv import

--- src/api/Controllers/UploadController.cs
+++ src/api/Controllers/UploadController.cs
@@ -7,7 +7,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Globalization;
+using System.Net;
 using System.Security.Claims;
+using ValidationException = System.ComponentModel.DataAnnotations.ValidationException;
 
 namespace BDMS.Controllers;
 
@@ -59,6 +61,8 @@ public class UploadController : ControllerBase
                     return b;
                 }).ToList();
 
+            ValidateBoreholes(boreholes);
+
             var userName = HttpContext.User.FindFirst(ClaimTypes.Name)?.Value;
 
             var user = await context.Users
@@ -88,19 +92,74 @@ public class UploadController : ControllerBase
         catch (Exception ex)
         {
             logger.LogError("Error while importing borehole(s) to workgroup with id <{WorkgroupId}>: <{Error}>", workgroupId, ex);
+
+            if (ex is ValidationException validationEx)
+            {
+                if (!ModelState.IsValid)
+                {
+                    var problemDetails = new ValidationProblemDetails(ModelState);
+
+                    return new ObjectResult(problemDetails)
+                    {
+                        ContentTypes = { "application/problem+json" },
+                        StatusCode = 400,
+                    };
+                }
+
+                return Problem(validationEx.Message, statusCode: (int)HttpStatusCode.BadRequest);
+            }
+
             return Problem("Error while importing borehole(s).");
+        }
+    }
+
+    private void ValidateBoreholes(List<Borehole> boreholes)
+    {
+        foreach (var borehole in boreholes.Select((value, index) => (value, index)))
+        {
+            if (string.IsNullOrEmpty(borehole.value.OriginalName))
+            {
+                ModelState.AddModelError($"Row{borehole.index}", "Field 'original_name' is invalid.");
+            }
+
+            if (borehole.value.LocationX == null && borehole.value.LocationXLV03 == null)
+            {
+                ModelState.AddModelError($"Row{borehole.index}", "Field 'location_x' is invalid.");
+            }
+
+            if (borehole.value.LocationY == null && borehole.value.LocationYLV03 == null)
+            {
+                ModelState.AddModelError($"Row{borehole.index}", "Field 'location_y' is invalid.");
+            }
+        }
+
+        if (!ModelState.IsValid)
+        {
+            throw new ValidationException("");
         }
     }
 
     private List<Borehole> ReadBoreholesFromCsv(IFormFile file)
     {
+        var requiredHeaders = new List<string>() { "original_name", "location_x", "location_y" };
         var csvConfig = new CsvConfiguration(new CultureInfo("de-CH"))
         {
-            MissingFieldFound = null,
-            HeaderValidated = null,
             Delimiter = ";",
             IgnoreReferences = true,
             PrepareHeaderForMatch = args => args.Header.Humanize(LetterCasing.Title),
+            MissingFieldFound = null,
+
+            // Check if all required Headers are present in the provided CSV file.
+            HeaderValidated = args =>
+            {
+                foreach (var header in requiredHeaders)
+                {
+                    if (args.Context.Reader.HeaderRecord != null && !args.Context.Reader.HeaderRecord.Contains(header))
+                    {
+                        throw new ValidationException($"Please ensure all reuqired Headers ({string.Join(", ", requiredHeaders)}) are defined.");
+                    }
+                }
+            },
         };
 
         using var reader = new StreamReader(file.OpenReadStream());
@@ -145,6 +204,59 @@ public class UploadController : ControllerBase
             };
 
             AutoMap(config);
+
+            // Define all optional properties of Borehole (only simple types need to be defined as optional)
+            Map(m => m.Id).Optional();
+            Map(m => m.CreatedById).Optional();
+            Map(m => m.Created).Optional();
+            Map(m => m.Updated).Optional();
+            Map(m => m.UpdatedById).Optional();
+            Map(m => m.Locked).Optional();
+            Map(m => m.LockedById).Optional();
+            Map(m => m.WorkgroupId).Optional();
+            Map(m => m.IsPublic).Optional();
+            Map(m => m.KindId).Optional();
+            Map(m => m.ElevationZ).Optional();
+            Map(m => m.HrsId).Optional();
+            Map(m => m.TotalDepth).Optional();
+            Map(m => m.RestrictionId).Optional();
+            Map(m => m.RestrictionUntil).Optional();
+            Map(m => m.AlternateName).Optional();
+            Map(m => m.QtLocationId).Optional();
+            Map(m => m.QtElevationId).Optional();
+            Map(m => m.ProjectName).Optional();
+            Map(m => m.DrillingMethodId).Optional();
+            Map(m => m.DrillingDate).Optional();
+            Map(m => m.CuttingsId).Optional();
+            Map(m => m.PurposeId).Optional();
+            Map(m => m.DrillingDiameter).Optional();
+            Map(m => m.StatusId).Optional();
+            Map(m => m.Inclination).Optional();
+            Map(m => m.InclinationDirection).Optional();
+            Map(m => m.QtInclinationDirectionId).Optional();
+            Map(m => m.QtDepthId).Optional();
+            Map(m => m.TopBedrock).Optional();
+            Map(m => m.QtTopBedrockId).Optional();
+            Map(m => m.HasGroundwater).Optional();
+            Map(m => m.Remarks).Optional();
+            Map(m => m.LithologyTopBedrockId).Optional();
+            Map(m => m.LithostratigraphyId).Optional();
+            Map(m => m.ChronostratigraphyId).Optional();
+            Map(m => m.SpudDate).Optional();
+            Map(m => m.TopBedrockTvd).Optional();
+            Map(m => m.QtTopBedrockTvdId).Optional();
+            Map(m => m.ReferenceElevation).Optional();
+            Map(m => m.QtReferenceElevationId).Optional();
+            Map(m => m.ReferenceElevationTypeId).Optional();
+            Map(m => m.TotalDepthTvd).Optional();
+            Map(m => m.QtTotalDepthTvdId).Optional();
+
+            // Define properties to ignore
+            Map(b => b.Municipality).Ignore();
+            Map(b => b.Canton).Ignore();
+            Map(b => b.Country).Ignore();
+
+            // Define additional mapping logic
             Map(b => b.OriginalReferenceSystem).Convert(args =>
             {
                 var locationX = args.Row.GetField<double?>("location_x");
@@ -172,25 +284,23 @@ public class UploadController : ControllerBase
                     ("id_kernlager", 100000011),
                 }.ForEach(id =>
                 {
-                    var value = args.Row.GetField<string>(id.Name);
-                    if (!string.IsNullOrEmpty(value))
+                    if (args.Row.HeaderRecord != null && args.Row.HeaderRecord.Any(h => h == id.Name))
                     {
-                        boreholeCodelists.Add(new BoreholeCodelist
+                        var value = args.Row.GetField<string?>(id.Name);
+                        if (!string.IsNullOrEmpty(value))
                         {
-                            CodelistId = id.CodeListId,
-                            SchemaName = "borehole_identifier",
-                            Value = value,
-                        });
+                            boreholeCodelists.Add(new BoreholeCodelist
+                            {
+                                CodelistId = id.CodeListId,
+                                SchemaName = "borehole_identifier",
+                                Value = value,
+                            });
+                        }
                     }
                 });
 
                 return boreholeCodelists;
             });
-
-            // Ignore properties that get calculated based on x- and y-coordinates.
-            Map(b => b.Municipality).Ignore();
-            Map(b => b.Canton).Ignore();
-            Map(b => b.Country).Ignore();
         }
     }
 
