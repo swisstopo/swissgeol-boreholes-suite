84c40f4a86a7ee64e2ec4cd10e6632994ef42dc6
MiraGeowerktstatt <mira.bleuler@geowerkstatt.ch>
2023-04-18 10:20:44 +0200

    Fix style warnings

--- src/api/Models/ObservationType.cs
+++ src/api/Models/ObservationType.cs
@@ -3,25 +3,27 @@
 /// <summary>
 /// Represents an observation type.
 /// </summary>
+#pragma warning disable CA1008 // Enums should have zero value
 public enum ObservationType
+#pragma warning restore CA1008 // Enums should have zero value
 {
     /// <summary>
     /// The observation type water ingress.
     /// </summary>
-        WaterIngress = 1,
+    WaterIngress = 1,
 
     /// <summary>
     /// The observation type groundwater level.
     /// </summary>
-        GroundwaterLevel = 2,
+    GroundwaterLevel = 2,
 
     /// <summary>
     /// The observation type hydrotest.
     /// </summary>
-        Hydrotest = 3,
+    Hydrotest = 3,
 
     /// <summary>
     /// The observation type field measurement.
     /// </summary>
-        FieldMeasurement = 4,
+    FieldMeasurement = 4,
 }
