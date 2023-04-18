a899ac590a2caec5c82d5a38c2a83dd6fd1de519
Frederic Stahel <frederic.stahel@geowerkstatt.ch>
2023-04-18 09:06:48 +0200

    Fix tests

--- tests/UploadControllerTest.cs
+++ tests/UploadControllerTest.cs
@@ -191,7 +191,7 @@ public class UploadControllerTest
         ValidationProblemDetails problemDetails = (ValidationProblemDetails)result.Value!;
         Assert.AreEqual(1, problemDetails.Errors.Count);
 
-        CollectionAssert.AreEquivalent(new[] { "Field 'location_x' is required.", "Field 'location_y' is required." }, problemDetails.Errors["Row0"]);
+        CollectionAssert.AreEquivalent(new[] { "Field 'location_x' is required.", "Field 'location_y' is required." }, problemDetails.Errors["Row1"]);
     }
 
     [TestMethod]
@@ -354,14 +354,14 @@ public class UploadControllerTest
         ValidationProblemDetails problemDetails = (ValidationProblemDetails)result.Value!;
         Assert.AreEqual(2, problemDetails.Errors.Count);
 
-        CollectionAssert.AreEquivalent(new[] { "Field 'location_y' is required." }, problemDetails.Errors["Row1"]);
+        CollectionAssert.AreEquivalent(new[] { "Field 'location_y' is required." }, problemDetails.Errors["Row2"]);
         CollectionAssert.AreEquivalent(new[]
         {
             "Field 'original_name' is required.",
             "Field 'location_x' is required.",
             "Field 'location_y' is required.",
         },
-        problemDetails.Errors["Row2"]);
+        problemDetails.Errors["Row3"]);
     }
 
     [TestMethod]
@@ -409,12 +409,12 @@ public class UploadControllerTest
         {
             $"Borehole with same Coordinates (+/- 2m) and same {nameof(Borehole.TotalDepth)} is provied multiple times.",
         },
-        problemDetails.Errors["Row0"]);
+        problemDetails.Errors["Row1"]);
         CollectionAssert.AreEquivalent(new[]
         {
             $"Borehole with same Coordinates (+/- 2m) and same {nameof(Borehole.TotalDepth)} is provied multiple times.",
         },
-        problemDetails.Errors["Row1"]);
+        problemDetails.Errors["Row2"]);
     }
 
     [TestMethod]
@@ -457,12 +457,12 @@ public class UploadControllerTest
         {
             $"Borehole with same Coordinates (+/- 2m) and same {nameof(Borehole.TotalDepth)} already exists in database.",
         },
-        problemDetails.Errors["Row0"]);
+        problemDetails.Errors["Row1"]);
         CollectionAssert.AreEquivalent(new[]
         {
             $"Borehole with same Coordinates (+/- 2m) and same {nameof(Borehole.TotalDepth)} already exists in database.",
         },
-        problemDetails.Errors["Row1"]);
+        problemDetails.Errors["Row2"]);
     }
 
     [TestMethod]
