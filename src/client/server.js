93c9ea71adb3010503ae8068c3aa9a44674787cb
danjov <daniel.jovanovic@geowerkstatt.ch>
2023-04-27 16:51:12 +0200

    Use rate limiter

--- src/client/server.js
+++ src/client/server.js
@@ -4,6 +4,16 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require("express-rate-limit");
+var limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 5,
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
+
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/help/*", (req, res) => {
