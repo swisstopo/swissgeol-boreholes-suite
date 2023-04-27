21508be8143560807c194e1bda905fee2c996404
danjov <daniel.jovanovic@geowerkstatt.ch>
2023-04-27 16:20:58 +0200

    Use express server for hosting the deployed app
    Express supports serving static html files out of the build or public folder, npm serve did not.

---
+++ src/client/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/help/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "help", "index.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
