0de25020dbc882ffc78d62c4ec33f724d02c5f9c
Frederic Stahel <frederic.stahel@geowerkstatt.ch>
2023-05-02 06:52:47 +0200

    Change BoreholeFileUpdate XML comments

--- src/api/Models/BoreholeFileUpdate.cs
+++ src/api/Models/BoreholeFileUpdate.cs
@@ -3,9 +3,8 @@
 /// <summary>
 /// Represents a the borehole file's updateable properties.
 /// </summary>
-/// <![CDATA[
-/// Extend this class with any additional updatable properties of the borehole file.
-/// ]]>
+/// <remarks>Extend this class with any additional updatable properties of the borehole file.</remarks>
+/// <seealso cref="Borehole"/>
 public class BoreholeFileUpdate
 {
     /// <summary>
