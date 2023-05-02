2619d09b5025931115f32f3673ca6304fefaff97
Frederic Stahel <frederic.stahel@geowerkstatt.ch>
2023-05-02 06:54:44 +0200

    Fix typo

--- src/api/Models/BoreholeFileUpdate.cs
+++ src/api/Models/BoreholeFileUpdate.cs
@@ -4,7 +4,7 @@
 /// Represents a the borehole file's updateable properties.
 /// </summary>
 /// <remarks>Extend this class with any additional updatable properties of the borehole file.</remarks>
-/// <seealso cref="Borehole"/>
+/// <seealso cref="BoreholeFile"/>
 public class BoreholeFileUpdate
 {
     /// <summary>
