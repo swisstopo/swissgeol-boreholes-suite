cff4a8a3d261455245f6fce299abca354b2f202c
root <root@GEOWCL18.geow.local>
2023-05-15 16:41:38 +0200

    Fix merge error

--- src/client/cypress/e2e/testHelpers.js
+++ src/client/cypress/e2e/testHelpers.js
@@ -49,6 +49,7 @@ export const interceptApiCalls = () => {
 
   cy.intercept("/api/v2/groundwaterlevelmeasurement*", req => {
     return (req.alias = `groundwaterlevelmeasurement_${req.method}`);
+  });
 
   cy.intercept("/api/v2/hydrotest*", req => {
     return (req.alias = `hydrotest_${req.method}`);
