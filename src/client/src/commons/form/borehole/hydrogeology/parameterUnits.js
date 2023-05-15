1ce377f7a14c6361785cee3e442080508bd7b492
MiraGeowerktstatt <mira.bleuler@geowerkstatt.ch>
2023-05-15 10:43:06 +0200

    Add documentation to ParameterUnits

--- src/client/src/commons/form/borehole/hydrogeology/parameterUnits.js
+++ src/client/src/commons/form/borehole/hydrogeology/parameterUnits.js
@@ -1,3 +1,20 @@
+/**
+ * `ParameterUnits` is an object that maps GeolCodes of the Hydrotest.parameter codelist "htestres101" to their corresponding units of measurement.
+ *
+ * Each property in the `ParameterUnits` object represents a GeolCode as a key and its corresponding
+ * unit of measurement as a value. This mapping is used to determine the unit of measurement for
+ * a particular parameter based on its GeolCode.
+ *
+ * Key-value pairs in `ParameterUnits`:
+ * - `1`, `2`: "m/s" (meters per second)
+ * - `3`: "m2/s" (square meters per second)
+ * - `4`, `9`: "" (no unit)
+ * - `5`: "Pa" (Pascals)
+ * - `6`: "1/m" (inverse meters)
+ * - `7`: "Lu" (Lambertian reflectance units)
+ * - `8`: "m" (meters)
+ */
+
 export const ParameterUnits = {
   1: "m/s",
   2: "m/s",
