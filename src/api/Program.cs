7a7938123366dd01a731162ba291582eaeb5d516
Frederic Stahel <frederic.stahel@geowerkstatt.ch>
2023-05-17 11:18:17 +0200

    Set ForcePathStyle to true in AmazonS3Config

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -99,6 +99,7 @@ builder.Services.AddSingleton<IAmazonS3>(sp =>
     var s3ConfigSection = builder.Configuration.GetSection("S3");
     var clientConfig = new AmazonS3Config
     {
+        ForcePathStyle = true,
         ServiceURL = s3ConfigSection.GetValue<string>("ENDPOINT"),
         UseHttp = s3ConfigSection.GetValue<string>("SECURE") == "0",
     };
