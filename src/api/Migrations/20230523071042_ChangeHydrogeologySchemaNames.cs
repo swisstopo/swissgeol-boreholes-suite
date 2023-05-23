f96ce8a8699c6f2eff3493750f456fb0e3d70201
MiraGeowerktstatt <mira.bleuler@geowerkstatt.ch>
2023-05-23 09:20:41 +0200

    Fix hydrogeology schema names

---
+++ src/api/Migrations/20230523071042_ChangeHydrogeologySchemaNames.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDMS.Migrations;
+
+public partial class ChangeHydrogeologySchemaNames : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(@"
+            UPDATE bdms.codelist
+            SET schema_cli = 'observation_reliability'
+            WHERE schema_cli = 'observ101';
+
+            UPDATE bdms.codelist
+            SET schema_cli = 'wateringress_quantitiy'
+            WHERE schema_cli = 'waing101';
+
+            UPDATE bdms.codelist
+            SET schema_cli = 'wateringress_condition'
+            WHERE schema_cli = 'waing102';
+
+            UPDATE bdms.codelist
+            SET schema_cli = 'groundwaterlevel_kind'
+            WHERE schema_cli = 'gwlme101';
+
+            UPDATE bdms.codelist
+            SET schema_cli = 'hydrotest_kind'
+            WHERE schema_cli = 'htest101';
+
+            UPDATE bdms.codelist
+            SET schema_cli = 'hydrotest_flowdirection'
+            WHERE schema_cli = 'htest102';
+
+            UPDATE bdms.codelist
+            SET schema_cli = 'hydrotest_evaluationmethod'
+            WHERE schema_cli = 'htest103';
+
+            UPDATE bdms.codelist
+            SET schema_cli = 'hydrotest_result'
+            WHERE schema_cli = 'htestres101';
+
+            UPDATE bdms.codelist
+            SET schema_cli = 'fieldmeasurement_type'
+            WHERE schema_cli = 'field101';
+
+            UPDATE bdms.codelist
+            SET schema_cli = 'fieldmeasurement_parameter'
+            WHERE schema_cli = 'field102';");
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+    }
+}
--- src/api/Models/HydrogeologySchemas.cs
+++ src/api/Models/HydrogeologySchemas.cs
@@ -5,14 +5,14 @@
 /// </summary>
 public static class HydrogeologySchemas
 {
-    public const string ObservationReliabilitySchema = "observ101";
-    public const string WateringressQualitySchema = "waing101";
-    public const string WateringressConditionsSchema = "waing102";
-    public const string HydrotestKindSchema = "htest101";
-    public const string FlowdirectionSchema = "htest102";
-    public const string EvaluationMethodSchema = "htest103";
-    public const string HydrotestResultParameterSchema = "htestres101";
-    public const string GroundwaterLevelMeasurementKindSchema = "gwlme101";
-    public const string FieldMeasurementSampleTypeSchema = "field101";
-    public const string FieldMeasurementParameterSchema = "field102";
+    public const string ObservationReliabilitySchema = "observation_reliability";
+    public const string WateringressQualitySchema = "wateringress_quantitiy";
+    public const string WateringressConditionsSchema = "wateringress_condition";
+    public const string HydrotestKindSchema = "hydrotest_kind";
+    public const string FlowdirectionSchema = "hydrotest_flowdirection";
+    public const string EvaluationMethodSchema = "hydrotest_evaluationmethod";
+    public const string HydrotestResultParameterSchema = "hydrotest_result";
+    public const string GroundwaterLevelMeasurementKindSchema = "groundwaterlevel_kind";
+    public const string FieldMeasurementSampleTypeSchema = "fieldmeasurement_type";
+    public const string FieldMeasurementParameterSchema = "fieldmeasurement_parameter";
 }
