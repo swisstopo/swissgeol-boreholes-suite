7bd5fe8729ffdba85e2f4e9e317da19606094472
Frederic Stahel <frederic.stahel@geowerkstatt.ch>
2023-05-24 14:17:37 +0200

    Add lithology import cypress test

--- src/client/cypress/e2e/editor/import.cy.js
+++ src/client/cypress/e2e/editor/import.cy.js
@@ -98,4 +98,50 @@ describe("Test for importing boreholes.", () => {
         "Borehole with same Coordinates (+/- 2m) and same TotalDepth is provided multiple times.",
       );
   });
+
+  it("Displays lithology validation errors.", () => {
+    login("/editor");
+    cy.contains("a", "Import").click();
+
+    // Select borehole csv file
+    getImportFileFromFixtures(
+      "borehole-valid.csv",
+      null,
+      "invalid-lithology",
+    ).as("borehole-valid");
+    cy.get('[data-cy="import-boreholeFile-input"]').within(() => {
+      cy.get("input[type=file]").selectFile("@borehole-valid");
+    });
+
+    // Select lithology csv file
+    let lithologyFile = new DataTransfer();
+    getImportFileFromFixtures(
+      "lithology-single-not-valid.csv",
+      null,
+      "invalid-lithology",
+    ).then(fileContent => {
+      const file = new File([fileContent], "lithology-single-not-valid.csv", {
+        type: "text/csv",
+      });
+      lithologyFile.items.add(file);
+    });
+    cy.get('[data-cy="import-lithologyFile-input"]').within(() => {
+      cy.get("input[type=file]", { force: true }).then(input => {
+        input[0].files = lithologyFile.files;
+        input[0].dispatchEvent(new Event("change", { bubbles: true }));
+      });
+    });
+
+    cy.intercept("/api/v2/upload?workgroupId=1").as("borehole-upload");
+
+    cy.contains("button", "Import").click();
+
+    cy.wait("@borehole-upload");
+
+    cy.get('[data-cy="borehole-import-error-modal-content"]')
+      .should("not.contain", "Row0")
+      .should("contain", "Row1")
+      .should("contain", "Field 'to_depth' is required.")
+      .should("contain", "Borehole with ImportId '123456' not found.");
+  });
 });
