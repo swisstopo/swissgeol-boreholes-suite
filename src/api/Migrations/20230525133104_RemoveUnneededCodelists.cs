d1d16d643e598c241511fd145a7068e3e085eeca
MiraGeowerktstatt <mira.bleuler@geowerkstatt.ch>
2023-05-25 18:22:32 +0200

    Remove borehole.form and ebor106

---
+++ src/api/Migrations/20230525133104_RemoveUnneededCodelists.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDMS.Migrations;
+
+public partial class RemoveUnneededCodelists : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(@"
+           	DELETE FROM bdms.codelist
+	        WHERE schema_cli = 'ebor106';
+
+           	DELETE FROM bdms.codelist
+	        WHERE schema_cli = 'borehole_form';
+");
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+    }
+}
