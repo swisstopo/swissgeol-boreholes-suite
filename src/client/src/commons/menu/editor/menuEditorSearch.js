d51f97d9074a95dd7750a183a78a23b758681d33
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2023-06-05 12:04:13 +0200

    Remove chronostratigraphy from lithology import

--- src/client/src/commons/menu/editor/menuEditorSearch.js
+++ src/client/src/commons/menu/editor/menuEditorSearch.js
@@ -323,7 +323,7 @@ class MenuEditorSearch extends React.Component {
                   <TranslationText id="csvFormatExplanation" />.
                   {this.ExampleHeadings(
                     "import_id;strati_import_id;strati_date;strati_name;from_depth;to_depth;" +
-                      "is_last;qt_description_id;lithology_id;lithostratigraphy_id;chronostratigraphy_id;" +
+                      "is_last;qt_description_id;lithology_id;lithostratigraphy_id;" +
                       "original_uscs;uscs_determination_id;uscs_1_id;grain_size_1_id;uscs_2_id;grain_size_2_id;" +
                       "is_striae;consistance_id;plasticity_id;compactness_id;cohesion_id;humidity_id;alteration_id;" +
                       "notes;original_lithology;uscs_3_ids;grain_shape_ids;grain_granularity_ids;organic_component_ids;" +
