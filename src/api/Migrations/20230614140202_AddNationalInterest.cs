00cfd65b5f5d03cfa6f2e8e9d462dc524eddad5f
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2023-06-14 16:10:52 +0200

    Add new field to api and db

---
+++ src/api/Migrations/20230614140202_AddNationalInterest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDMS.Migrations
+{
+    public partial class AddNationalInterest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_national_interest",
+                schema: "bdms",
+                table: "borehole",
+                type: "boolean",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_national_interest",
+                schema: "bdms",
+                table: "borehole");
+        }
+    }
+}
