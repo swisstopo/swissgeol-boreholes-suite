f50abb0023891b01b9f31c65c8d8b142e400bcbb
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2023-12-04 16:24:20 +0100

    Add migration for changes caused by .net8 update

---
+++ src/api/Migrations/20231204151555_UpdateAfterMigration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDMS.Migrations;
+
+/// <inheritdoc />
+public partial class UpdateAfterMigration : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterDatabase()
+            .Annotation("Npgsql:PostgresExtension:ltree", ",,")
+            .Annotation("Npgsql:PostgresExtension:postgis", ",,");
+
+        migrationBuilder.AlterColumn<int>(
+            name: "id_rol_fk",
+            schema: "bdms",
+            table: "workflow",
+            type: "integer",
+            nullable: true,
+            oldClrType: typeof(int),
+            oldType: "int",
+            oldNullable: true);
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+    }
+}
