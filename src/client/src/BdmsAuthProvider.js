f016abdb36fa899c3f4f89dbf0d9303b72040e25
Philipp Lüthi <philipp.luethi@geowerkstatt.ch>
2023-12-22 11:11:31 +0100

    Enable token refreshes & auto logout after expiration

--- src/client/src/BdmsAuthProvider.js
+++ src/client/src/BdmsAuthProvider.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { AuthProvider, useAuth } from "react-oidc-context";
 import { useDispatch } from "react-redux";
 import { setAuthentication, unsetAuthentication } from "./api-lib";
@@ -8,32 +8,36 @@ const AuthenticationStoreSync = () => {
   const auth = useAuth();
   const dispatch = useDispatch();
 
+  const [userValueExpired, setUserValueExpired] = useState(false);
+
   useEffect(() => {
-    if (auth.user) {
+    if (auth.isLoading) return;
+
+    if (auth.user && !auth.user.expired) {
+      // Trigger delayed rerender to reevaluate user.expired value.
+      setTimeout(
+        () => setUserValueExpired(current => !current),
+        auth.user.expires_in * 1000,
+      );
       dispatch(setAuthentication(auth.user));
+    } else {
+      dispatch(unsetAuthentication());
     }
-  }, [auth.user, dispatch]);
+  }, [auth.user, userValueExpired, dispatch, auth.isLoading]);
 };
 
 export const BdmsAuthProvider = props => {
-  const dispatch = useDispatch();
-
   const onSigninCallback = user => {
     window.history.replaceState({}, document.title, window.location.pathname);
-  };
-
-  const onRemoveUser = () => {
-    dispatch(unsetAuthentication());
   };
 
   const oidcConfig = {
     authority: window.env.AUTHORITY,
     client_id: window.env.CLIENT_ID,
     scope: "openid profile email",
-    redirect_uri: window.location.origin,
+    redirect_uri: window.location.origin + window.location.pathname,
     userStore: new WebStorageStateStore({ store: window.localStorage }),
     onSigninCallback,
-    onRemoveUser,
   };
 
   return (
