c190672eb5c80d5f0dbee2c148a9bcc0f85951ad
Philipp Lüthi <philipp.luethi@geowerkstatt.ch>
2024-01-16 14:50:54 +0100

    Add AuthSettings doc

--- src/api/Models/AuthSettings.cs
+++ src/api/Models/AuthSettings.cs
@@ -1,3 +1,9 @@
 ﻿namespace BDMS.Models;
 
+/// <summary>
+/// Represents the authentication settings for the application.
+/// </summary>
+/// <param name="Authority">The authority URL providing OpenID Connect discovery document.</param>
+/// <param name="Audience">The audience for required by the application.</param>
+/// <param name="Scopes">The scopes required by the application separated by space.</param>
 public record AuthSettings(string Authority, string Audience, string Scopes);
