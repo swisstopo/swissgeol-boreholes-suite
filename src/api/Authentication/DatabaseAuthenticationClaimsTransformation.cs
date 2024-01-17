a8203feaee110495222c2838177be6e9c6def5e1
Philipp Lüthi <philipp.luethi@geowerkstatt.ch>
2024-01-17 18:27:29 +0100

    Fix race condition on user insert

--- src/api/Authentication/DatabaseAuthenticationClaimsTransformation.cs
+++ src/api/Authentication/DatabaseAuthenticationClaimsTransformation.cs
@@ -1,5 +1,6 @@
 ﻿using BDMS.Models;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 
 namespace BDMS.Authentication;
@@ -21,26 +22,45 @@ public class DatabaseAuthenticationClaimsTransformation : IClaimsTransformation
     /// <inheritdoc/>
     public async Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
     {
-        var userId = principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
-        if (userId is null) return principal;
-
-        var authenticatedUser =
-            dbContext.Users.FirstOrDefault(u => u.SubjectId == userId.Value) ?? new User { SubjectId = userId.Value };
-
-        if (authenticatedUser.IsDisabled) return principal;
-
-        authenticatedUser.FirstName = principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.GivenName)?.Value ?? authenticatedUser.FirstName;
-        authenticatedUser.LastName = principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Surname)?.Value ?? authenticatedUser.LastName;
-        authenticatedUser.Name = $"{authenticatedUser.FirstName[0]}. {authenticatedUser.LastName}";
-        dbContext.Update(authenticatedUser);
-        await dbContext.SaveChangesAsync().ConfigureAwait(false);
+        var authenticatedUser = await GetOrInsertUser(principal).ConfigureAwait(false);
+        if (authenticatedUser is null)
+            return principal;
 
         var claimsIdentity = new ClaimsIdentity();
         claimsIdentity.AddClaim(new Claim(ClaimTypes.Name, authenticatedUser.Name));
-        if (authenticatedUser.IsAdmin) claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, PolicyNames.Admin));
-        else if (authenticatedUser.IsViewer) claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, PolicyNames.Viewer));
-
+        if (authenticatedUser.IsAdmin)
+            claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, PolicyNames.Admin));
+        else if (authenticatedUser.IsViewer)
+            claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, PolicyNames.Viewer));
         principal.AddIdentity(claimsIdentity);
+
         return principal;
+    }
+
+    private async Task<User?> GetOrInsertUser(ClaimsPrincipal principal)
+    {
+        var subjectId = principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+        if (subjectId is null)
+            return null;
+
+        var user = dbContext.Users.FirstOrDefault(u => u.SubjectId == subjectId) ?? new User { SubjectId = subjectId };
+        user.FirstName = principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.GivenName)?.Value ?? user.FirstName;
+        user.LastName = principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Surname)?.Value ?? user.LastName;
+        user.Name = $"{user.FirstName[0]}. {user.LastName}";
+        dbContext.Update(user);
+
+        try
+        {
+            await dbContext.SaveChangesAsync().ConfigureAwait(false);
+        }
+        catch (DbUpdateException ex)
+        {
+            if (!ex.InnerException.Message.Contains("users_subject_id_unique", StringComparison.OrdinalIgnoreCase))
+                throw;
+
+            user = dbContext.Users.First(u => u.SubjectId == subjectId);
+        }
+
+        return user;
     }
 }
