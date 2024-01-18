d563bb2bf36acc7b0e8f43f8459f976c91d79714
Philipp Lüthi <philipp.luethi@geowerkstatt.ch>
2024-01-18 16:21:35 +0100

    Fix invalid new users by adding viewer privelege

--- src/api/Authentication/DatabaseAuthenticationClaimsTransformation.cs
+++ src/api/Authentication/DatabaseAuthenticationClaimsTransformation.cs
@@ -52,7 +52,7 @@ public class DatabaseAuthenticationClaimsTransformation : IClaimsTransformation
         if (subjectId is null)
             return null;
 
-        var user = dbContext.Users.SingleOrDefault(u => u.SubjectId == subjectId) ?? new User { SubjectId = subjectId };
+        var user = dbContext.Users.SingleOrDefault(u => u.SubjectId == subjectId) ?? new User { SubjectId = subjectId, IsViewer = true };
         user.FirstName = principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.GivenName)?.Value ?? user.FirstName;
         user.LastName = principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Surname)?.Value ?? user.LastName;
         user.Name = $"{user.FirstName[0]}. {user.LastName}";
