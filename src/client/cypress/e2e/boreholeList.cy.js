864f1bd9c2ef25348314f67bac1664835c11bcea
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2024-02-12 09:00:31 +0100

    Fix cypress tests

--- src/client/cypress/e2e/boreholeList.cy.js
+++ src/client/cypress/e2e/boreholeList.cy.js
@@ -104,9 +104,9 @@ describe("Borehole list tests", () => {
         }
       });
 
-    cy.get("tbody").children().eq(0).contains("td", "08.02.2024");
-    cy.get("tbody").children().eq(1).contains("td", "08.02.2024");
-    cy.get("tbody").children().eq(2).contains("td", "08.02.2024");
+    cy.get("tbody").children().eq(0).contains("td", "31.12.2021");
+    cy.get("tbody").children().eq(1).contains("td", "31.12.2021");
+    cy.get("tbody").children().eq(2).contains("td", "31.12.2021");
 
     cy.contains("th", "Creation date").click();
     cy.wait("@editorBorehole");
