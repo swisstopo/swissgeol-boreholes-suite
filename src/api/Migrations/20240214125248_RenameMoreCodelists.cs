eea0c5a12d1674f11b75bd392bb44a3cf87d59b9
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2024-02-14 16:09:30 +0100

    Convert to file-scoped namesspace

--- src/api/Migrations/20240214125248_RenameMoreCodelists.cs
+++ src/api/Migrations/20240214125248_RenameMoreCodelists.cs
@@ -2,19 +2,18 @@
 
 #nullable disable
 
-namespace BDMS.Migrations
+namespace BDMS.Migrations;
+
+/// <inheritdoc />
+public partial class RenameMoreCodelists : Migration
 {
     /// <inheritdoc />
-    public partial class RenameMoreCodelists : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.Sql(@"
+        migrationBuilder.Sql(@"
             UPDATE bdms.codelist SET schema_cli = 'borehole_type' WHERE schema_cli = 'kind';
             UPDATE bdms.codelist SET schema_cli = 'spatial_reference_system' WHERE schema_cli = 'srs';
             UPDATE bdms.codelist SET schema_cli = 'height_reference_system' WHERE schema_cli = 'hrs'; 
             UPDATE bdms.codelist SET schema_cli = 'countries' WHERE schema_cli = 'country'; ");
-        }
     }
 }
