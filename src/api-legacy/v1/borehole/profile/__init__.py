bba22a584c89ac8a924e2c7f8fb26b9a786e7dbb
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2024-02-15 08:56:53 +0100

    Remove unused code

--- src/api-legacy/v1/borehole/profile/__init__.py
+++ src/api-legacy/v1/borehole/profile/__init__.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
 # Actions
-from bms.v1.borehole.profile.list import ListProfiles
-from bms.v1.borehole.profile.get import GetProfile
 from bms.v1.borehole.profile.validate import ValidateProfile
 
