530a99a22fc847d1a19627c631d0e92b30e8c255
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2024-02-26 12:08:01 +0100

    Add documentation for ILayerCodeExtensions

--- src/api/Models/ILayerCodeExtensions.cs
+++ src/api/Models/ILayerCodeExtensions.cs
@@ -1,7 +1,14 @@
 ﻿namespace BDMS.Models;
 
+/// <summary>
+/// Provides extension methods for the ILayerCode interface.
+/// </summary>
 public static class ILayerCodeExtensions
 {
+    /// <summary>
+    /// Resets the LayerId of each ILayerCode in the provided collection to 0.
+    /// </summary>
+    /// <param name="layerCodes">The collection of ILayerCode objects to reset.</param>
     public static void ResetLayerIds(this IEnumerable<ILayerCode> layerCodes)
     {
         foreach (var layerCode in layerCodes)
