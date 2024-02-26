6c29c84a6e8ef668f75368f7277406bc66db1344
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2024-02-26 12:05:37 +0100

    Add migrations for precision

---
+++ src/api/Migrations/20240226101420_AddPrecisionAttributeForCoordinates.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPrecisionAttributeForCoordinates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "precision_location_x",
+                schema: "bdms",
+                table: "borehole",
+                type: "integer",
+                nullable: true,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "precision_location_x_lv03",
+                schema: "bdms",
+                table: "borehole",
+                type: "integer",
+                nullable: true,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "precision_location_y",
+                schema: "bdms",
+                table: "borehole",
+                type: "integer",
+                nullable: true,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "precision_location_y_lv03",
+                schema: "bdms",
+                table: "borehole",
+                type: "integer",
+                nullable: true,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "precision_location_x",
+                schema: "bdms",
+                table: "borehole");
+
+            migrationBuilder.DropColumn(
+                name: "precision_location_x_lv03",
+                schema: "bdms",
+                table: "borehole");
+
+            migrationBuilder.DropColumn(
+                name: "precision_location_y",
+                schema: "bdms",
+                table: "borehole");
+
+            migrationBuilder.DropColumn(
+                name: "precision_location_y_lv03",
+                schema: "bdms",
+                table: "borehole");
+        }
+    }
+}
