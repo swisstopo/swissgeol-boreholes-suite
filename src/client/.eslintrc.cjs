004edbdfa93237e60a8059df3dd99bc757c55e74
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2024-02-28 08:25:29 +0100

    Add eslint config and include prettier config

---
+++ src/client/.eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "detect" } },
+  plugins: ["react-refresh", "prettier"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        bracketSameLine: true,
+        trailingComma: "all",
+        arrowParens: "avoid",
+        printWidth: 120,
+        endOfLine: "auto",
+      },
+    ],
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    "react/prop-types": "off",
+  },
+};
