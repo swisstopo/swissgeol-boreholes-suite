2df665a7b5a8c0e6bca3cad09304a682736fdae0
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2024-03-06 17:03:39 +0100

    Fix migration style

--- src/api/Migrations/20240306155407_RemoveIsLastAttributes.cs
+++ src/api/Migrations/20240306155407_RemoveIsLastAttributes.cs
@@ -2,65 +2,64 @@
 
 #nullable disable
 
-namespace BDMS.Migrations
+namespace BDMS.Migrations;
+
+/// <inheritdoc />
+public partial class RemoveIsLastAttributes : Migration
 {
     /// <inheritdoc />
-    public partial class RemoveIsLastAttributes : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "is_last",
-                schema: "bdms",
-                table: "lithostratigraphy");
+        migrationBuilder.DropColumn(
+            name: "is_last",
+            schema: "bdms",
+            table: "lithostratigraphy");
 
-            migrationBuilder.DropColumn(
-                name: "is_last",
-                schema: "bdms",
-                table: "lithological_description");
+        migrationBuilder.DropColumn(
+            name: "is_last",
+            schema: "bdms",
+            table: "lithological_description");
 
-            migrationBuilder.DropColumn(
-                name: "is_last",
-                schema: "bdms",
-                table: "facies_description");
+        migrationBuilder.DropColumn(
+            name: "is_last",
+            schema: "bdms",
+            table: "facies_description");
 
-            migrationBuilder.DropColumn(
-                name: "is_last",
-                schema: "bdms",
-                table: "chronostratigraphy");
-        }
+        migrationBuilder.DropColumn(
+            name: "is_last",
+            schema: "bdms",
+            table: "chronostratigraphy");
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<bool>(
-                name: "is_last",
-                schema: "bdms",
-                table: "lithostratigraphy",
-                type: "boolean",
-                nullable: true);
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<bool>(
+            name: "is_last",
+            schema: "bdms",
+            table: "lithostratigraphy",
+            type: "boolean",
+            nullable: true);
 
-            migrationBuilder.AddColumn<bool>(
-                name: "is_last",
-                schema: "bdms",
-                table: "lithological_description",
-                type: "boolean",
-                nullable: true);
+        migrationBuilder.AddColumn<bool>(
+            name: "is_last",
+            schema: "bdms",
+            table: "lithological_description",
+            type: "boolean",
+            nullable: true);
 
-            migrationBuilder.AddColumn<bool>(
-                name: "is_last",
-                schema: "bdms",
-                table: "facies_description",
-                type: "boolean",
-                nullable: true);
+        migrationBuilder.AddColumn<bool>(
+            name: "is_last",
+            schema: "bdms",
+            table: "facies_description",
+            type: "boolean",
+            nullable: true);
 
-            migrationBuilder.AddColumn<bool>(
-                name: "is_last",
-                schema: "bdms",
-                table: "chronostratigraphy",
-                type: "boolean",
-                nullable: true);
-        }
+        migrationBuilder.AddColumn<bool>(
+            name: "is_last",
+            schema: "bdms",
+            table: "chronostratigraphy",
+            type: "boolean",
+            nullable: true);
     }
 }
