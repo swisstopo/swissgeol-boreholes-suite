19d1c664e0295b01248a18b737c7fd104ac63300
Philipp Lüthi <philipp.luethi@geowerkstatt.ch>
2024-03-18 15:31:18 +0100

    Fix lint errors

--- src/client/src/pages/settings/dataLoader.jsx
+++ src/client/src/pages/settings/dataLoader.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch, useStore } from "react-redux";
 
 import { loadDomains, loadBoreholes, loadSettings } from "../../api-lib/index";
@@ -6,23 +6,22 @@ import { SplashScreen } from "../../commons/auth/SplashScreen";
 import { CircularProgress } from "@mui/material";
 
 export const DataLoader = ({ children }) => {
-    const dispatch = useDispatch();
-    const isReady = useStore(state => state.dataLoaderState.isReady)
+  const dispatch = useDispatch();
+  const isReady = useStore(state => state.dataLoaderState.isReady);
 
-    useEffect(() => {
-        dispatch(loadDomains());
-        // Only load one borehole to get the total borehole count.
-        // We need the count in case of the map only appearance, otherwise the boreholes get loaded by the borehole table.
-        dispatch(loadBoreholes(1, 1));
-        dispatch(loadSettings());
-    })
+  useEffect(() => {
+    dispatch(loadDomains());
+    // Only load one borehole to get the total borehole count.
+    // We need the count in case of the map only appearance, otherwise the boreholes get loaded by the borehole table.
+    dispatch(loadBoreholes(1, 1));
+    dispatch(loadSettings());
+  });
 
-    return isReady ? (
-      children
-    ) : (
-      <SplashScreen>
-        <CircularProgress />
-      </SplashScreen>
-    );
-}
-
+  return isReady ? (
+    children
+  ) : (
+    <SplashScreen>
+      <CircularProgress />
+    </SplashScreen>
+  );
+};
