9c17f7e8f59328641c0feb3889a1d34f0b61ad5a
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2024-04-02 11:09:15 +0200

    Reset empty lines

--- src/client/cypress/e2e/app.cy.js
+++ src/client/cypress/e2e/app.cy.js
@@ -7,6 +7,7 @@ describe("General app tests", () => {
 
     // Fail GET of auth setting to prevent auto login.
     cy.intercept("/api/v2/settings/auth", req => req.destroy());
+
     cy.visit("/");
     cy.contains("Welcome to");
 
@@ -26,7 +27,6 @@ describe("General app tests", () => {
   it("Displays the current host as app title", () => {
     loginAsEditor();
     cy.visit("/");
-
     cy.get('[data-cy="app-title"]').contains("localhost");
   });
 });
