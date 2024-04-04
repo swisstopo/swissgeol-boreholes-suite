5b69dd8f2ca97e4eadd5ede7cb5945b09909a4ac
Patrick Ackermann <patrick.ackermann@geowerkstatt.ch>
2024-04-04 10:45:36 +0200

    Fix doc comment detached from method

--- src/client/src/commons/form/borehole/stratigraphy/clamp.js
+++ src/client/src/commons/form/borehole/stratigraphy/clamp.js
@@ -1,1 +1,10 @@
+/**
+ * Clamp a number to the range defined by min and max.
+ * If min > max, max is returned.
+ *
+ * @param {Number} num The number to clamp
+ * @param {Number} min The lower boundary
+ * @param {Number} max The upper boundary
+ * @returns A number between min and max
+ */
 export const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
