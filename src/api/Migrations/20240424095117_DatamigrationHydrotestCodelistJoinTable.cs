0d2cac389d001dc895675c6faef315089db1383a
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2024-04-25 09:25:54 +0200

    Fix sql script

--- src/api/Migrations/20240424095117_DatamigrationHydrotestCodelistJoinTable.cs
+++ src/api/Migrations/20240424095117_DatamigrationHydrotestCodelistJoinTable.cs
@@ -11,29 +11,31 @@ public partial class DatamigrationHydrotestCodelistJoinTable : Migration
     /// <inheritdoc />
     protected override void Up(MigrationBuilder migrationBuilder)
     {
-        migrationBuilder.Sql(@"INSERT INTO hydrotest_kind_codelist (hydrotest_id, codelist_id)
+        migrationBuilder.Sql(@"
+            INSERT INTO bdms.hydrotest_kind_codelist (hydrotest_id, codelist_id)
             SELECT hc.id_ht_fk, hc.id_cli_fk
-            FROM hydrotest_codelist hc
-            JOIN codelists c ON hc.codelist_id = c.Id
-            WHERE c.schema_name = 'hydrotest_kind';
-            
-            INSERT INTO hydrotest_flowdirection_codelist (hydrotest_id, codelist_id)
+            FROM bdms.hydrotest_codelist hc
+            JOIN bdms.codelist cl ON hc.id_cli_fk = cl.id_cli
+            WHERE cl.schema_cli = 'hydrotest_kind';
+
+            INSERT INTO bdms.hydrotest_flowdirection_codelist (hydrotest_id, codelist_id)
             SELECT hc.id_ht_fk, hc.id_cli_fk
-            FROM hydrotest_codelist hc
-            JOIN codelists c ON hc.codelist_id = c.Id
-            WHERE c.schema_name = 'hydrotest_flowdirection';
-            
-            INSERT INTO hydrotest_evaluationmethod_codelist (hydrotest_id, codelist_id)
+            FROM bdms.hydrotest_codelist hc
+            JOIN bdms.codelist cl ON hc.id_cli_fk = cl.id_cli
+            WHERE cl.schema_cli = 'hydrotest_flowdirection';
+
+            INSERT INTO bdms.hydrotest_evaluationmethod_codelist (hydrotest_id, codelist_id)
             SELECT hc.id_ht_fk, hc.id_cli_fk
-            FROM hydrotest_codelist hc
-            JOIN codelists c ON hc.codelist_id = c.Id
-            WHERE c.schema_name = 'hydrotest_evaluationmethod';
-            
-            INSERT INTO hydrotest_result_codelist (hydrotest_id, codelist_id)
+            FROM bdms.hydrotest_codelist hc
+            JOIN bdms.codelist cl ON hc.id_cli_fk = cl.id_cli
+            WHERE cl.schema_cli = 'hydrotest_evaluationmethod';
+
+            INSERT INTO bdms.hydrotest_result_codelist (hydrotest_id, codelist_id)
             SELECT hc.id_ht_fk, hc.id_cli_fk
-            FROM hydrotest_codelist hc
-            JOIN codelists c ON hc.codelist_id = c.Id
-            WHERE c.schema_name = 'hydrotest_result';");
+            FROM bdms.hydrotest_codelist hc
+            JOIN bdms.codelist cl ON hc.id_cli_fk = cl.id_cli
+            WHERE cl.schema_cli = 'hydrotest_result';
+            ");
 
         migrationBuilder.DropTable(
             name: "hydrotest_codelist",
