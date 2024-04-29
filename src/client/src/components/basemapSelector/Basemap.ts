1464a30c3ceb00622df077d2d0b62d5bf5f8b6ae
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2024-04-29 13:57:38 +0200

    Improve basemap creation

--- src/client/src/components/basemapSelector/Basemap.ts
+++ src/client/src/components/basemapSelector/Basemap.ts
@@ -1,9 +1,8 @@
 import TileLayer from "ol/layer/Tile";
-import LayerGroup from "ol/layer/Group";
 import WMTS from "ol/source/WMTS";
 
 export interface Basemap {
   previewImg: string;
   shortName: string;
-  layer: LayerGroup | TileLayer<WMTS>;
+  layer: TileLayer<WMTS>;
 }
--- src/client/src/components/basemapSelector/basemaps.ts
+++ src/client/src/components/basemapSelector/basemaps.ts
@@ -1,124 +1,77 @@
 import TileLayer from "ol/layer/Tile";
 import WMTSTileGrid from "ol/tilegrid/WMTS";
-import LayerGroup from "ol/layer/Group";
 import WMTS from "ol/source/WMTS";
 import { Basemap } from "./Basemap";
 import { ProjectionLike, get as getProjection } from "ol/proj";
 
-const srs = "EPSG:2056";
-const projection: ProjectionLike = getProjection(srs) as ProjectionLike;
-const matrixSet = "2056";
-const requestEncoding = "REST";
-const style = "default";
-const crossOrigin = "anonymous";
-const attributions: string =
-  '&copy; Data: <a style="color: black; text-decoration: underline;" href="https://www.swisstopo.admin.ch">swisstopo</a>';
+const projection: ProjectionLike = getProjection("EPSG:2056") as ProjectionLike;
 
 const resolutions: number[] = [
   4000, 3750, 3500, 3250, 3000, 2750, 2500, 2250, 2000, 1750, 1500, 1250, 1000, 750, 650, 500, 250, 100, 50, 20, 10, 5,
   2.5, 2, 1.5, 1, 0.5, 0.25, 0.1, 0.05, 0.025, 0.01,
 ];
-const extent: number[] = [2420000, 1030000, 2900000, 1350000];
+
 const matrixIds: string[] = [];
 for (let i = 0; i < resolutions.length; i++) {
   matrixIds.push(i.toString());
 }
+
+export const swissExtent: number[] = [2420000, 1030000, 2900000, 1350000];
+
 const tileGrid: WMTSTileGrid = new WMTSTileGrid({
-  origin: [extent[0], extent[3]],
+  origin: [swissExtent[0], swissExtent[3]],
   resolutions,
   matrixIds,
 });
 
+const createLayer = (layerInfo: { zIndex: number; layerName: string }) => {
+  return new TileLayer({
+    zIndex: layerInfo.zIndex,
+    source: new WMTS({
+      layer: layerInfo.layerName,
+      url: "https://wmts.geo.admin.ch/1.0.0/{Layer}/default/current/2056/{TileMatrix}/{TileCol}/{TileRow}.jpeg",
+      crossOrigin: "anonymous",
+      attributions:
+        '&copy; Data: <a style="color: black; text-decoration: underline;" href="https://www.swisstopo.admin.ch">swisstopo</a>',
+      requestEncoding: "REST",
+      style: "default",
+      matrixSet: "2056",
+      tileGrid,
+      projection,
+    }),
+  });
+};
+
+const baseLayerNames = {
+  satellite: "ch.swisstopo.swissimage",
+  colormap: "ch.swisstopo.pixelkarte-farbe",
+  greymap: "ch.swisstopo.pixelkarte-grau",
+};
+
 export const basemaps: Basemap[] = [
   {
     shortName: "colormap",
-    previewImg: "ch.swisstopo.pixelkarte-farbe",
-    layer: ((): LayerGroup => {
-      const layerGroup = new LayerGroup({
-        zIndex: 0,
-        layers: [
-          new TileLayer({
-            minResolution: 2.5,
-            source: new WMTS({
-              dimensions: {
-                Time: "current",
-              },
-              layer: "ch.swisstopo.pixelkarte-farbe",
-              url: "https://wmts10.geo.admin.ch/1.0.0/{Layer}/default/{Time}/2056/{TileMatrix}/{TileCol}/{TileRow}.jpeg",
-              crossOrigin,
-              attributions,
-              tileGrid,
-              projection,
-              requestEncoding,
-              style,
-              matrixSet,
-            }),
-          }),
-          new TileLayer({
-            maxResolution: 2.5,
-            source: new WMTS({
-              dimensions: {
-                Time: "current",
-              },
-              layer: "ch.swisstopo.swisstlm3d-karte-farbe",
-              url: "https://wmts10.geo.admin.ch/1.0.0/{Layer}/default/{Time}/2056/{TileMatrix}/{TileCol}/{TileRow}.png",
-              crossOrigin,
-              attributions,
-              tileGrid,
-              projection,
-              requestEncoding,
-              style,
-              matrixSet,
-            }),
-          }),
-        ],
-      });
-      layerGroup.set("name", "colormap");
-      return layerGroup;
-    })() as LayerGroup,
+    previewImg: baseLayerNames.colormap,
+    layer: createLayer({
+      zIndex: 0,
+      layerName: baseLayerNames.colormap,
+    }),
   },
   {
     shortName: "satellite",
-    previewImg: "ch.swisstopo.swissimage",
-    layer: ((): TileLayer<WMTS> => {
-      const tileLayer = new TileLayer({
-        zIndex: 1,
-        source: new WMTS({
-          layer: "ch.swisstopo.swissimage",
-          url: "https://wmts10.geo.admin.ch/1.0.0/{Layer}/default/current/2056/{TileMatrix}/{TileCol}/{TileRow}.jpeg",
-          crossOrigin,
-          attributions,
-          tileGrid,
-          projection,
-          requestEncoding,
-          style,
-          matrixSet,
-        }),
-      });
-      tileLayer.set("name", "swissimage");
-      return tileLayer;
-    })() as TileLayer<WMTS>,
+    previewImg: baseLayerNames.satellite,
+    layer: createLayer({
+      zIndex: 1,
+      layerName: baseLayerNames.satellite,
+    }),
   },
+
   {
     shortName: "greymap",
-    previewImg: "ch.swisstopo.pixelkarte-grau",
-    layer: ((): TileLayer<WMTS> => {
-      const tileLayer = new TileLayer({
-        zIndex: 2,
-        source: new WMTS({
-          layer: "ch.swisstopo.pixelkarte-grau",
-          url: "https://wmts10.geo.admin.ch/1.0.0/{Layer}/default/current/2056/{TileMatrix}/{TileCol}/{TileRow}.jpeg",
-          crossOrigin,
-          attributions,
-          tileGrid,
-          projection,
-          requestEncoding,
-          style,
-          matrixSet,
-        }),
-      });
-      tileLayer.set("name", "greymap");
-      return tileLayer;
-    })(),
+    previewImg: baseLayerNames.greymap,
+    layer: createLayer({
+      zIndex: 2,
+      layerName: baseLayerNames.greymap,
+    }),
   },
 ];
