3b144b1b20829087444b6ad90fe6b6d07b3eec36
Patrick Ackermann <patrick.ackermann@geowerkstatt.ch>
2024-05-03 13:08:23 +0200

    Add frontend UI

---
+++ src/client/src/commons/form/borehole/borehole/geometryImport.jsx
@@ -0,0 +1,136 @@
+import { useTranslation } from "react-i18next";
+import { useContext, useEffect } from "react";
+import { FileDropzone } from "../../../../commons/files/fileDropzone";
+import { AddButton } from "../../../../components/buttons/buttons";
+import {
+  Stack,
+  Typography,
+  Card,
+  CardContent,
+  CardActions,
+  TextField,
+  IconButton,
+  InputAdornment,
+  Tooltip,
+} from "@mui/material/";
+import {
+  useBoreholeGeometry,
+  useBoreholeGeometryMutations,
+  useBoreholeGeometryFormats,
+} from "../../../../api/fetchApiV2";
+import { FormProvider, useForm, Controller, useWatch } from "react-hook-form";
+import { FormSelect } from "../../../../components/form/form";
+import { DevTool } from "@hookform/devtools";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import { AlertContext } from "../../../../components/alert/alertContext";
+
+const GeometryImport = ({ boreholeId }) => {
+  const { t } = useTranslation();
+  const { error } = useContext(AlertContext);
+
+  const {
+    set: { mutate: setBoreholeGeometry },
+  } = useBoreholeGeometryMutations();
+  const { data } = useBoreholeGeometry(boreholeId);
+  const { data: geometryFormats } = useBoreholeGeometryFormats();
+
+  const formMethods = useForm({
+    defaultValues: { geometryFormat: "", geometryFile: [] },
+  });
+
+  useEffect(() => {
+    formMethods.setValue("geometryFormat", geometryFormats?.at(0)?.key);
+  }, [geometryFormats, formMethods]);
+
+  const uploadGeometryCSV = data => {
+    let formData = new FormData();
+    formData.append("geometryFile", data.geometryFile[0]);
+    formData.append("geometryFormat", data.geometryFormat);
+    setBoreholeGeometry(
+      { boreholeId, formData },
+      {
+        onSuccess: async data => {
+          // fetch does not fail promises on 4xx or 5xx responses
+          // ¯\_(ツ)_/¯
+          if (!data.ok) {
+            data.json().then(msg => error(msg.detail ?? t("errorDuringBoreholeFileUpload")));
+          }
+        },
+      },
+    );
+  };
+
+  const watch = useWatch({ control: formMethods.control });
+  const expectedCSVHeader =
+    geometryFormats?.find(f => f.key === formMethods.getValues("geometryFormat"))?.csvHeader ?? "";
+
+  return (
+    <>
+      <Card>
+        <CardContent>
+          <DevTool control={formMethods.control} placement="top-left" />
+          <FormProvider {...formMethods}>
+            <form onSubmit={formMethods.handleSubmit(uploadGeometryCSV)}>
+              <Stack direction="row" alignItems="flex-start">
+                <Stack sx={{ width: "50%" }} spacing={2} direction="column">
+                  <FormSelect
+                    fieldName="geometryFormat"
+                    label="boreholeGeometryFormat"
+                    values={geometryFormats}
+                    required={true}
+                  />
+                  {expectedCSVHeader !== "" && (
+                    <>
+                      <Typography>{t("csvFormatExplanation")}</Typography>
+                      <TextField
+                        value={expectedCSVHeader}
+                        size="small"
+                        InputProps={{
+                          readOnly: true,
+                          endAdornment: (
+                            <InputAdornment position="end">
+                              <Tooltip title={t("copyToClipboard")}>
+                                <IconButton onClick={() => navigator.clipboard.writeText(expectedCSVHeader)}>
+                                  <ContentCopyIcon />
+                                </IconButton>
+                              </Tooltip>
+                            </InputAdornment>
+                          ),
+                        }}
+                      />
+                    </>
+                  )}
+                </Stack>
+                <Controller
+                  name="geometryFile"
+                  control={formMethods.control}
+                  render={({ field }) => (
+                    <FileDropzone
+                      onHandleFileChange={field.onChange}
+                      defaultText={"dropZoneGeometryText"}
+                      restrictAcceptedFileTypeToCsv={true}
+                      maxFilesToSelectAtOnce={1}
+                      maxFilesToUpload={1}
+                      isDisabled={false}
+                      dataCy={"import-geometry-input"}
+                    />
+                  )}
+                />
+              </Stack>
+            </form>
+          </FormProvider>
+        </CardContent>
+        <CardActions>
+          <AddButton
+            sx={{ marginLeft: "auto" }}
+            label={data?.length > 0 ? "boreholeGeometryReplace" : "boreholeGeometryImport"}
+            onClick={formMethods.handleSubmit(uploadGeometryCSV)}
+            disabled={watch?.geometryFile?.length === 0}
+          />
+        </CardActions>
+      </Card>
+    </>
+  );
+};
+
+export default GeometryImport;
---
+++ src/client/src/commons/form/borehole/borehole/geometryTable.jsx
@@ -0,0 +1,57 @@
+import { useTranslation } from "react-i18next";
+import {
+  Typography,
+  Card,
+  CardContent,
+  Table,
+  TableHead,
+  TableBody,
+  TableContainer,
+  TableRow,
+  TableCell,
+  CircularProgress,
+} from "@mui/material/";
+import { FullPageCentered } from "../../../../components/baseComponents";
+
+const GeometryTable = ({ data }) => {
+  const { t } = useTranslation();
+
+  return (
+    <Card>
+      <CardContent>
+        {!data ? (
+          <FullPageCentered>
+            <CircularProgress />
+          </FullPageCentered>
+        ) : data.length === 0 ? (
+          <Typography>{t("msgBoreholeGeometryEmpty")}</Typography>
+        ) : (
+          <TableContainer sx={{ aspectRatio: 1 }}>
+            <Table stickyHeader size="small">
+              <TableHead>
+                <TableRow>
+                  {Object.keys(data[0]).map(key => (
+                    <TableCell key={key} sx={{ fontWeight: 900 }}>
+                      {key}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data.map((element, index) => (
+                  <TableRow key={index} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                    {Object.keys(element).map(key => (
+                      <TableCell key={key}>{element[key].toFixed(3)}</TableCell>
+                    ))}
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        )}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default GeometryTable;
