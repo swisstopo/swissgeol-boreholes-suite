ec8720b0640901054e238f7eecd27376fdd1e5bf
Patrick Ackermann <patrick.ackermann@geowerkstatt.ch>
2024-05-16 11:42:15 +0200

    Refactor BoreholeGeometryController

---
+++ src/api/BoreholeGeometryFormat/Helper.cs
@@ -0,0 +1,29 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using Humanizer;
+using System.Globalization;
+
+namespace BDMS.BoreholeGeometryFormat;
+
+public static class Helper
+{
+    internal static readonly CsvConfiguration CsvConfig = new(new CultureInfo("de-CH"))
+    {
+        Delimiter = ";",
+        IgnoreReferences = true,
+        PrepareHeaderForMatch = args => args.Header.Humanize(LetterCasing.Title),
+        MissingFieldFound = null,
+    };
+
+    internal static string GetCSVHeader<T>()
+    {
+        var context = new CsvContext(CsvConfig);
+        var map = context.AutoMap<T>();
+        return string.Join("; ", map.MemberMaps.Select(m => m.Data.Names.FirstOrDefault(m.Data.Member.Name)));
+    }
+
+    internal static double ToRadians(double degrees)
+    {
+        return degrees * Math.PI / 180;
+    }
+}
---
+++ src/api/BoreholeGeometryFormat/XYZFormat.cs
@@ -0,0 +1,50 @@
+﻿using BDMS.Models;
+using CsvHelper;
+
+namespace BDMS.BoreholeGeometryFormat;
+
+/// <summary>
+/// Accepts a CSV file with XYZ values that can be used directly without conversion.
+/// </summary>
+internal sealed class XYZFormat : IBoreholeGeometryFormat
+{
+    public string Key => "XYZ";
+    public string Name => "X Y Z";
+    private Lazy<string> expectedCsvHeader = new(Helper.GetCSVHeader<Geometry>);
+    public string CsvHeader => expectedCsvHeader.Value;
+
+    public IList<BoreholeGeometryElement> ReadCsv(IFormFile file, int boreholeId)
+    {
+        using var reader = new StreamReader(file.OpenReadStream());
+        using var csv = new CsvReader(reader, Helper.CsvConfig);
+
+        var data = csv.GetRecords<Geometry>();
+        return ToBoreholeGeometry(data, boreholeId);
+    }
+
+    /// <summary>
+    /// Convert <see cref="Geometry"/> data to <see cref="BoreholeGeometryElement"/> data that can be
+    /// written to the Database.
+    /// </summary>
+    /// <param name="data">The <see cref="Geometry"/> data.</param>
+    /// <param name="boreholeId">The borehole this geometry belongs to.</param>
+    public static List<BoreholeGeometryElement> ToBoreholeGeometry(IEnumerable<Geometry> data, int boreholeId)
+    {
+        return data
+            .Select(g => new BoreholeGeometryElement
+            {
+                BoreholeId = boreholeId,
+                X = g.X,
+                Y = g.Y,
+                Z = g.Z,
+            })
+            .ToList();
+    }
+
+    internal sealed class Geometry
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+    }
+}
