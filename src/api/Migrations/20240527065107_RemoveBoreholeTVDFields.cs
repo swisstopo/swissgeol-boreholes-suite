e78d6b05e2c25bc4805e304f17e0099187554a4d
Patrick Ackermann <patrick.ackermann@geowerkstatt.ch>
2024-05-27 10:02:49 +0200

    Fix FK and Index name in migration

--- src/api/Migrations/20240527065107_RemoveBoreholeTVDFields.cs
+++ src/api/Migrations/20240527065107_RemoveBoreholeTVDFields.cs
@@ -11,12 +11,12 @@ public partial class RemoveBoreholeTVDFields : Migration
     protected override void Up(MigrationBuilder migrationBuilder)
     {
         migrationBuilder.DropForeignKey(
-            name: "FK_borehole_codelist_qt_total_depth_tvd_id_cli",
+            name: "borehole_qt_total_depth_tvd_id_cli_fkey",
             schema: "bdms",
             table: "borehole");
 
         migrationBuilder.DropIndex(
-            name: "IX_borehole_qt_total_depth_tvd_id_cli",
+            name: "IX_borehole_qt_total_depth_tvd_id_cli_fkey",
             schema: "bdms",
             table: "borehole");
 
@@ -39,52 +39,5 @@ public partial class RemoveBoreholeTVDFields : Migration
             name: "total_depth_tvd_bho",
             schema: "bdms",
             table: "borehole");
-    }
-
-    /// <inheritdoc />
-    protected override void Down(MigrationBuilder migrationBuilder)
-    {
-        migrationBuilder.AddColumn<double>(
-            name: "qt_top_bedrock_tvd",
-            schema: "bdms",
-            table: "borehole",
-            type: "double precision",
-            nullable: true);
-
-        migrationBuilder.AddColumn<int>(
-            name: "qt_total_depth_tvd_id_cli",
-            schema: "bdms",
-            table: "borehole",
-            type: "integer",
-            nullable: true);
-
-        migrationBuilder.AddColumn<double>(
-            name: "top_bedrock_tvd_bho",
-            schema: "bdms",
-            table: "borehole",
-            type: "double precision",
-            nullable: true);
-
-        migrationBuilder.AddColumn<double>(
-            name: "total_depth_tvd_bho",
-            schema: "bdms",
-            table: "borehole",
-            type: "double precision",
-            nullable: true);
-
-        migrationBuilder.CreateIndex(
-            name: "IX_borehole_qt_total_depth_tvd_id_cli",
-            schema: "bdms",
-            table: "borehole",
-            column: "qt_total_depth_tvd_id_cli");
-
-        migrationBuilder.AddForeignKey(
-            name: "FK_borehole_codelist_qt_total_depth_tvd_id_cli",
-            schema: "bdms",
-            table: "borehole",
-            column: "qt_total_depth_tvd_id_cli",
-            principalSchema: "bdms",
-            principalTable: "codelist",
-            principalColumn: "id_cli");
     }
 }
