340f94707e9160bf6c1857513966ba748a0cfe35
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2024-05-29 13:45:50 +0200

    Fix reset for all datatypes

--- src/client/src/commons/search/editor/FilterInterfaces.ts
+++ src/client/src/commons/search/editor/FilterInterfaces.ts
@@ -1,9 +1,9 @@
 export interface FilterChipsProps {
   activeFilters: Filter[];
-  setFilter: (key: string, value: string) => void;
+  setFilter: (key: string, value: string | boolean | number | null) => void;
 }
 
 export interface Filter {
   key: string;
-  value: string;
+  value: string | boolean | number | null;
 }
