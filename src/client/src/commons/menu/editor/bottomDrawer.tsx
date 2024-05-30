e514d103cc58e259c3efa093a06319de8840060e
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2024-05-30 18:09:29 +0200

    Adjust table height to small screens

--- src/client/src/commons/menu/editor/bottomDrawer.tsx
+++ src/client/src/commons/menu/editor/bottomDrawer.tsx
@@ -9,6 +9,9 @@ export const BottomDrawer = ({ drawerOpen, children }: { drawerOpen: boolean; ch
         position: "relative",
         top: 0,
         transition: "left 0.3s ease-out",
+        "@media (max-height: 1200px)": {
+          maxHeight: "60vh",
+        },
       }}>
       <Collapse in={drawerOpen} orientation="vertical">
         <Paper
@@ -17,6 +20,9 @@ export const BottomDrawer = ({ drawerOpen, children }: { drawerOpen: boolean; ch
             height: "40vh",
             boxShadow: theme.palette.boxShadow + " 2px 6px 6px 0px",
             borderRight: "1px solid rgba(0, 0, 0, 0.12)",
+            "@media (max-height: 1200px)": {
+              height: "60vh",
+            },
           }}>
           {children}
         </Paper>
