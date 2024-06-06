7462e719173b9eed6ad5c44cb1e2a2fe12103abd
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2024-06-06 13:54:48 +0200

    Remove description columns from codelists

---
+++ src/api/Migrations/20240606115318_RemoveDescriptionFromCodelists.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveDescriptionFromCodelists : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "description_cli_de",
+                schema: "bdms",
+                table: "codelist");
+
+            migrationBuilder.DropColumn(
+                name: "description_cli_en",
+                schema: "bdms",
+                table: "codelist");
+
+            migrationBuilder.DropColumn(
+                name: "description_cli_fr",
+                schema: "bdms",
+                table: "codelist");
+
+            migrationBuilder.DropColumn(
+                name: "description_cli_it",
+                schema: "bdms",
+                table: "codelist");
+
+            migrationBuilder.DropColumn(
+                name: "description_cli_ro",
+                schema: "bdms",
+                table: "codelist");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "description_cli_de",
+                schema: "bdms",
+                table: "codelist",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "description_cli_en",
+                schema: "bdms",
+                table: "codelist",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "description_cli_fr",
+                schema: "bdms",
+                table: "codelist",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "description_cli_it",
+                schema: "bdms",
+                table: "codelist",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "description_cli_ro",
+                schema: "bdms",
+                table: "codelist",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
