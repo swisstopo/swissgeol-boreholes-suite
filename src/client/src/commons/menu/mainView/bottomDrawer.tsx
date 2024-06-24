d632321ee5d89898242300b150e670ff0fc2d6bf
Patrick Ackermann <patrick.ackermann@geowerkstatt.ch>
2024-06-24 11:39:16 +0200

    Set bottom drawer height to 50% for all screens

--- src/client/src/commons/menu/mainView/bottomDrawer.tsx
+++ src/client/src/commons/menu/mainView/bottomDrawer.tsx
@@ -5,24 +5,18 @@ export const BottomDrawer = ({ drawerOpen, children }: { drawerOpen: boolean; ch
   return (
     <Box
       sx={{
-        maxHeight: "40vh",
+        maxHeight: "50vh",
         position: "relative",
         top: 0,
         transition: "left 0.3s ease-out",
-        "@media (max-height: 1200px)": {
-          maxHeight: "60vh",
-        },
       }}>
       <Collapse in={drawerOpen} orientation="vertical">
         <Paper
           sx={{
             padding: 2,
-            height: "40vh",
+            height: "50vh",
             boxShadow: theme.palette.boxShadow + " 2px 6px 6px 0px",
             borderRight: "1px solid rgba(0, 0, 0, 0.12)",
-            "@media (max-height: 1200px)": {
-              height: "60vh",
-            },
           }}>
           {children}
         </Paper>
