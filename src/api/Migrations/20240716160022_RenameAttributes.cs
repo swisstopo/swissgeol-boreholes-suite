a9796e80c096ab566250e736c4accf04778eaeef
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2024-07-16 18:34:23 +0200

    Fix migration

--- src/api/Migrations/20240716160022_RenameAttributes.cs
+++ src/api/Migrations/20240716160022_RenameAttributes.cs
@@ -11,7 +11,7 @@ public partial class RenameAttributes : Migration
     protected override void Up(MigrationBuilder migrationBuilder)
     {
         migrationBuilder.DropForeignKey(
-            name: "FK_borehole_codelist_kind_id_cli",
+            name: "borehole_kind_id_cli_fkey",
             schema: "bdms",
             table: "borehole");
 
@@ -34,58 +34,16 @@ public partial class RenameAttributes : Migration
             newName: "top_bedrock_fresh_md");
 
         migrationBuilder.RenameIndex(
-            name: "IX_borehole_kind_id_cli",
+            name: "IX_borehole_kind_id_cli_fkey",
             schema: "bdms",
             table: "borehole",
-            newName: "IX_borehole_borehole_type_id");
+            newName: "IX_borehole_borehole_type_id_fkey");
 
         migrationBuilder.AddForeignKey(
-            name: "FK_borehole_codelist_borehole_type_id",
+            name: "borehole_borehole_type_id_fkey",
             schema: "bdms",
             table: "borehole",
             column: "borehole_type_id",
-            principalSchema: "bdms",
-            principalTable: "codelist",
-            principalColumn: "id_cli");
-    }
-
-    /// <inheritdoc />
-    protected override void Down(MigrationBuilder migrationBuilder)
-    {
-        migrationBuilder.DropForeignKey(
-            name: "FK_borehole_codelist_borehole_type_id",
-            schema: "bdms",
-            table: "borehole");
-
-        migrationBuilder.RenameColumn(
-            name: "borehole_type_id",
-            schema: "bdms",
-            table: "borehole",
-            newName: "kind_id_cli");
-
-        migrationBuilder.RenameColumn(
-            name: "top_bedrock_weathered_md",
-            schema: "bdms",
-            table: "borehole",
-            newName: "top_bedrock_bho");
-
-        migrationBuilder.RenameColumn(
-            name: "top_bedrock_fresh_md",
-            schema: "bdms",
-            table: "borehole",
-            newName: "qt_top_bedrock");
-
-        migrationBuilder.RenameIndex(
-            name: "IX_borehole_borehole_type_id",
-            schema: "bdms",
-            table: "borehole",
-            newName: "IX_borehole_kind_id_cli");
-
-        migrationBuilder.AddForeignKey(
-            name: "FK_borehole_codelist_kind_id_cli",
-            schema: "bdms",
-            table: "borehole",
-            column: "kind_id_cli",
             principalSchema: "bdms",
             principalTable: "codelist",
             principalColumn: "id_cli");
