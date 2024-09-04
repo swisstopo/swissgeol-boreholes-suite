384c59b6bfbd54ec01d3a53e8c0262d98b0078d7
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2024-09-04 11:03:57 +0200

    Save panel position in cache

--- src/client/src/pages/detail/labeling/labelingContext.tsx
+++ src/client/src/pages/detail/labeling/labelingContext.tsx
@@ -1,5 +1,5 @@
 import { LabelingContextInterface, PanelPosition } from "./labelingInterfaces.tsx";
-import { createContext, FC, PropsWithChildren, useState } from "react";
+import { createContext, FC, PropsWithChildren, useEffect, useLayoutEffect, useState } from "react";
 
 export const LabelingContext = createContext<LabelingContextInterface>({
   panelPosition: "right",
@@ -11,6 +11,17 @@ export const LabelingContext = createContext<LabelingContextInterface>({
 export const LabelingProvider: FC<PropsWithChildren> = ({ children }) => {
   const [panelPosition, setPanelPosition] = useState<PanelPosition>("right");
   const [panelOpen, setPanelOpen] = useState(false);
+
+  useLayoutEffect(() => {
+    const storedPosition = localStorage.getItem("panelPosition") as PanelPosition;
+    if (storedPosition) {
+      setPanelPosition(storedPosition);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("panelPosition", panelPosition);
+  }, [panelPosition]);
 
   const togglePanel = () => {
     setPanelOpen(!panelOpen);
