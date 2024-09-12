ba23bc2902b6dd7b1ec3fa9efde209b5247dc6d4
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2024-09-12 11:12:50 +0200

    Move elevation inputs to separate segment

--- src/client/src/components/map/pointComponent.jsx
+++ src/client/src/components/map/pointComponent.jsx
@@ -309,11 +309,11 @@ class PointComponent extends React.Component {
           style={{
             padding: "0px",
             flex: "1 1 100%",
-            height: 450,
+            height: 560,
           }}
         />
         <ZoomControls onZoomIn={this.onZoomIn} onZoomOut={this.onZoomOut} onFitToExtent={this.onFitToExtent} />
-        <Box sx={{ position: "absolute", right: "0px", top: "425px" }}>
+        <Box sx={{ position: "absolute", right: 0, top: 535 }}>
           <BasemapSelector marginBottom="0px" />
         </Box>
         <Box
---
+++ src/client/src/pages/detail/form/location/elevationSegment.tsx
@@ -0,0 +1,131 @@
+import { FC } from "react";
+import { Form, Segment } from "semantic-ui-react";
+import { NumericFormat } from "react-number-format";
+import { useTranslation } from "react-i18next";
+import DomainDropdown from "../../../../components/legacyComponents/domain/dropdown/domainDropdown.jsx";
+import DomainText from "../../../../components/legacyComponents/domain/domainText.jsx";
+import { parseFloatWithThousandsSeparator } from "../../../../components/legacyComponents/formUtils.js";
+import { Borehole, User } from "../../../../api-lib/ReduxStateInterfaces.ts";
+
+interface ElevationSegmentProps {
+  size: string;
+  borehole: Borehole;
+  user: User;
+  updateChange: (
+    fieldName: keyof Borehole["data"] | "location",
+    value: string | number | null | (number | string | null)[],
+    to?: boolean,
+  ) => void;
+  updateNumber: (fieldName: keyof Borehole["data"], value: number | null) => void;
+}
+
+const ElevationSegment: FC<ElevationSegmentProps> = ({ size, borehole, user, updateChange, updateNumber }) => {
+  const { t } = useTranslation();
+
+  // --- Derived states ---
+  const isEditable: boolean =
+    borehole?.data.role === "EDIT" && borehole?.data.lock !== null && borehole?.data.lock?.id === user?.data.id;
+
+  return (
+    <Segment>
+      <Form size={size}>
+        <Form.Group widths="equal">
+          <Form.Field error={borehole.data.elevation_z == null} required>
+            <label>{t("elevation_z")}</label>
+            <NumericFormat
+              type="text"
+              autoCapitalize="off"
+              autoComplete="off"
+              autoCorrect="off"
+              onChange={e => {
+                updateNumber(
+                  "elevation_z",
+                  e.target.value === "" ? null : parseFloatWithThousandsSeparator(e.target.value),
+                );
+              }}
+              fixedDecimalScale
+              spellCheck="false"
+              value={borehole.data.elevation_z ?? ""}
+              thousandSeparator="'"
+              readOnly={!isEditable}
+            />
+          </Form.Field>
+          <Form.Field required>
+            <label>{t("elevation_precision")}</label>
+            <DomainDropdown
+              onSelected={(selected: { id: string }) => {
+                updateChange("elevation_precision", selected.id, false);
+              }}
+              schema="elevation_precision"
+              selected={borehole.data.elevation_precision}
+              readOnly={!isEditable}
+            />
+          </Form.Field>
+        </Form.Group>
+        <Form.Group widths="equal">
+          <Form.Field error={borehole.data.reference_elevation == null} required>
+            <label>{t("reference_elevation")}</label>
+            <NumericFormat
+              autoCapitalize="off"
+              type="text"
+              autoComplete="off"
+              autoCorrect="off"
+              onChange={e => {
+                updateNumber(
+                  "reference_elevation",
+                  e.target.value === "" ? null : parseFloatWithThousandsSeparator(e.target.value),
+                );
+                if (/^-?\d*[.,]?\d*$/.test(e.target.value)) {
+                  updateChange("reference_elevation", e.target.value === "" ? null : parseFloat(e.target.value));
+                }
+              }}
+              spellCheck="false"
+              value={borehole.data.reference_elevation ?? ""}
+              thousandSeparator="'"
+              readOnly={!isEditable}
+            />
+          </Form.Field>
+          <Form.Field required>
+            <label>{t("reference_elevation_qt")}</label>
+            <DomainDropdown
+              onSelected={(selected: { id: string | number }) => {
+                updateChange("qt_reference_elevation", selected.id, false);
+              }}
+              schema="elevation_precision"
+              selected={borehole.data.qt_reference_elevation}
+              readOnly={!isEditable}
+            />
+          </Form.Field>
+        </Form.Group>
+        <Form.Group widths="equal">
+          <Form.Field error={borehole.data.reference_elevation_type === null} required>
+            <label>{t("reference_elevation_type")}</label>
+            <DomainDropdown
+              onSelected={(selected: { id: string }) => {
+                updateChange("reference_elevation_type", selected.id, false);
+              }}
+              schema="reference_elevation_type"
+              selected={borehole.data.reference_elevation_type}
+              readOnly={!isEditable}
+            />
+          </Form.Field>
+          <Form.Field required>
+            <label>{t("height_reference_system")}</label>
+            <div
+              style={{
+                height: "36px",
+                display: "flex",
+                alignItems: "center",
+              }}>
+              <div>
+                <DomainText id={borehole.data.height_reference_system} schema="height_reference_system" />
+              </div>
+            </div>
+          </Form.Field>
+        </Form.Group>
+      </Form>
+    </Segment>
+  );
+};
+
+export default ElevationSegment;
