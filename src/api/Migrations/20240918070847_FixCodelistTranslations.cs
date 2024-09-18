54f91b43358106195a1b2526ab05dfd1b9a936e8
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2024-09-18 09:16:58 +0200

    Fix translations

---
+++ src/api/Migrations/20240918070847_FixCodelistTranslations.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDMS.Migrations;
+
+/// <inheritdoc />
+public partial class FixCodelistTranslations : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(@"
+            UPDATE bdms.codelist
+            SET text_cli_en = 'Tonpellets'
+            WHERE id_cli = 25000309;
+            ");
+    }
+}
