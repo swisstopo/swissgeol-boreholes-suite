e3b9fb11072dcd849c3299caac586ff3ab56959d
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2024-09-26 16:55:04 +0200

    Fix design of cantonMunicipalitySegment.jsx

--- src/client/src/pages/detail/form/location/cantonMunicipalitySegment.jsx
+++ src/client/src/pages/detail/form/location/cantonMunicipalitySegment.jsx
@@ -1,41 +1,85 @@
-import { useTranslation } from "react-i18next";
-import { Form, Icon, Input } from "semantic-ui-react";
+import { useEffect } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { InputAdornment } from "@mui/material";
+import { Card, CardContent } from "@mui/material/";
+import { MapPin } from "lucide-react";
+import { FormContainer, FormValueType } from "../../../../components/form/form";
+import { FormInput } from "../../../../components/form/formInput";
 import { FormSegmentBox } from "../../../../components/styledComponents";
 
 const CantonMunicipalitySegment = props => {
   const { country, canton, municipality, isEditable } = props;
-  const { t } = useTranslation();
+
+  const formMethods = useForm({
+    mode: "all",
+  });
+
+  useEffect(() => {
+    formMethods.setValue("country", country);
+  }, [formMethods, country]);
+
+  useEffect(() => {
+    formMethods.setValue("canton", canton);
+  }, [formMethods, canton]);
+
+  useEffect(() => {
+    formMethods.setValue("city", municipality);
+  }, [formMethods, municipality]);
 
   return (
     <FormSegmentBox>
-      <Form autoCapitalize="off" autoComplete="off" autoCorrect="off" spellCheck="false">
-        <Form.Group widths="equal">
-          <Form.Field>
-            <label>
-              {t("country")}
-              &nbsp;
-              <Icon name="map marker" />
-            </label>
-            <Input data-cy="country" value={country ?? ""} readOnly={!isEditable} />
-          </Form.Field>
-          <Form.Field>
-            <label>
-              {t("canton")}
-              &nbsp;
-              <Icon name="map marker" />
-            </label>
-            <Input data-cy="canton" value={canton ?? ""} readOnly={!isEditable} />
-          </Form.Field>
-          <Form.Field>
-            <label>
-              {t("city")}
-              &nbsp;
-              <Icon name="map marker" />
-            </label>
-            <Input data-cy="municipality" value={municipality ?? ""} readOnly={!isEditable} />
-          </Form.Field>
-        </Form.Group>
-      </Form>
+      <FormProvider {...formMethods}>
+        <Card>
+          <CardContent sx={{ pt: 4, px: 3 }}>
+            <FormContainer direction="row">
+              <FormInput
+                fieldName={"country"}
+                label="country"
+                type={FormValueType.Text}
+                value={country ?? ""}
+                inputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <MapPin />
+                    </InputAdornment>
+                  ),
+                }}
+                readonly={!isEditable}
+              />
+
+              <FormInput
+                fieldName={"canton"}
+                label="canton"
+                type={FormValueType.Text}
+                value={canton ?? ""}
+                inputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <MapPin />
+                    </InputAdornment>
+                  ),
+                }}
+                readonly={!isEditable}
+              />
+
+              <FormInput
+                fieldName={"city"}
+                label="city"
+                type={FormValueType.Text}
+                value={municipality ?? ""}
+                inputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <MapPin />
+                    </InputAdornment>
+                  ),
+                }}
+                readonly={!isEditable}
+              />
+            </FormContainer>
+          </CardContent>
+        </Card>
+      </FormProvider>
     </FormSegmentBox>
   );
 };
