3059da046dfee37fc8a0edf9e98f8639ece1ddc1
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2024-09-30 16:07:29 +0200

    Add new style for canton municipality segment

--- src/client/src/pages/detail/form/location/cantonMunicipalitySegment.jsx
+++ src/client/src/pages/detail/form/location/cantonMunicipalitySegment.jsx
@@ -5,7 +5,6 @@ import { Card, CardContent } from "@mui/material/";
 import { MapPin } from "lucide-react";
 import { FormContainer, FormValueType } from "../../../../components/form/form";
 import { FormInput } from "../../../../components/form/formInput";
-import { FormSegmentBox } from "../../../../components/styledComponents";
 
 const CantonMunicipalitySegment = props => {
   const { country, canton, municipality, isEditable } = props;
@@ -27,60 +26,58 @@ const CantonMunicipalitySegment = props => {
   }, [formMethods, municipality]);
 
   return (
-    <FormSegmentBox>
-      <FormProvider {...formMethods}>
-        <Card>
-          <CardContent sx={{ pt: 4, px: 3 }}>
-            <FormContainer direction="row">
-              <FormInput
-                fieldName={"country"}
-                label="country"
-                type={FormValueType.Text}
-                value={country ?? ""}
-                inputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <MapPin />
-                    </InputAdornment>
-                  ),
-                }}
-                readonly={!isEditable}
-              />
+    <FormProvider {...formMethods}>
+      <Card>
+        <CardContent sx={{ pt: 4, px: 3 }}>
+          <FormContainer direction="row">
+            <FormInput
+              fieldName={"country"}
+              label="country"
+              type={FormValueType.Text}
+              value={country ?? ""}
+              inputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <MapPin />
+                  </InputAdornment>
+                ),
+              }}
+              readonly={!isEditable}
+            />
 
-              <FormInput
-                fieldName={"canton"}
-                label="canton"
-                type={FormValueType.Text}
-                value={canton ?? ""}
-                inputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <MapPin />
-                    </InputAdornment>
-                  ),
-                }}
-                readonly={!isEditable}
-              />
+            <FormInput
+              fieldName={"canton"}
+              label="canton"
+              type={FormValueType.Text}
+              value={canton ?? ""}
+              inputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <MapPin />
+                  </InputAdornment>
+                ),
+              }}
+              readonly={!isEditable}
+            />
 
-              <FormInput
-                fieldName={"city"}
-                label="city"
-                type={FormValueType.Text}
-                value={municipality ?? ""}
-                inputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <MapPin />
-                    </InputAdornment>
-                  ),
-                }}
-                readonly={!isEditable}
-              />
-            </FormContainer>
-          </CardContent>
-        </Card>
-      </FormProvider>
-    </FormSegmentBox>
+            <FormInput
+              fieldName={"city"}
+              label="city"
+              type={FormValueType.Text}
+              value={municipality ?? ""}
+              inputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <MapPin />
+                  </InputAdornment>
+                ),
+              }}
+              readonly={!isEditable}
+            />
+          </FormContainer>
+        </CardContent>
+      </Card>
+    </FormProvider>
   );
 };
 
