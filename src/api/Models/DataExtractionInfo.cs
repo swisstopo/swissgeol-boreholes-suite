8cb0123d96a926e3555e52877b909e469e94c604
danjov <daniel.jovanovic@geowerkstatt.ch>
2024-10-08 15:02:44 +0200

    Add documentation

--- src/api/Models/DataExtractionInfo.cs
+++ src/api/Models/DataExtractionInfo.cs
@@ -5,8 +5,23 @@
 /// </summary>
 public class DataExtractionInfo
 {
+    /// <summary>
+    /// The name of the file.
+    /// </summary>
     public string FileName { get; set; }
+
+    /// <summary>
+    /// The width of the file.
+    /// </summary>
     public int Width { get; set; }
+
+    /// <summary>
+    /// The height of the file.
+    /// </summary>
     public int Height { get; set; }
+
+    /// <summary>
+    /// The number of data extraction images for this file.
+    /// </summary>
     public int Count { get; set; }
 }
