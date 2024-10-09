88806f23a3732df43f665e42f846f603f4286e65
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2024-10-09 11:17:13 +0200

    Add hint to translate error message

--- src/client/src/pages/detail/labeling/labelingPanel.tsx
+++ src/client/src/pages/detail/labeling/labelingPanel.tsx
@@ -160,6 +160,7 @@ const LabelingPanel: FC<LabelingPanelProps> = ({ boreholeId }) => {
               setExtractionState(ExtractionState.error);
               // TODO: https://github.com/swisstopo/swissgeol-boreholes-suite/issues/1546
               //  Check if error message is correct, resp. handle all error cases with different messages
+              //   Translate error message
               showAlert(t(error.message), "error");
             }
           })
