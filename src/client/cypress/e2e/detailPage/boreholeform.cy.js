6b1b00fb2da3e4434bb40559949e9f5371200062
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2024-10-25 22:30:55 +0200

    Fix national interest evaluation

--- src/client/cypress/e2e/detailPage/boreholeform.cy.js
+++ src/client/cypress/e2e/detailPage/boreholeform.cy.js
@@ -54,7 +54,7 @@ describe("Test for the borehole form.", () => {
     evaluateInput("alternateName", "Zena Rath");
     evaluateInput("projectName", "Reactive asymmetric alliance");
     evaluateSelect("restrictionId", "");
-    evaluateSelect("nationalInterest", ""); // not set
+    cy.get(`[data-cy="nationalInterest-formSelect"] input`).should("have.attr", "value", "false");
     // evaluateSelect("spatial_reference_system", "20104002"); // LV03
     // evaluateSelect("location_precision", "20113005");
 
