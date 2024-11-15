08f683c82857200adab3a806aba866c7c51786fd
Oliver Gut <oliver.gut@geowerkstatt.ch>
2024-11-15 10:26:00 +0100

    Add additional sync tasks for testing purposes
    to verify and test the CI/CD pipelines

---
+++ src/extern-sync/Tasks/MigrateDatabase.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BDMS.ExternSync.Tasks;
+
+/// <summary>
+/// Checks whether the source and target databases have the same schema version.
+/// </summary>
+/// <remarks>
+/// IMPORTANT! This class does not yet implement the actual behavior. It only
+/// contains sample code to verify the testing and integration concepts.
+/// </remarks>
+public class MigrateDatabase(ISyncContext syncContext, ILogger<MigrateDatabase> logger) : SyncTask(syncContext, logger)
+{
+    /// <inheritdoc/>
+    protected override async Task RunTaskAsync(CancellationToken cancellationToken)
+    {
+        // Check the target database schema version and migrate if necessary
+        var targetDbSchemaVersion = await Target.GetDbSchemaVersionAsync(cancellationToken).ConfigureAwait(false);
+        if (targetDbSchemaVersion.IsNullOrEmpty())
+        {
+            Logger.LogInformation("The target database hat not been migrated yet.\nInitializing migration...");
+            await Target.Database.MigrateAsync(cancellationToken).ConfigureAwait(false);
+
+            Logger.LogInformation("Clean-up superfluous data...");
+            await Target.CleanUpSuperfluousDataAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        // Log the source and target database schema versions
+        Logger.LogInformation(
+            "Source database schema version: {SourceDatabase}\nTarget database schema version: {TargetDatabase}",
+            await Source.GetDbSchemaVersionAsync(cancellationToken).ConfigureAwait(false),
+            await Target.GetDbSchemaVersionAsync(cancellationToken).ConfigureAwait(false));
+    }
+
+    /// <inheritdoc/>
+    protected override async Task ValidateTaskAsync(CancellationToken cancellationToken)
+    {
+        var sourceDbSchemaVersion = await Source.GetDbSchemaVersionAsync(cancellationToken).ConfigureAwait(false);
+        var targetDbSchemaVersion = await Target.GetDbSchemaVersionAsync(cancellationToken).ConfigureAwait(false);
+        if (sourceDbSchemaVersion != targetDbSchemaVersion)
+        {
+            throw new InvalidOperationException(
+                $"Source and target databases have different schema versions\n" +
+                $"Source: <{sourceDbSchemaVersion}>, Target: <{targetDbSchemaVersion}>");
+        }
+    }
+}
---
+++ src/extern-sync/Tasks/UpdateSequences.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Logging;
+using Npgsql;
+using static BDMS.ExternSync.SyncContextHelpers;
+
+namespace BDMS.ExternSync.Tasks;
+
+/// <summary>
+/// Sets the sequences in the target database.
+/// </summary>
+/// <remarks>
+/// IMPORTANT! This class does not yet implement the actual behavior. It only
+/// contains sample code to verify the testing and integration concepts.
+/// </remarks>
+public class UpdateSequences(ISyncContext syncContext, ILogger<UpdateSequences> logger) : SyncTask(syncContext, logger)
+{
+    private const int minValue = 20000;
+    private const string sequenceName = "users_id_usr_seq";
+    private const string getSequenceLastValueQuery = $"SELECT last_value FROM {BoreholesDatabaseSchemaName}.{sequenceName};";
+    private const string setSequenceValueQuery = $"SELECT setval('{BoreholesDatabaseSchemaName}.{sequenceName}', ($1));";
+
+    /// <inheritdoc/>
+    protected override async Task RunTaskAsync(CancellationToken cancellationToken)
+    {
+        var targetDbConnection = await Target.GetDbConnectionAsync(cancellationToken).ConfigureAwait(false);
+        using var selectCommand = new NpgsqlCommand(getSequenceLastValueQuery, targetDbConnection);
+
+        var lastValue = await selectCommand.ExecuteScalarAsync(cancellationToken).ConfigureAwait(false) as long? ?? -1;
+        Logger.LogInformation("{SchemaName}.{SequenceName} last_value: <{LastValue}>", BoreholesDatabaseSchemaName, sequenceName, lastValue);
+
+        if (lastValue == -1)
+        {
+            Logger.LogError("Error while reading sequence {SchemaName}.{SequenceName}.", BoreholesDatabaseSchemaName, sequenceName);
+        }
+        else if (lastValue < minValue)
+        {
+            using var alterCommand = new NpgsqlCommand(setSequenceValueQuery, targetDbConnection);
+            alterCommand.Parameters.AddWithValue(minValue);
+
+            await alterCommand.ExecuteScalarAsync(cancellationToken).ConfigureAwait(false);
+
+            Logger.LogInformation("Sequence {SchemaName}.{SequenceName} has been set to {MinValue}.", BoreholesDatabaseSchemaName, sequenceName, minValue);
+        }
+        else
+        {
+            Logger.LogInformation("Sequence for {SchemaName}.{SequenceName} has already been set.", BoreholesDatabaseSchemaName, sequenceName);
+        }
+    }
+
+    /// <inheritdoc/>
+    protected override async Task ValidateTaskAsync(CancellationToken cancellationToken) =>
+        await Task.FromResult(true).ConfigureAwait(false);
+}
