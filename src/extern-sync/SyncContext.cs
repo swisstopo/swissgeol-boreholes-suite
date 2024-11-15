fa1a933c18fa6af1b5e64146712b8bc2d64ce276
Oliver Gut <oliver.gut@geowerkstatt.ch>
2024-11-15 10:19:19 +0100

    Add ExternSync (base infrastructure)
    - Adds a task manager which executes a given list of tasks in sequence.
    - Adds a sync base class to execute and validate tasks.
    - Adds a sync context containing a source and a target database context.

---
+++ src/extern-sync/SyncContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Data.Common;
+using static BDMS.ExternSync.SyncContextHelpers;
+
+namespace BDMS.ExternSync;
+
+public class SyncContext(
+    [FromKeyedServices(SourceBdmsContextName)] DbConnection sourceDbConnection,
+    [FromKeyedServices(TargetBdmsContextName)] DbConnection targetDbConnection)
+    : ISyncContext, IDisposable
+{
+    private bool disposedValue;
+
+    /// <inheritdoc/>
+    public BdmsContext Source { get; } = new BdmsContext(GetDbContextOptions(sourceDbConnection));
+
+    /// <inheritdoc/>
+    public BdmsContext Target { get; } = new BdmsContext(GetDbContextOptions(targetDbConnection));
+
+    /// <inheritdoc/>
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue && disposing)
+        {
+            Source.Dispose();
+            Target.Dispose();
+
+            disposedValue=true;
+        }
+    }
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
---
+++ src/extern-sync/SyncTask.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace BDMS.ExternSync;
+
+/// <summary>
+/// Represents a sync task containing source and target database contexts
+/// that can be executed and validated.
+/// </summary>
+/// <param name="syncContext">The sync context.</param>
+/// <param name="logger">The logger for this instance.</param>
+public abstract class SyncTask(ISyncContext syncContext, ILogger<SyncTask> logger)
+    : IDisposable, ISyncTask
+{
+    private bool disposedValue;
+
+    /// <summary>
+    /// The source database context.
+    /// </summary>
+    protected BdmsContext Source { get; } = syncContext.Source;
+
+    /// <summary>
+    /// The target database context.
+    /// </summary>
+    protected BdmsContext Target { get; } = syncContext.Target;
+
+    /// <summary>
+    /// The logger for the <see cref="SyncTask"/>.
+    /// </summary>
+    protected ILogger<SyncTask> Logger { get; } = logger;
+
+    /// <inheritdoc/>
+    public async Task ExecuteAndValidateAsync(CancellationToken cancellationToken)
+    {
+        await RunTaskAsync(cancellationToken).ConfigureAwait(false);
+        await ValidateTaskAsync(cancellationToken).ConfigureAwait(false);
+    }
+
+    /// <summary>
+    /// Runs the <see cref="SyncTask"/>.
+    /// </summary>
+    protected abstract Task RunTaskAsync(CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Validates the result after the <see cref="SyncTask"/> has been executed.
+    /// </summary>
+    protected abstract Task ValidateTaskAsync(CancellationToken cancellationToken);
+
+    /// <inheritdoc/>
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue && disposing)
+        {
+            Source.Dispose();
+            Target.Dispose();
+
+            disposedValue=true;
+        }
+    }
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
