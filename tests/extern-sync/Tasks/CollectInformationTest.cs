55bb439fd1db43d03f1715776b1b9ba639ddeb93
Oliver Gut <oliver.gut@geowerkstatt.ch>
2024-11-15 10:32:22 +0100

    Add ExternSync tests (base infrastructure)
    - Adds some unit tests for the sync task manager.
    - Adds a test sync context with in-memory or real database contexts.

---
+++ tests/extern-sync/Tasks/CollectInformationTest.cs
@@ -0,0 +1,18 @@
+﻿using BDMS.ExternSync.Tasks;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace BDMS.ExternSync.Test;
+
+[TestClass]
+public class CollectInformationTest
+{
+    [TestMethod]
+    public async Task CollectInformation()
+    {
+        using var syncContext = await SyncContext.BuildAsync().ConfigureAwait(false);
+        using var syncTask = new CollectInformation(syncContext, new Mock<ILogger<CollectInformation>>().Object);
+
+        await syncTask.ExecuteAndValidateAsync(Mock.Of<CancellationTokenSource>().Token);
+    }
+}
---
+++ tests/extern-sync/Tasks/SynchronizeUsersTest.cs
@@ -0,0 +1,30 @@
+﻿using BDMS.ExternSync.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace BDMS.ExternSync.Test;
+
+[TestClass]
+public class SynchronizeUsersTest
+{
+    [TestMethod]
+    public async Task SynchronizeUsers()
+    {
+        using var syncContext = await SyncContext.BuildAsync(useInMemory: true).ConfigureAwait(false);
+        using var syncTask = new SynchronizeUsers(syncContext, Mock.Of<ILogger<SynchronizeUsers>>());
+
+        await syncContext.SeedUserTestDataAsync().ConfigureAwait(false);
+        var (source, target) = (syncContext.Source, syncContext.Target);
+
+        Assert.AreNotEqual(source.Users.CountAsync().Result, target.Users.CountAsync().Result);
+        Assert.AreEqual(4, await source.Users.CountAsync());
+        Assert.AreEqual(1, await target.Users.CountAsync());
+
+        await syncTask.ExecuteAndValidateAsync(Mock.Of<CancellationTokenSource>().Token);
+
+        Assert.AreEqual(source.Users.CountAsync().Result, target.Users.CountAsync().Result);
+        Assert.AreEqual(4, await source.Users.CountAsync());
+        Assert.AreEqual(4, await target.Users.CountAsync());
+    }
+}
