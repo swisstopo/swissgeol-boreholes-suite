aa5abb45d74eb91728a280241f95b41a2be33ecd
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2024-11-28 09:31:24 +0100

    Remove depth entries from all groundwater observations

---
+++ src/api/Migrations/20241128080655_RemoveGroundwaterDepthEntries.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDMS.Migrations;
+
+/// <inheritdoc />
+public partial class RemoveGroundwaterDepthEntries : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(@"
+            UPDATE bdms.observation
+	            SET from_depth_m=NULL, to_depth_m=NULL, from_depth_masl=NULL, to_depth_masl=NULL
+	            WHERE observation_type = 2;");
+    }
+}
