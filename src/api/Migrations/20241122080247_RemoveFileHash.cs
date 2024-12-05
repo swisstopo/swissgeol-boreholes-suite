3a1d1d2d46a00749584a1560a98988c02f14546e
Oliver Gut <oliver.gut@geowerkstatt.ch>
2024-12-05 14:36:41 +0100

    Remove obsolete file hash

---
+++ src/api/Migrations/20241122080247_RemoveFileHash.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BDMS.Migrations;
+
+/// <inheritdoc />
+public partial class RemoveFileHash : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "hash_fil",
+            schema: "bdms",
+            table: "files");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<string>(
+            name: "hash_fil",
+            schema: "bdms",
+            table: "files",
+            type: "text",
+            nullable: false,
+            defaultValue: "");
+    }
+}
