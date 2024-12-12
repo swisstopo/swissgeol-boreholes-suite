25ec7b68e5cf4466da92cf78731f13faa29c2a7a
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2024-12-12 15:07:25 +0100

    Add missing rules to seed

--- src/api/BdmsContextExtensions.cs
+++ src/api/BdmsContextExtensions.cs
@@ -198,6 +198,9 @@ public static class BdmsContextExtensions
            .RuleFor(o => o.PrecisionLocationY, f => f.PickRandom(Enumerable.Range(0, 10)))
            .RuleFor(o => o.PrecisionLocationXLV03, f => f.PickRandom(Enumerable.Range(0, 10)))
            .RuleFor(o => o.PrecisionLocationYLV03, f => f.PickRandom(Enumerable.Range(0, 10)))
+           .RuleFor(o => o.TotalDepthTvd, _ => null)
+           .RuleFor(o => o.TopBedrockFreshTvd, _ => null)
+           .RuleFor(o => o.TopBedrockWeatheredTvd, _ => null)
            .FinishWith((f, o) => { o.Name = o.OriginalName; });
 
         Borehole SeededBoreholes(int seed) => fakeBoreholes.UseSeed(seed).Generate();
