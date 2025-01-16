6df56219bd30da6a7fb25fea60cc66ae4c4cb8f2
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2025-01-16 16:28:39 +0100

    Add data migration

---
+++ src/api/Migrations/20250116151315_DataMigrationTopBedrockIntersected.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDMS.Migrations;
+
+/// <inheritdoc />
+public partial class DataMigrationTopBedrockIntersected : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(@"
+                UPDATE bdms.borehole SET top_bedrock_intersected = true WHERE top_bedrock_fresh_md is not null OR top_bedrock_weathered_md is not null;
+                UPDATE bdms.borehole SET top_bedrock_intersected = false WHERE lithology_top_bedrock_id_cli = 15104769;");
+    }
+}
