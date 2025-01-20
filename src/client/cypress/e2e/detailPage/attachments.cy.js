0ae84e9fd9da275cdfa34a73b3f45b6460aabaef
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2025-01-20 15:22:31 +0100

    Add test

--- src/client/cypress/e2e/detailPage/attachments.cy.js
+++ src/client/cypress/e2e/detailPage/attachments.cy.js
@@ -1,7 +1,10 @@
+import { saveWithSaveBar } from "../helpers/buttonHelpers.js";
+import { evaluateInput, setInput } from "../helpers/formHelpers.js";
 import {
   createBorehole,
   deleteBorehole,
   deleteDownloadedFile,
+  getElementByDataCy,
   loginAsAdmin,
   readDownloadedFile,
   startBoreholeEditing,
@@ -9,29 +12,30 @@ import {
 } from "../helpers/testHelpers";
 
 describe("Tests for 'Attachments' edit page.", () => {
+  const uploadLoudSpatulaFile = () => {
+    cy.get("input[type=file]").selectFile(
+      {
+        contents: Cypress.Buffer.from(Math.random().toString()),
+        fileName: "LOUDSPATULA.txt",
+        mimeType: "text/plain",
+      },
+      { force: true },
+    );
+
+    // // upload file
+    getElementByDataCy("attachments-upload-button").should("be.visible").click();
+    cy.wait(["@upload-files", "@getAllAttachments"]);
+  };
+
   it("creates, downloads and deletes attachments.", () => {
     createBorehole({ "extended.original_name": "JUNIORSOUFFLE" }).as("borehole_id");
     cy.get("@borehole_id").then(boreholeId => {
       loginAsAdmin(`/${boreholeId}`);
 
       startBoreholeEditing();
-
       // navigate to attachments tab
-      cy.get('[data-cy="attachments-menu-item"]').click();
-
-      // create file "LOUDSPATULA.pdf" for input
-      cy.get("input[type=file]").selectFile(
-        {
-          contents: Cypress.Buffer.from(Math.random().toString()),
-          fileName: "LOUDSPATULA.txt",
-          mimeType: "text/plain",
-        },
-        { force: true },
-      );
-
-      // // upload file
-      cy.get('[data-cy="attachments-upload-button"]').should("be.visible").click();
-      cy.wait(["@upload-files", "@getAllAttachments"]);
+      getElementByDataCy("attachments-menu-item").click();
+      uploadLoudSpatulaFile();
 
       // check list of attachments
       cy.get("tbody").children().should("have.length", 1);
@@ -49,7 +53,7 @@ describe("Tests for 'Attachments' edit page.", () => {
       );
 
       // upload and verify file IRATETRINITY.pdf
-      cy.get('[data-cy="attachments-upload-button"]').should("be.visible").click();
+      getElementByDataCy("attachments-upload-button").should("be.visible").click();
       cy.wait(["@upload-files", "@getAllAttachments"]);
       cy.get("tbody").children().should("have.length", 2);
       cy.get("tbody").children().contains("td", "text/plain");
@@ -64,7 +68,7 @@ describe("Tests for 'Attachments' edit page.", () => {
         },
         { force: true },
       );
-      cy.get('[data-cy="attachments-upload-button"]').should("be.visible").click();
+      getElementByDataCy("attachments-upload-button").should("be.visible").click();
       cy.wait(["@upload-files", "@getAllAttachments"]);
       cy.get("tbody").children().should("have.length", 3);
       cy.get("tbody").children().contains("td", "text/plain");
@@ -81,7 +85,7 @@ describe("Tests for 'Attachments' edit page.", () => {
       readDownloadedFile("IRATETRINITY_2.pdf");
 
       // delete attachments
-      cy.get('[data-cy="attachments-detach-button"]').children().first().click();
+      getElementByDataCy("attachments-detach-button").children().first().click();
       cy.wait(["@delete-file", "@getAllAttachments"]);
       cy.get("tbody").children().should("have.length", 2);
       cy.get('[data-cy="attachments-detach-button"]').children().first().click();
@@ -97,5 +101,29 @@ describe("Tests for 'Attachments' edit page.", () => {
       // reset test data
       deleteBorehole(boreholeId);
     });
+  });
+
+  it("can save changes on a  borehole with attachments.", () => {
+    createBorehole({ "extended.original_name": "AAA_COBRA" }).as("borehole_id");
+    cy.get("@borehole_id").then(boreholeId => {
+      loginAsAdmin(`/${boreholeId}`);
+      startBoreholeEditing();
+    });
+    getElementByDataCy("attachments-menu-item").click();
+    uploadLoudSpatulaFile();
+    cy.get("tbody").children().should("have.length", 1);
+    getElementByDataCy("borehole-menu-item").click();
+
+    // change something and save
+    setInput("totalDepth", 465);
+    saveWithSaveBar();
+
+    // navigate back to attachments and return
+    getElementByDataCy("attachments-menu-item").click();
+    cy.get("tbody").children().should("have.length", 1);
+
+    getElementByDataCy("borehole-menu-item").click();
+    evaluateInput("totalDepth", "465");
+    stopBoreholeEditing();
   });
 });
