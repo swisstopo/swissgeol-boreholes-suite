757a41614cb5e988346cb0defc3e9707e6112244
Frederic Stahel <frederic.stahel@geowerkstatt.ch>
2025-01-21 10:18:38 +0100

    Add null check for api response content type

--- src/client/src/api/fetchApiV2.js
+++ src/client/src/api/fetchApiV2.js
@@ -33,8 +33,9 @@ export async function fetchApiV2(url, method, payload = null) {
     if (contentType && contentType.indexOf("application/json") !== -1) {
       return await response.json();
     } else if (
-      contentType.indexOf("application/geopackage+sqlite") !== -1 ||
-      contentType.indexOf("application/octet-stream") !== -1
+      contentType &&
+      (contentType.indexOf("application/geopackage+sqlite") !== -1 ||
+        contentType.indexOf("application/octet-stream") !== -1)
     ) {
       return await response.blob(); // Binary response
     } else {
