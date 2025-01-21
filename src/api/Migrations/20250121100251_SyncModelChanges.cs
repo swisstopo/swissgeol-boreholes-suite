c45cf406e896165f1dbb8b39425e11dc4213b553
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2025-01-21 11:05:18 +0100

    Add sync migration

---
+++ src/api/Migrations/20250121100251_SyncModelChanges.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncModelChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "conf_cli",
+                schema: "bdms",
+                table: "codelist",
+                type: "json",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "conf_cli",
+                schema: "bdms",
+                table: "codelist",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "json",
+                oldNullable: true);
+        }
+    }
+}
