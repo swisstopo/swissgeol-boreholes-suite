21fd0213ddaaf6be71f4f637483577dfa166c794
Frederic Stahel <frederic.stahel@geowerkstatt.ch>
2025-01-22 13:56:31 +0100

    Add migration script to remove whitespaces in attachment filenames

---
+++ src/api/Migrations/20250122124741_ReplaceAttachmentFilenameWhitespaces.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReplaceAttachmentFilenameWhitespaces : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE bdms.files
+                SET name_fil = REPLACE(name_fil, ' ', '_'); ");
+        }
+    }
+}
