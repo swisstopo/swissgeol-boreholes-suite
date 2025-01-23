d3611f6f97ddba7745127081592266f779dfccf2
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2025-01-23 17:34:50 +0100

    Fix tab style

--- src/client/src/components/styledTabComponents.tsx
+++ src/client/src/components/styledTabComponents.tsx
@@ -4,17 +4,14 @@ import { theme } from "../AppTheme";
 
 export const BdmsTabContentBox = styled(Box)(() => ({
   backgroundColor: theme.palette.background.default,
-  borderBottom: `1px solid ${theme.palette.boxShadow}`,
-  borderRight: `1px solid ${theme.palette.boxShadow}`,
-  borderLeft: `1px solid ${theme.palette.boxShadow}`,
-  borderRadius: "3px",
-  padding: "10px 10px 5px 10px",
-  margin: "0 5px 10px 5px",
+  border: `1px solid ${theme.palette.boxShadow}`,
+  padding: `${theme.spacing(3)}`,
   display: "flex",
   flexDirection: "column",
 }));
 
 export const BdmsTabs = styled(Tabs)({
+  overflow: "visible",
   margin: "0 4px",
   "& .MuiTabs-indicator": {
     display: "none",
@@ -22,12 +19,23 @@ export const BdmsTabs = styled(Tabs)({
 });
 
 export const BdmsTab = styled(props => <Tab disableRipple {...props} />)(() => ({
+  fontSize: "16px",
+  fontWeight: "400",
+  paddingTop: `${theme.spacing(2)}`,
+  paddingBottom: `${theme.spacing(2)}`,
+  paddingRight: `${theme.spacing(3)}`,
+  paddingLeft: `${theme.spacing(3)}`,
+  color: theme.palette.secondary.main,
+
   "&.Mui-selected": {
+    color: theme.palette.background.menuItemActive,
     backgroundColor: theme.palette.background.default,
     borderTop: `1px solid ${theme.palette.boxShadow}`,
     borderRight: `1px solid ${theme.palette.boxShadow}`,
     borderLeft: `1px solid ${theme.palette.boxShadow}`,
-    borderRadius: "3px solid black",
+    borderBottom: `1px solid ${theme.palette.background.default}`,
+    borderRadius: `${theme.spacing(0.5)} ${theme.spacing(0.5)} 0 0`,
+    top: "1px",
   },
   "&.Mui-focusVisible": {
     backgroundColor: "rgba(100, 95, 228, 0.32)",
