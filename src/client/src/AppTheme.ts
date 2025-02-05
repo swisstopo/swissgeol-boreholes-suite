04a24ea4251561a6bba1ed3c337d5b40bfcf4a85
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2025-02-05 12:16:35 +0100

    Fix header on select

--- src/client/src/AppTheme.ts
+++ src/client/src/AppTheme.ts
@@ -563,9 +563,10 @@ export const theme = createTheme({
           "& .MuiDataGrid-columnHeader": {
             backgroundColor: themePalette.border.light,
           },
-          "& .MuiDataGrid-columnHeaderCheckbox, .MuiDataGrid-cellCheckbox, .MuiDataGrid-cell": {
-            outline: "none !important",
-          },
+          "& .MuiDataGrid-columnHeaderCheckbox, .MuiDataGrid-cellCheckbox, .MuiDataGrid-cell, .MuiDataGrid-columnHeader":
+            {
+              outline: "none !important",
+            },
           "& .MuiDataGrid-toolbarContainer": {
             paddingLeft: "2px !important",
             paddingBottom: "20px !important",
