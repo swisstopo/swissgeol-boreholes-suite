8ef5c0576cd6bc9ba5e2bc48a6c1095ac2231261
danjov <daniel.jovanovic@geowerkstatt.ch>
2025-02-13 09:48:31 +0100

    Add security response headers on express/vite

--- src/client/vite.config.js
+++ src/client/vite.config.js
@@ -25,5 +25,12 @@ export default defineConfig({
       },
     },
     port: 3000,
+    headers: {
+      "Content-Security-Policy": "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' fonts.googleapis.com; img-src 'self' data: https:; font-src 'self' data: https: fonts.gstatic.com; frame-ancestors 'none'",
+      "X-FRAME-OPTIONS": "DENY",
+      "X-Content-Type-Options": "nosniff",
+      "Referrer-Policy": "strict-origin-when-cross-origin",
+      "Strict-Transport-Security": "max-age=31536000; includeSubDomains",
+    },
   },
 });
