976be1566f67388014069df7ebfd741f7a984cf7
danjov <daniel.jovanovic@geowerkstatt.ch>
2025-02-17 13:42:37 +0100

    Update CSP header
    Allow blob images.

--- src/client/vite.config.js
+++ src/client/vite.config.js
@@ -27,7 +27,7 @@ export default defineConfig({
     port: 3000,
     headers: {
       "Content-Security-Policy":
-        "default-src 'self'; connect-src 'self' https://*.geo.admin.ch http://localhost:4011; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' fonts.googleapis.com; img-src 'self' https://*.geo.admin.ch data:; font-src 'self' data: fonts.gstatic.com; frame-ancestors 'none'",
+        "default-src 'self'; connect-src 'self' https://*.geo.admin.ch http://localhost:4011; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' fonts.googleapis.com; img-src 'self' https://*.geo.admin.ch data: blob:; font-src 'self' data: fonts.gstatic.com; frame-ancestors 'none'",
       "X-FRAME-OPTIONS": "DENY",
       "X-Content-Type-Options": "nosniff",
       "Referrer-Policy": "strict-origin-when-cross-origin",
