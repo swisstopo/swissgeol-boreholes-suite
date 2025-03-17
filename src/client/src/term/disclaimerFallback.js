ae10680c197dc318728465cc21329a435982ec2c
danjov <daniel.jovanovic@geowerkstatt.ch>
2025-03-17 13:57:50 +0100

    Refine data collection terminology

--- src/client/src/term/disclaimerFallback.js
+++ src/client/src/term/disclaimerFallback.js
@@ -6,7 +6,7 @@ Swisstopo behält sich ausdrücklich vor, jederzeit Inhalte ohne Ankündigung ga
 Haftungsansprüche gegen swisstopo wegen Schäden materieller oder immaterieller Art, welche aus dem Zugriff oder der Nutzung bzw. Nichtnutzung der veröffentlichten Informationen, durch Missbrauch der Verbindung oder durch technische Störungen entstanden sind, werden ausgeschlossen.
 
 # Datenerfassung
-Damit wir unser Webangebot optimal auf Ihre Bedürfnisse ausrichten können, verwenden wir die Analysetools Google Analytics. Dabei wird Ihr Verhalten auf der Website in anonymisierter Form erfasst. Es werden also keine personenbezogenen Daten übermittelt oder gespeichert. Wenn Sie damit nicht einverstanden sind, können Sie die Datenerfassung durch Analysetools unterbinden und diese Website trotzdem ohne Einschränkungen nutzen.
+Damit wir unser Webangebot optimal auf Ihre Bedürfnisse ausrichten können, verwenden wir das Analysetool Google Analytics. Dabei wird Ihr Verhalten auf der Website in anonymisierter Form erfasst. Es werden also keine personenbezogenen Daten übermittelt oder gespeichert. Wenn Sie damit nicht einverstanden sind, können Sie die Datenerfassung durch Analysetools unterbinden und diese Website trotzdem ohne Einschränkungen nutzen.
 
 Weitere rechtliche Bestimmungen finden Sie hier: https://www.swissgeol.ch/datenschutz/
 `;
@@ -19,7 +19,7 @@ swisstopo reserves the right to alter or remove the content, in full or in part,
 Liability claims against swisstopo for material or immaterial damage resulting from access to or use or non-use of the published information, from misuse of the connection or from technical faults are excluded.
 
 # Data acquisition
-To enable us to optimally tailor our website to your needs, we use the analysis tools Google Analytics and Sentry. Your behaviour on the website is recorded in anonymised form. No personal data is transmitted or stored. If you do not wish to consent to this, you can stop data collection by analysis tools and still use this website without restrictions.
+To enable us to optimally tailor our website to your needs, we use the analysis tool Google Analytics. Your behaviour on the website is recorded in anonymised form. No personal data is transmitted or stored. If you do not wish to consent to this, you can stop data collection by analysis tools and still use this website without restrictions.
 
 Further legal provisions can be found here: https://www.swissgeol.ch/datenschutz-en/
 `;
@@ -32,7 +32,7 @@ swisstopo se réserve expressément le droit de modifier en partie ou en totalit
 swisstopo ne saurait être tenu pour responsable des dommages matériels ou immatériels qui pourraient être causés par l’accès aux informations diffusées ou par leur utilisation ou non-utilisation, par le mauvais usage de la connexion ou par des problèmes techniques.
 
 # Acquisition de données
-Nous utilisons les outils d’analyse Google Analytics et Sentry afin d’adapter au mieux notre site web à vos besoins. Votre comportement sur le site est enregistré de manière anonyme. Aucune donnée personnelle n’est transmise ou stockée. Si vous n’acceptez pas cet état de fait, vous pouvez empêcher la collecte de données par des outils d’analyse et continuer à utiliser ce site web sans restriction.
+Nous utilisons l’outil d’analyse Google Analytics et Sentry afin d’adapter au mieux notre site web à vos besoins. Votre comportement sur le site est enregistré de manière anonyme. Aucune donnée personnelle n’est transmise ou stockée. Si vous n’acceptez pas cet état de fait, vous pouvez empêcher la collecte de données par des outils d’analyse et continuer à utiliser ce site web sans restriction.
 
 Pour plus d'informations légales, veuillez consulter le lien suivant: https://www.swissgeol.ch/datenschutz-fr/
 `;
@@ -45,7 +45,7 @@ swisstopo si riserva esplicitamente il diritto in qualsiasi momento di modificar
 swisstopo declina ogni responsabilità per danni materiali o immateriali derivanti dall’accesso alle informazioni diffuse, dall’uso o dal mancato uso del sito, oppure che sono riconducibili a un malfunzionamento del collegamento o a disturbi tecnici del sito.
 
 # Acquisizione di dati
-Per poter adattare la nostra offerta web in modo ottimale alle vostre esigenze, utilizziamo gli strumenti di analisi Google Analytics e Sentry. Ciò significa, che il vostro comportamento sul sito web viene registrato in forma anonima. Di conseguenza, non vengono trasmessi o memorizzati dati personali. Se non siete dʼaccordo, potete interrompere la raccolta dei dati da parte degli strumenti di analisi e continuare ad utilizzare questo sito web senza restrizioni.
+Per poter adattare la nostra offerta web in modo ottimale alle vostre esigenze, uUtilizziamo lo strumento di analisi Google Analytics. Ciò significa, che il vostro comportamento sul sito web viene registrato in forma anonima. Di conseguenza, non vengono trasmessi o memorizzati dati personali. Se non siete dʼaccordo, potete interrompere la raccolta dei dati da parte degli strumenti di analisi e continuare ad utilizzare questo sito web senza restrizioni.
 
 Per ulteriori informazioni legali, consultare il seguente link: https://www.swissgeol.ch/datenschutz-it/
 `;
