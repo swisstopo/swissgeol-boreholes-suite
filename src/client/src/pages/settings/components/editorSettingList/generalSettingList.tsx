d10bc80173accc45c2f14e4d9e25331ff7d22dec
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2025-03-24 13:50:04 +0100

    Fix settings update

--- src/client/src/pages/settings/components/editorSettingList/generalSettingList.tsx
+++ src/client/src/pages/settings/components/editorSettingList/generalSettingList.tsx
@@ -31,65 +31,65 @@ const GeneralSettingList: FC<GeneralSettingListProps> = ({
   const [checkedStates, setCheckedStates] = useState<{ [key: string]: boolean }>({});
   const { data: domains } = useDomains();
 
-  const layerKindConfigEntry: Codelist | undefined = domains?.find((c: Codelist) => c.schema === "layer_kind");
-  const conf = layerKindConfigEntry?.conf ? JSON.parse(layerKindConfigEntry?.conf) : "";
-
-  const isChecked = useCallback(
-    (item: SettingsItem) => {
+  useEffect(() => {
+    const isChecked = (item: SettingsItem) => {
       const isVisible = (field: string) => {
+        const layerKindConfigEntry = domains?.find((c: Codelist) => c.schema === "layer_kind");
+        const conf = layerKindConfigEntry?.conf ? JSON.parse(layerKindConfigEntry?.conf) : "";
         return conf?.fields?.[field] ?? false;
       };
-
       const getNestedValue = (settingValue: string) => {
         const splitStrings = settingValue.split(".");
         // @ts-expect-error setting values inside efilter have not been typed
         return splitStrings.length > 1 ? data?.[splitStrings[0]]?.[splitStrings[1]] : data?.[settingValue];
       };
-
       return listName === "lithologyfields" ? isVisible(item.value) : getNestedValue(item.value);
-    },
-    [conf, data, listName],
-  );
+    };
 
-  useEffect(() => {
     const initialState: { [key: string]: boolean } = {};
     settingsItems.forEach(item => {
       initialState[item.value] = isChecked(item);
     });
     setCheckedStates(initialState);
-  }, [settingsItems, data, listName, isChecked]);
+  }, [data, domains, listName, settingsItems]);
 
-  const sendSelectAll = (shouldSelectAll: boolean) => {
-    const newData: string[] = [];
-    settingsItems.forEach(element => {
-      newData.push(element.value);
-    });
-
-    if (listName === "lithologyfields") {
-      toggleFieldArray(newData, shouldSelectAll);
-    } else toggleFilterArray(newData, shouldSelectAll);
-
-    setCheckedStates(prevState => {
-      const newState: { [key: string]: boolean } = { ...prevState };
-      settingsItems.forEach(item => {
-        newState[item.value] = shouldSelectAll;
+  const sendSelectAll = useCallback(
+    (shouldSelectAll: boolean) => {
+      const newData: string[] = [];
+      settingsItems.forEach(element => {
+        newData.push(element.value);
       });
-      return newState;
-    });
-  };
 
-  const handleCheckboxChange = (item: SettingsItem, checked: boolean) => {
-    setCheckedStates(prevState => ({
-      ...prevState,
-      [item.value]: checked,
-    }));
+      if (listName === "lithologyfields") {
+        toggleFieldArray(newData, shouldSelectAll);
+      } else toggleFilterArray(newData, shouldSelectAll);
 
-    if (listName === "lithologyfields") {
-      toggleField(item.value, checked);
-    } else {
-      toggleFilter(item.value, checked);
-    }
-  };
+      setCheckedStates(prevState => {
+        const newState: { [key: string]: boolean } = { ...prevState };
+        settingsItems.forEach(item => {
+          newState[item.value] = shouldSelectAll;
+        });
+        return newState;
+      });
+    },
+    [listName, settingsItems, toggleFieldArray, toggleFilterArray],
+  );
+
+  const handleCheckboxChange = useCallback(
+    (item: SettingsItem, checked: boolean) => {
+      setCheckedStates(prevState => ({
+        ...prevState,
+        [item.value]: checked,
+      }));
+
+      if (listName === "lithologyfields") {
+        toggleField(item.value, checked);
+      } else {
+        toggleFilter(item.value, checked);
+      }
+    },
+    [listName, toggleField, toggleFilter],
+  );
 
   return (
     <Styled.Container>
@@ -110,16 +110,18 @@ const GeneralSettingList: FC<GeneralSettingListProps> = ({
           {t("unselectAll")}
         </Button>
       </Segment>
-      {settingsItems?.map(item => (
-        <Segment key={item.id}>
-          <Checkbox
-            checked={!!checkedStates[item.value]}
-            onChange={e => handleCheckboxChange(item, e.target.checked)}
-            data-cy={`checkbox-${item.value}`}
-          />
-          <TranslationText id={item.label} />
-        </Segment>
-      ))}
+      {settingsItems?.map(item => {
+        return (
+          <Segment key={item.id}>
+            <Checkbox
+              checked={!!checkedStates[item.value]}
+              onChange={e => handleCheckboxChange(item, e.target.checked)}
+              data-cy={`checkbox-${item.value}`}
+            />
+            <TranslationText id={item.label} />
+          </Segment>
+        );
+      })}
     </Styled.Container>
   );
 };
