9111b1cf3189e90e30410fc47d4214c98aeeeecf
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2025-04-01 15:22:50 +0200

    Fix groundwaterlevel inputs

--- src/client/src/pages/detail/form/hydrogeology/groundwaterLevelMeasurement/groundwaterLevelMeasurementInput.tsx
+++ src/client/src/pages/detail/form/hydrogeology/groundwaterLevelMeasurement/groundwaterLevelMeasurementInput.tsx
@@ -1,7 +1,8 @@
 import { FC } from "react";
 import DataInputCard from "../../../../../components/dataCard/dataInputCard.js";
-import { FormContainer, FormInput, FormValueType } from "../../../../../components/form/form.ts";
+import { FormContainer, FormInput } from "../../../../../components/form/form.ts";
 import { FormDomainSelect } from "../../../../../components/form/formDomainSelect.tsx";
+import { parseFloatWithThousandsSeparator } from "../../../../../components/form/formUtils.ts";
 import { prepareCasingDataForSubmit } from "../../completion/casingUtils.jsx";
 import { hydrogeologySchemaConstants } from "../hydrogeologySchemaConstants.ts";
 import { ObservationType, prepareObservationDataForSubmit } from "../Observation.ts";
@@ -20,6 +21,8 @@ const GroundwaterLevelMeasurementInput: FC<{ item: GroundwaterLevelMeasurement; 
     data = prepareCasingDataForSubmit(data);
     data = prepareObservationDataForSubmit(data, parentId);
     data.type = ObservationType.groundwaterLevelMeasurement;
+    data.levelM = parseFloatWithThousandsSeparator(data.levelM);
+    data.levelMasl = parseFloatWithThousandsSeparator(data.levelMasl);
 
     if (data.casingId == null) {
       data.casingId = item.casingId;
@@ -46,8 +49,8 @@ const GroundwaterLevelMeasurementInput: FC<{ item: GroundwaterLevelMeasurement; 
         />
       </FormContainer>
       <FormContainer direction="row">
-        <FormInput fieldName="levelMasl" label="gwlm_levelmasl" value={item.levelMasl} type={FormValueType.Number} />
-        <FormInput fieldName="levelM" label="gwlm_levelm" value={item.levelM} type={FormValueType.Number} />
+        <FormInput fieldName="levelMasl" label="gwlm_levelmasl" value={item.levelMasl} withThousandSeparator={true} />
+        <FormInput fieldName="levelM" label="gwlm_levelm" value={item.levelM} withThousandSeparator={true} />
       </FormContainer>
     </DataInputCard>
   );
