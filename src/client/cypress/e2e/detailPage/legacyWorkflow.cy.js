4435e4b9f614cd185e601586ae0ae3077c65425d
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2025-04-29 11:34:20 +0200

    Add cypress test

--- src/client/cypress/e2e/detailPage/legacyWorkflow.cy.js
+++ src/client/cypress/e2e/detailPage/legacyWorkflow.cy.js
@@ -24,7 +24,7 @@ const verifyStatusTextsNotInHeader = status => {
 };
 
 // Skip the test until deleting of boreholes is fixed (see github issue #1188)
-describe("Tests the publication workflow.", () => {
+describe("Tests the legacy publication workflow.", () => {
   it("Publishes a borehole without rejections", () => {
     createBorehole({ "extended.original_name": "Borehole to publish" }).as("borehole_id");
     cy.get("@borehole_id").then(id => {
