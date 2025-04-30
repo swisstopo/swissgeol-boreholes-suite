3ebf96b33dccd836e21316a4d40bd22a125773b7
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2025-04-30 12:27:02 +0200

    Add assigned person card

--- src/client/src/pages/detail/form/workflow/workflowAssignedUserCard.tsx
+++ src/client/src/pages/detail/form/workflow/workflowAssignedUserCard.tsx
@@ -1,13 +1,31 @@
 import { useTranslation } from "react-i18next";
-import { Typography } from "@mui/material";
+import { useParams } from "react-router-dom";
+import { Button, Stack, Typography } from "@mui/material";
+import AssignUser from "../../../../assets/icons/assignUser.svg?react";
+import { capitalizeFirstLetter } from "../../../../utils.ts";
 import { WorkflowCard } from "./styledWorkflowComponents.tsx";
+import { useWorkflow } from "./workflow.ts";
 
 export const WorkflowAssignedUserCard = () => {
   const { t } = useTranslation();
+  const { id: boreholeId } = useParams<{ id: string }>();
+  const { data: workflow } = useWorkflow(parseInt(boreholeId));
 
   return (
     <WorkflowCard>
-      <Typography variant="h5">{t("assignedPerson")}</Typography>
+      <Stack gap={1.5}>
+        <Typography variant="h5">{t("assignedPerson")}</Typography>
+        <Typography variant="body1">{`${[capitalizeFirstLetter(workflow?.assignee?.firstName), capitalizeFirstLetter(workflow?.assignee?.lastName)].filter(Boolean).join(" ")}`}</Typography>
+        <Button
+          variant="outlined"
+          data-cy={"assign-user-button"}
+          endIcon={<AssignUser />}
+          onClick={() => {
+            console.log("Assign user");
+          }}>
+          {t("assignUser")}
+        </Button>
+      </Stack>
     </WorkflowCard>
   );
 };
