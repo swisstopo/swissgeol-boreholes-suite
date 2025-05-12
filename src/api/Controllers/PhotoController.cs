e797b001892807a49ad3691f269aacd2cf1ebdec
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2025-05-12 17:23:16 +0200

    Add missing model

--- src/api/Controllers/PhotoController.cs
+++ src/api/Controllers/PhotoController.cs
@@ -7,7 +7,6 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
 using System.IO.Compression;
-using System.Linq;
 
 namespace BDMS.Controllers;
 
---
+++ src/api/Models/PhotoUpdate.cs
@@ -0,0 +1,17 @@
+﻿namespace BDMS.Models;
+
+/// <summary>
+/// Represents a <see cref="Photo"/>'s updatable properties.
+/// </summary>
+public class PhotoUpdate
+{
+    /// <summary>
+    /// Gets the <see cref="Photo"/>'s id.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Gets the <see cref="Photo"/>'s public state.
+    /// </summary>
+    public bool Public { get; set; }
+}
