d0f0f35300492c0c8f8bf6b0ecd427afc9ad0cb7
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2025-05-26 17:11:09 +0200

    Fix sonar issue

--- src/client/src/components/map/clickablePopup.tsx
+++ src/client/src/components/map/clickablePopup.tsx
@@ -13,10 +13,11 @@ export const ClickablePopup: FC<ClickablePopupProps> = ({ features = [] }) => {
     <Box display="none">
       <Box className="ol-popup" id="popup-overlay">
         {features?.length > 0 &&
-          features.map((feature, index) => {
-            const name = feature.get("name") ?? feature.getId();
+          features.map(feature => {
+            const featureId = feature.getId();
+            const name = feature.get("name") ?? featureId;
             return (
-              <Typography noWrap key={index} onClick={() => navigate("/" + feature.getId())} sx={{ cursor: "pointer" }}>
+              <Typography noWrap key={featureId} onClick={() => navigate("/" + featureId)} sx={{ cursor: "pointer" }}>
                 {name}
               </Typography>
             );
