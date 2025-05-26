54b060424da8ebafab66358b955b8df3a0992389
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2025-05-26 15:59:39 +0200

    Remove unused import

--- src/client/src/pages/overview/layout/mapView.tsx
+++ src/client/src/pages/overview/layout/mapView.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext, useState } from "react";
+import { useCallback, useContext, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { Stack } from "@mui/material";
