def773e01f25a62ab70c1f7f4e0fbb97eeedda9e
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2025-07-10 15:08:18 +0200

    Fix migration style

--- src/api/Migrations/20250710115322_AddPropertyToTabStatus.cs
+++ src/api/Migrations/20250710115322_AddPropertyToTabStatus.cs
@@ -2,50 +2,49 @@
 
 #nullable disable
 
-namespace BDMS.Migrations
+namespace BDMS.Migrations;
+
+/// <inheritdoc />
+public partial class AddPropertyToTabStatus : Migration
 {
     /// <inheritdoc />
-    public partial class AddPropertyToTabStatus : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<bool>(
-                name: "general",
-                schema: "bdms",
-                table: "tab_status",
-                type: "boolean",
-                nullable: false,
-                oldClrType: typeof(bool),
-                oldType: "boolean",
-                oldDefaultValue: false);
+        migrationBuilder.AlterColumn<bool>(
+            name: "general",
+            schema: "bdms",
+            table: "tab_status",
+            type: "boolean",
+            nullable: false,
+            oldClrType: typeof(bool),
+            oldType: "boolean",
+            oldDefaultValue: false);
 
-            migrationBuilder.AddColumn<bool>(
-                name: "location",
-                schema: "bdms",
-                table: "tab_status",
-                type: "boolean",
-                nullable: false,
-                defaultValue: false);
-        }
+        migrationBuilder.AddColumn<bool>(
+            name: "location",
+            schema: "bdms",
+            table: "tab_status",
+            type: "boolean",
+            nullable: false,
+            defaultValue: false);
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "location",
-                schema: "bdms",
-                table: "tab_status");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "location",
+            schema: "bdms",
+            table: "tab_status");
 
-            migrationBuilder.AlterColumn<bool>(
-                name: "general",
-                schema: "bdms",
-                table: "tab_status",
-                type: "boolean",
-                nullable: false,
-                defaultValue: false,
-                oldClrType: typeof(bool),
-                oldType: "boolean");
-        }
+        migrationBuilder.AlterColumn<bool>(
+            name: "general",
+            schema: "bdms",
+            table: "tab_status",
+            type: "boolean",
+            nullable: false,
+            defaultValue: false,
+            oldClrType: typeof(bool),
+            oldType: "boolean");
     }
 }
