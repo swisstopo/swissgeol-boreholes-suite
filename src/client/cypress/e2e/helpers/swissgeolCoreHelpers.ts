10c8966df947a7a1b1bf0de78bea4d9ebbe51b8b
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2025-07-30 12:08:01 +0200

    Reinstroduce short wait

--- src/client/cypress/e2e/helpers/swissgeolCoreHelpers.ts
+++ src/client/cypress/e2e/helpers/swissgeolCoreHelpers.ts
@@ -6,6 +6,8 @@ export function assertWorkflowSteps(activeStep: string) {
 }
 
 export function clickSgcButtonWithContent(content: string) {
+  // eslint-disable-next-line cypress/no-unnecessary-waiting
+  cy.wait(100);
   cy.get("sgc-button").contains(content).click();
 }
 
