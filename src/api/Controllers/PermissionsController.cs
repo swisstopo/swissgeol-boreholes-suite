8341f23f29e50ca54830e179c2c373029471499d
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2025-07-30 11:55:07 +0200

    Fix locking error

---
+++ src/api/Controllers/PermissionsController.cs
@@ -0,0 +1,41 @@
+﻿using BDMS.Authentication;
+using BDMS.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BDMS.Controllers;
+
+[ApiController]
+[Route("api/v{version:apiVersion}/[controller]")]
+public class PermissionsController : ControllerBase
+{
+    private readonly BdmsContext context;
+    private readonly IBoreholePermissionService permissionService;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PermissionsController"/> class.
+    /// </summary>
+    /// <param name="context">The EF database context containing data for the BDMS application.</param>
+    /// <param name="permissionService"><see cref="BoreholePermissionService"/>.</param>
+    public PermissionsController(BdmsContext context, IBoreholePermissionService permissionService)
+    {
+        this.context = context;
+        this.permissionService = permissionService;
+    }
+
+    /// <summary>
+    /// Gets whether the currently authenticated user has permission to edit the <see cref="Borehole"/>.
+    /// </summary>
+    /// <param name="boreholeId">The id of the borehole to get the edit permissions for.</param>
+    [HttpGet("canEdit")]
+    [Authorize(Policy = PolicyNames.Viewer)]
+    public async Task<bool> CanEditAsync([FromQuery] int? boreholeId)
+    {
+        if (!boreholeId.HasValue || boreholeId <= 0)
+        {
+            return false;
+        }
+
+        return await permissionService.CanEditBoreholeAsync(HttpContext.GetUserSubjectId(), boreholeId).ConfigureAwait(false);
+    }
+}
---
+++ tests/api/Controllers/PermissionsControllerTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using static BDMS.Helpers;
+
+namespace BDMS.Controllers;
+
+[TestClass]
+public class PermissionsControllerTest
+{
+    private BdmsContext context;
+    private Mock<IBoreholePermissionService> boreholePermissionServiceMock;
+    private PermissionsController controller;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        context = ContextFactory.GetTestContext();
+
+        boreholePermissionServiceMock = new Mock<IBoreholePermissionService>(MockBehavior.Strict);
+        boreholePermissionServiceMock
+            .Setup(x => x.CanEditBoreholeAsync("sub_admin", It.IsAny<int?>(), It.IsAny<bool?>()))
+            .ReturnsAsync(true);
+        boreholePermissionServiceMock
+            .Setup(x => x.CanViewBoreholeAsync("sub_admin", It.IsAny<int?>()))
+            .ReturnsAsync(true);
+
+        controller = new PermissionsController(context,  boreholePermissionServiceMock.Object) { ControllerContext = GetControllerContextAdmin() }; ;
+    }
+
+    [TestCleanup]
+    public async Task TestCleanup()
+    {
+        await context.DisposeAsync();
+    }
+
+    [TestMethod]
+    public async Task CheckPermissionsForInexistantBorehole()
+    {
+        var response = await controller.CanEditAsync(0).ConfigureAwait(false);
+        Assert.AreEqual(response, false);
+    }
+
+    [TestMethod]
+    public async Task CheckPermissionsForExistingBorehole()
+    {
+        var response = await controller.CanEditAsync(1003010).ConfigureAwait(false);
+        Assert.AreEqual(response, true);
+    }
+}
