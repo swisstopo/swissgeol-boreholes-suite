8aa716b14b217e6862a6268c62978281798dcc8f
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2025-08-12 10:19:24 +0200

    Update set publication status script

--- src/extern-sync/db-init/01_set_publication_status.sh
+++ src/extern-sync/db-init/01_set_publication_status.sh
@@ -4,7 +4,7 @@
 # DESCRIPTION: As part of the docker-entrypoint initialization, this script
 #              sets the publication status for some boreholes to 'published'.
 #              Only lightweight boreholes, with an id greater than 1000100, are
-#              used, because the full blown do not met the requirements
+#              used, because the full blown do not meet the requirements
 #              regarding the use of casings.
 # ------------------------------------------------------------------------------
 
@@ -22,10 +22,24 @@ psql \
     DO \$\$
     DECLARE
       bho_ids INTEGER[] := ARRAY[1000299, 1000300, 1000301, 1000302, 1000999, 1002008, 1002999];
+      tabs_id1 INTEGER;
+      tabs_id2 INTEGER;
     BEGIN
-      INSERT INTO $SOURCE_DB_SCHEMA.workflow (id_bho_fk, id_usr_fk, started_wkf, finished_wkf, id_rol_fk)
-      SELECT id, 1, NOW(), NOW(), r.id_rol_fk
-      FROM unnest(bho_ids) AS id
-      CROSS JOIN (VALUES (1), (2), (3), (4)) AS r(id_rol_fk);
+      -- For each borehole
+      FOREACH bho_id IN ARRAY bho_ids LOOP
+        -- Insert first tab_status entry
+        INSERT INTO $SOURCE_DB_SCHEMA.tab_status (\"general\", \"location\", \"section\", geometry, lithology, chronostratigraphy,lithostratigraphy, casing, instrumentation, backfill, water_ingress, groundwater, field_measurement, hydrotest, profile, photo, \"document\")
+        VALUES (true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true)
+        RETURNING tab_status_id INTO tabs_id1;
+
+        -- Insert second tab_status
+        INSERT INTO $SOURCE_DB_SCHEMA.tab_status (\"general\", \"location\", \"section\", geometry, lithology, chronostratigraphy,lithostratigraphy, casing, instrumentation, backfill, water_ingress, groundwater, field_measurement, hydrotest, profile, photo, \"document\")
+        VALUES (true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true)
+        RETURNING tab_status_id INTO tabs_id2;
+
+        -- Insert workflow entry with the new tab_status ids
+        INSERT INTO $SOURCE_DB_SCHEMA.workflow (has_requested_changes, status, borehole_id, reviewed_tabs_id, published_tabs_id, assignee_id)
+        VALUES (false, 3, bho_id, tabs_id1, tabs_id2, null);
+      END LOOP;
     END \$\$
   "
