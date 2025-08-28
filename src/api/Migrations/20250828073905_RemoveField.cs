856c866300b27a34669feeb78166132dd417666d
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2025-08-28 09:48:18 +0200

    Remove field from lithologyDescription

---
+++ src/api/Migrations/20250828073905_RemoveField.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDMS.Migrations;
+
+/// <inheritdoc />
+public partial class RemoveField : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropForeignKey(
+            name: "FK_lithology_texture_meta_codelist_lithology_description_Litho~",
+            schema: "bdms",
+            table: "lithology_texture_meta_codelist");
+
+        migrationBuilder.DropIndex(
+            name: "IX_lithology_texture_meta_codelist_LithologyDescriptionId",
+            schema: "bdms",
+            table: "lithology_texture_meta_codelist");
+
+        migrationBuilder.DropColumn(
+            name: "LithologyDescriptionId",
+            schema: "bdms",
+            table: "lithology_texture_meta_codelist");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<int>(
+            name: "LithologyDescriptionId",
+            schema: "bdms",
+            table: "lithology_texture_meta_codelist",
+            type: "integer",
+            nullable: true);
+
+        migrationBuilder.CreateIndex(
+            name: "IX_lithology_texture_meta_codelist_LithologyDescriptionId",
+            schema: "bdms",
+            table: "lithology_texture_meta_codelist",
+            column: "LithologyDescriptionId");
+
+        migrationBuilder.AddForeignKey(
+            name: "FK_lithology_texture_meta_codelist_lithology_description_Litho~",
+            schema: "bdms",
+            table: "lithology_texture_meta_codelist",
+            column: "LithologyDescriptionId",
+            principalSchema: "bdms",
+            principalTable: "lithology_description",
+            principalColumn: "id");
+    }
+}
