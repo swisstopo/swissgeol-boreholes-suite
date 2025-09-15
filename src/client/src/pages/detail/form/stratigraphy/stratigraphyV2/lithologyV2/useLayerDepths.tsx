7d4c30dd5fe8d6edc0246db90d4dd7d06e15644a
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2025-09-15 14:23:41 +0200

    Refactor to use cognitive complexity

--- src/client/src/pages/detail/form/stratigraphy/stratigraphyV2/lithologyV2/useLayerDepths.tsx
+++ src/client/src/pages/detail/form/stratigraphy/stratigraphyV2/lithologyV2/useLayerDepths.tsx
@@ -2,6 +2,85 @@ import { useMemo } from "react";
 import { BaseLayer, FaciesDescription, LithologicalDescription } from "../../../../../../api/stratigraphy.ts";
 import { LayerDepth, Lithology } from "../../lithology.ts";
 
+const handleInsertBefore = (description: BaseLayer, layerDepth: LayerDepth, i: number, layerDepths: LayerDepth[]) => {
+  if (description.toDepth <= layerDepth.fromDepth) {
+    layerDepths.splice(i, 0, { fromDepth: description.fromDepth, toDepth: description.toDepth, lithologyId: 0 });
+    return true;
+  }
+  return false;
+};
+
+const handleSkipLayer = (description: BaseLayer, layerDepth: LayerDepth) => description.fromDepth >= layerDepth.toDepth;
+
+const handleContained = (description: BaseLayer, layerDepth: LayerDepth) =>
+  layerDepth.lithologyId !== 0 &&
+  description.fromDepth >= layerDepth.fromDepth &&
+  description.toDepth <= layerDepth.toDepth;
+
+const handleExactMatch = (description: BaseLayer, layerDepth: LayerDepth) =>
+  layerDepth.lithologyId === 0 &&
+  description.fromDepth === layerDepth.fromDepth &&
+  description.toDepth === layerDepth.toDepth;
+
+const handleWithinLayer = (description: BaseLayer, layerDepth: LayerDepth, i: number, layerDepths: LayerDepth[]) => {
+  if (
+    layerDepth.lithologyId === 0 &&
+    description.fromDepth > layerDepth.fromDepth &&
+    description.toDepth < layerDepth.toDepth
+  ) {
+    layerDepths.splice(
+      i,
+      1,
+      { fromDepth: layerDepth.fromDepth, toDepth: description.fromDepth, lithologyId: 0 },
+      { fromDepth: description.fromDepth, toDepth: description.toDepth, lithologyId: 0 },
+      { fromDepth: description.toDepth, toDepth: layerDepth.toDepth, lithologyId: 0 },
+    );
+    return true;
+  }
+  return false;
+};
+
+const handlePreviousOverlap = (
+  description: BaseLayer,
+  layerDepth: LayerDepth,
+  i: number,
+  layerDepths: LayerDepth[],
+) => {
+  if (
+    layerDepth.lithologyId === 0 &&
+    description.fromDepth <= layerDepth.fromDepth &&
+    description.toDepth < layerDepth.toDepth &&
+    description.toDepth > layerDepth.fromDepth
+  ) {
+    layerDepths.splice(
+      i,
+      1,
+      { fromDepth: description.fromDepth, toDepth: description.toDepth, lithologyId: 0 },
+      { fromDepth: description.toDepth, toDepth: layerDepth.toDepth, lithologyId: 0 },
+    );
+    return true;
+  }
+  return false;
+};
+
+const handleNextOverlap = (description: BaseLayer, layerDepth: LayerDepth, i: number, layerDepths: LayerDepth[]) => {
+  if (
+    layerDepth.lithologyId === 0 &&
+    description.fromDepth > layerDepth.fromDepth &&
+    description.fromDepth < layerDepth.toDepth &&
+    description.toDepth >= layerDepth.toDepth
+  ) {
+    layerDepths.splice(
+      i,
+      1,
+      { fromDepth: layerDepth.fromDepth, toDepth: description.fromDepth, lithologyId: 0 },
+      { fromDepth: description.fromDepth, toDepth: description.toDepth, lithologyId: 0 },
+    );
+    return true;
+  }
+  return false;
+};
+
 export function useLayerDepths(
   lithologies: Lithology[] | undefined,
   lithologicalDescriptions: LithologicalDescription[] | undefined,
@@ -14,81 +93,20 @@ export function useLayerDepths(
     });
     layerDepths.sort((a, b) => a.fromDepth - b.fromDepth);
 
-    const canInsertBefore = (description: BaseLayer, layerDepth: LayerDepth) =>
-      description.toDepth <= layerDepth.fromDepth;
-
-    const canSkipLayer = (description: BaseLayer, layerDepth: LayerDepth) =>
-      description.fromDepth >= layerDepth.toDepth;
-
-    const isContainedInLithology = (description: BaseLayer, layer: LayerDepth) =>
-      layer.lithologyId !== 0 && description.fromDepth >= layer.fromDepth && description.toDepth <= layer.toDepth;
-
-    const isExactMatch = (description: BaseLayer, layerDepth: LayerDepth) =>
-      description.fromDepth === layerDepth.fromDepth && description.toDepth === layerDepth.toDepth;
-
-    const isWithinLayer = (description: BaseLayer, layerDepth: LayerDepth) =>
-      description.fromDepth > layerDepth.fromDepth && description.toDepth < layerDepth.toDepth;
-
-    const isPreviousOverlap = (description: BaseLayer, layerDepth: LayerDepth) =>
-      description.fromDepth <= layerDepth.fromDepth &&
-      description.toDepth < layerDepth.toDepth &&
-      description.toDepth > layerDepth.fromDepth;
-
-    const isNextOverlap = (description: BaseLayer, layerDepth: LayerDepth) =>
-      description.fromDepth > layerDepth.fromDepth &&
-      description.fromDepth < layerDepth.toDepth &&
-      description.toDepth >= layerDepth.toDepth;
-
     const insertDescription = (description: BaseLayer) => {
       for (let i = 0; i < layerDepths.length; i++) {
         const layerDepth = layerDepths[i];
-
-        if (canInsertBefore(description, layerDepth)) {
-          layerDepths.splice(i, 0, { fromDepth: description.fromDepth, toDepth: description.toDepth, lithologyId: 0 });
-          return;
-        }
-        if (canSkipLayer(description, layerDepth)) continue;
-        if (isContainedInLithology(description, layerDepth)) return;
-
-        if (layerDepth.lithologyId !== 0) continue;
-
-        if (isExactMatch(description, layerDepth)) return;
-
-        if (isWithinLayer(description, layerDepth)) {
-          layerDepths.splice(
-            i,
-            1,
-            { fromDepth: layerDepth.fromDepth, toDepth: description.fromDepth, lithologyId: 0 },
-            { fromDepth: description.fromDepth, toDepth: description.toDepth, lithologyId: 0 },
-            { fromDepth: description.toDepth, toDepth: layerDepth.toDepth, lithologyId: 0 },
-          );
-          return;
-        }
-
-        if (isPreviousOverlap(description, layerDepth)) {
-          layerDepths.splice(
-            i,
-            1,
-            { fromDepth: description.fromDepth, toDepth: description.toDepth, lithologyId: 0 },
-            { fromDepth: description.toDepth, toDepth: layerDepth.toDepth, lithologyId: 0 },
-          );
-          return;
-        }
-
-        if (isNextOverlap(description, layerDepth)) {
-          layerDepths.splice(
-            i,
-            1,
-            { fromDepth: layerDepth.fromDepth, toDepth: description.fromDepth, lithologyId: 0 },
-            { fromDepth: description.fromDepth, toDepth: description.toDepth, lithologyId: 0 },
-          );
-          return;
-        }
+        if (handleInsertBefore(description, layerDepth, i, layerDepths)) return;
+        if (handleSkipLayer(description, layerDepth)) continue;
+        if (handleContained(description, layerDepth)) return;
+        if (handleExactMatch(description, layerDepth)) return;
+        if (handleWithinLayer(description, layerDepth, i, layerDepths)) return;
+        if (handlePreviousOverlap(description, layerDepth, i, layerDepths)) return;
+        if (handleNextOverlap(description, layerDepth, i, layerDepths)) return;
       }
       layerDepths.push({ fromDepth: description.fromDepth, toDepth: description.toDepth, lithologyId: 0 });
     };
 
-    // Insert descriptions
     lithologicalDescriptions?.forEach(l => {
       insertDescription(l);
     });
@@ -96,7 +114,6 @@ export function useLayerDepths(
       insertDescription(f);
     });
 
-    // Fill gaps between layers
     layerDepths.sort((a, b) => a.fromDepth - b.fromDepth);
     const filledLayerDepths: LayerDepth[] = [];
     for (let i = 0; i < layerDepths.length; i++) {
