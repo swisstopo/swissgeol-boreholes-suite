2da7a39b2c9413ddacdf5ea839b54b119de31adf
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2025-09-18 16:29:19 +0200

    Add missing alteration degree code

---
+++ src/api/Migrations/20250918142259_AddAlterationDegreeCode.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDMS.Migrations;
+
+/// <inheritdoc />
+public partial class AddAlterationDegreeCode : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(@"
+            INSERT INTO bdms.codelist(
+                schema_cli, code_cli, text_cli_en, text_cli_de, text_cli_fr, text_cli_it, order_cli
+            ) VALUES
+                ('alteration_degree', '', 'weathered', 'verwittert', 'altére', 'alterato', 11);
+        ");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+    }
+}
