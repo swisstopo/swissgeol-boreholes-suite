2341304ecbe82e08e57392efe2f3ab1a155ad012
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2025-09-20 11:41:57 +0200

    Use Number instead of parseInt

--- src/client/src/pages/detail/form/borehole/geometryImport.jsx
+++ src/client/src/pages/detail/form/borehole/geometryImport.jsx
@@ -36,7 +36,7 @@ const GeometryImport = ({ boreholeId }) => {
   } = useBoreholeGeometryMutations();
   const resetTabStatus = useResetTabStatus(["geometry"]);
 
-  const { data } = useBoreholeGeometry(parseInt(boreholeId));
+  const { data } = useBoreholeGeometry(Number(boreholeId));
   const [geometryFormats, setGeometryFormats] = useState([]);
 
   useEffect(() => {
