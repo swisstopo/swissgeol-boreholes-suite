f1f146ba11e3ff4bce2c1e65b36369c813477d58
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2025-09-22 12:20:12 +0200

    Migrate stratigraphies

---
+++ src/api/Migrations/20250922094307_MigrateStratigraphies.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDMS.Migrations;
+#pragma warning disable CA1505
+
+/// <inheritdoc />
+public partial class MigrateStratigraphies : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(@"
+            INSERT INTO bdms.stratigraphy_v2(
+                id, borehole_id, name, date, is_primary, update, updater, creation, creator
+            ) 
+            SELECT
+                id_sty,
+                id_bho_fk,
+                COALESCE(name_sty, 'Stratigraphie'),
+                date_sty,
+                COALESCE(primary_sty, FALSE),
+                update_sty,
+                updater_sty,
+                creation_sty,
+                author_sty
+            FROM bdms.stratigraphy WHERE id_bho_fk IS NOT NULL;
+        ");
+    }
+}
+#pragma warning restore CA1505
+
