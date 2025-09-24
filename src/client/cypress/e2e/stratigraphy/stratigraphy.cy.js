cff99621fe4531354d7ea6f7902822493c519e62
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2025-09-24 12:52:58 +0200

    Fix test

--- src/client/cypress/e2e/stratigraphy/stratigraphy.cy.js
+++ src/client/cypress/e2e/stratigraphy/stratigraphy.cy.js
@@ -394,7 +394,7 @@ describe("Tests for stratigraphy", () => {
         navigateToTabWithTitle("JOLLYBOUNCE");
         getElementByDataCy("stratigraphy-content")
           .find(".MuiChip-root")
-          .should("have.length", 3) // 2 chips in header + 1 chip for layer gap
+          .should("have.length", 2)
           .and("contain", "Main stratigraphy")
           .and("contain", "20.03.2024");
       });
