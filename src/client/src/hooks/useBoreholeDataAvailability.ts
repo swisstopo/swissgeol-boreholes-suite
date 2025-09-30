e14aa3b17861d3c01055faec63445e556e2ba8ce
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2025-09-30 23:52:29 +0200

    Fix useBoreholeDataAvailability.ts

--- src/client/src/hooks/useBoreholeDataAvailability.ts
+++ src/client/src/hooks/useBoreholeDataAvailability.ts
@@ -1,14 +1,10 @@
 import { useMemo } from "react";
 import { BoreholeV2 } from "../api/borehole";
 import { ObservationType } from "../pages/detail/form/hydrogeology/Observation";
-import { useDevMode } from "./useDevMode";
 
 export const useBoreholeDataAvailability = (borehole: BoreholeV2) => {
-  const { runsDevMode } = useDevMode();
-
   return useMemo(() => {
-    const hasStratigraphy =
-      (runsDevMode ? (borehole.stratigraphiesV2?.length ?? 0) : (borehole.stratigraphies?.length ?? 0)) > 0;
+    const hasStratigraphy = (borehole.stratigraphies?.length ?? 0) > 0;
     const hasCompletion = (borehole.completions?.length ?? 0) > 0;
     const hasObservation = (borehole.observations?.length ?? 0) > 0;
     const hasSections = (borehole.sections?.length ?? 0) > 0;
@@ -32,34 +28,16 @@ export const useBoreholeDataAvailability = (borehole: BoreholeV2) => {
       hasCompletion && (borehole.completions?.some(completion => completion.backfills?.length > 0) ?? false);
     const hasInstrumentations =
       hasCompletion && (borehole.completions?.some(completion => completion.instrumentations?.length > 0) ?? false);
-
-    let hasLithology = false;
-    let hasLithostratigraphy = false;
-    let hasChronostratigraphy = false;
-    if (runsDevMode) {
-      hasLithology =
-        (hasStratigraphy && borehole.stratigraphiesV2?.some(stratigraphy => stratigraphy.lithologies?.length > 0)) ??
-        false;
-      hasLithostratigraphy =
-        (hasStratigraphy &&
-          borehole.stratigraphiesV2?.some(stratigraphy => stratigraphy.lithostratigraphies?.length > 0)) ??
-        false;
-      hasChronostratigraphy =
-        (hasStratigraphy &&
-          borehole.stratigraphiesV2?.some(stratigraphy => stratigraphy.chronostratigraphies?.length > 0)) ??
-        false;
-    } else {
-      hasLithology =
-        (hasStratigraphy && borehole.stratigraphies?.some(stratigraphy => stratigraphy?.layers?.length > 0)) ?? false;
-      hasLithostratigraphy =
-        (hasStratigraphy &&
-          borehole.stratigraphies?.some(stratigraphy => stratigraphy.lithostratigraphyLayers?.length > 0)) ??
-        false;
-      hasChronostratigraphy =
-        (hasStratigraphy &&
-          borehole.stratigraphies?.some(stratigraphy => stratigraphy.chronostratigraphyLayers?.length > 0)) ??
-        false;
-    }
+    const hasLithology =
+      (hasStratigraphy && borehole.stratigraphies?.some(stratigraphy => stratigraphy?.layers?.length > 0)) ?? false;
+    const hasLithostratigraphy =
+      (hasStratigraphy &&
+        borehole.stratigraphies?.some(stratigraphy => stratigraphy.lithostratigraphyLayers?.length > 0)) ??
+      false;
+    const hasChronostratigraphy =
+      (hasStratigraphy &&
+        borehole.stratigraphies?.some(stratigraphy => stratigraphy.chronostratigraphyLayers?.length > 0)) ??
+      false;
 
     return {
       hasSections,
@@ -91,7 +69,5 @@ export const useBoreholeDataAvailability = (borehole: BoreholeV2) => {
     borehole.photos?.length,
     borehole.sections?.length,
     borehole.stratigraphies,
-    borehole.stratigraphiesV2,
-    runsDevMode,
   ]);
 };
