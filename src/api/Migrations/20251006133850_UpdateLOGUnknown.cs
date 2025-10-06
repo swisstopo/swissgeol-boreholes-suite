45f1022926ccad447e0fc1f0b5a622ff7c9d8d2b
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2025-10-06 15:50:44 +0200

    Update codelist translations

---
+++ src/api/Migrations/20251006133850_UpdateLOGUnknown.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDMS.Migrations;
+#pragma warning disable CA1505
+
+/// <inheritdoc />
+public partial class UpdateLOGUnknown : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(@"
+            UPDATE bdms.codelist
+            SET text_cli_de = 'Anderer'
+            WHERE id_cli = 100003002 OR id_cli = 100003007;
+
+            UPDATE bdms.codelist
+            SET
+                text_cli_en = 'Not specified',
+                text_cli_de = 'Keine Angabe',
+                text_cli_fr = 'Sans indication',
+                text_cli_it = 'Senza indicazioni'
+            WHERE id_cli = 100003003 OR id_cli = 100003008;
+        ");
+    }
+}
+#pragma warning restore CA1505
