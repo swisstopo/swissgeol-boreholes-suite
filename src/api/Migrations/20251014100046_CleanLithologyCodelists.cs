7357c2fe644f72e0d39f024de1cf024b1d190afd
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2025-10-14 13:15:58 +0200

    Clean up lithology codelists

---
+++ src/api/Migrations/20251014100046_CleanLithologyCodelists.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDMS.Migrations;
+#pragma warning disable CA1505
+
+/// <inheritdoc />
+public partial class CleanLithologyCodelists : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(@"
+            UPDATE bdms.codelist
+            SET order_cli=90
+            WHERE id_cli=23101010;
+
+            UPDATE bdms.lithology_description
+            SET gradation_id=100000495
+            WHERE gradation_id=30000020;
+
+            DELETE FROM bdms.codelist
+            WHERE id_cli=30000020;
+        ");
+    }
+}
+#pragma warning restore CA1505
