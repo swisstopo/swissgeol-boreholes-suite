09cfa4f81039491860628b6bf68596824b81151c
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2025-10-20 08:57:23 +0200

    Remove duplicate start bracket

--- src/client/cypress/e2e/detailPage/log.cy.js
+++ src/client/cypress/e2e/detailPage/log.cy.js
@@ -30,10 +30,9 @@ import {
 } from "../helpers/testHelpers";
 
 function assertExportButtonsDisabled(isDisabled = true) {
-  {
-    // TODO: Re-add once logic is implemented with https://github.com/swisstopo/swissgeol-boreholes-suite/issues/2361
-    // getElementByDataCy("exportdata-button").should(isDisabled ? "have.attr" : "not.have.attr", "disabled");
-    // getElementByDataCy("exporttable-button").should(isDisabled ? "have.attr" : "not.have.attr", "disabled");
+  // TODO: Re-add once logic is implemented with https://github.com/swisstopo/swissgeol-boreholes-suite/issues/2361
+  // getElementByDataCy("exportdata-button").should(isDisabled ? "have.attr" : "not.have.attr", "disabled");
+  // getElementByDataCy("exporttable-button").should(isDisabled ? "have.attr" : "not.have.attr", "disabled");
 }
 
 function assertCountDisplayed(textContent) {
