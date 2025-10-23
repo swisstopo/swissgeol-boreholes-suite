5fe39ee419f5da4b6dd7c58c4051e79e7a35f424
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2025-10-23 15:13:37 +0200

    Fix db schema

---
+++ src/api/Migrations/20251023130331_UpdateLogFileTable.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDMS.Migrations;
+#pragma warning disable CA1505
+
+/// <inheritdoc />
+public partial class UpdateLogFileTable : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<string>(
+            name: "name_uuid",
+            schema: "bdms",
+            table: "log_file",
+            type: "text",
+            nullable: false,
+            defaultValue: null,
+            oldClrType: typeof(string),
+            oldType: "text",
+            oldNullable: true);
+    }
+}
+#pragma warning restore CA1505
