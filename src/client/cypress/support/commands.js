723434c068da444197d429bfb5408a6ab40b4d68
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2025-10-27 14:55:40 +0100

    Add quotation marks to command
    Else data-cy elements with "." or " " won't work

--- src/client/cypress/support/commands.js
+++ src/client/cypress/support/commands.js
@@ -4,9 +4,9 @@
  * @param {string} key - The data-cy value to search for.
  * @returns {Cypress.Chainable<JQuery<HTMLElement>>}
  */
-Cypress.Commands.add("dataCy", { prevSubject: "optional" }, (subject, key) => {
-  if (subject) {
-    return cy.wrap(subject).find(`[data-cy=${key}]`);
-  }
-  return cy.get(`[data-cy=${key}]`);
+Cypress.Commands.add("dataCy", {prevSubject: "optional"}, (subject, key) => {
+    if (subject) {
+        return cy.wrap(subject).find(`[data-cy="${key}"]`);
+    }
+    return cy.get(`[data-cy="${key}"]`);
 });
