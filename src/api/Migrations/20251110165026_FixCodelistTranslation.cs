d7d0bf2ad7f5535b64eea724d8eaa280e2d6d880
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2025-11-10 17:52:27 +0100

    Fix codelist translation

---
+++ src/api/Migrations/20251110165026_FixCodelistTranslation.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDMS.Migrations;
+
+/// <inheritdoc />
+public partial class FixCodelistTranslation : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(@"UPDATE bdms.codelist SET text_cli_de = 'geschiefert' WHERE id_cli = 100000470;");
+
+    }
+}
