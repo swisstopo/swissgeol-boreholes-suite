ee012c9a057adb7288229b1f73c78f5d804f3487
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2026-03-05 09:59:03 +0100

    Remove IDKernlager , IDGeoDin codelists

---
+++ src/api/Migrations/20260305082759_RemoveDeprecatedBoreholeIdCodelists.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDMS.Migrations;
+
+/// <inheritdoc />
+public partial class RemoveDeprecatedBoreholeIdCodelists : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(@"DELETE FROM bdms.codelist where id_cli in (100000010, 100000011)");
+    }
+}
