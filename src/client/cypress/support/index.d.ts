0f4633c4734a6d3f294a53ce6624a280adea6681
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2026-04-08 11:45:23 +0200

    Move all cypress specific type extensions to index.d.ts

--- src/client/cypress/support/index.d.ts
+++ src/client/cypress/support/index.d.ts
@@ -14,6 +14,12 @@ declare global {
        * @example cy.dataCy('greeting')
        */
       dataCy(key: string, options?: Partial<TypeOptions>): Chainable<JQuery<HTMLElement>>;
+      get(alias: "@borehole_id"): Chainable<number>;
+      get(alias: "@stratigraphy_id"): Chainable<number>;
+      get(alias: "@completion_id"): Chainable<number>;
+      get(alias: "@casing1_id"): Chainable<number>;
+      get(alias: "@casing2_id"): Chainable<number>;
+      get(alias: "@id_token"): Chainable<string>;
     }
   }
 }
