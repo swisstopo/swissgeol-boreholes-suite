3e189614e28ca3ec8725bd86317039aa3f7d23a9
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2026-04-16 13:58:46 +0200

    Remove GeoTherm ID

--- src/api/Migrations/20260413141257_UpdateBoreholeIdentifierCodelistTexts.cs
+++ src/api/Migrations/20260413141257_UpdateBoreholeIdentifierCodelistTexts.cs
@@ -13,6 +13,8 @@ public partial class UpdateBoreholeIdentifierCodelistTexts : Migration
         migrationBuilder.Sql(@"
             DELETE FROM bdms.borehole_identifiers_codelist WHERE identifier_id = 100000009;
             DELETE FROM bdms.codelist WHERE id_cli = 100000009;
+            DELETE FROM bdms.borehole_identifiers_codelist WHERE identifier_id = 100000008;
+            DELETE FROM bdms.codelist WHERE id_cli = 100000008;
             UPDATE bdms.codelist SET text_cli_en = 'original ID',  text_cli_de = 'ID original',  text_cli_fr = 'ID original',    text_cli_it = 'ID originale'    WHERE id_cli = 100000004;
             UPDATE bdms.codelist SET text_cli_en = 'GeODin ID',    text_cli_de = 'ID GeODin',    text_cli_fr = 'ID GeODin',      text_cli_it = 'ID GeODin'       WHERE id_cli = 100000000;
             UPDATE bdms.codelist SET text_cli_en = 'InfoGeol ID',  text_cli_de = 'ID InfoGeol',  text_cli_fr = 'ID InfoGeol',    text_cli_it = 'ID InfoGeol'     WHERE id_cli = 100000003;
