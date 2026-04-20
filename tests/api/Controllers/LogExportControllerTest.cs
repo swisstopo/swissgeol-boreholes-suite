7fea73412d64290bd31f5f51e164f0df87a7a4d9
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2026-04-20 10:50:26 +0200

    Fix test

--- tests/api/Controllers/LogExportControllerTest.cs
+++ tests/api/Controllers/LogExportControllerTest.cs
@@ -133,12 +133,12 @@ public class LogExportControllerTest : TestControllerBase
 
         var fields = lines[1].Split(';');
         Assert.AreEqual("RUN-CSV", fields[0]);
-        Assert.AreEqual("10", fields[1]);
-        Assert.AreEqual("20", fields[2]);
+        Assert.AreEqual("10.00", fields[1]);
+        Assert.AreEqual("20.00", fields[2]);
         Assert.AreEqual("CAL,GYRO", fields[3]); // Tool types have to be alphabetically sorted by Codelist.Code
         Assert.AreEqual("CH", fields[4]);
-        Assert.AreEqual("01/06/2023", fields[5]); // RunDate dd/MM/yyyy (uses InvariantCulture "/" separator)
-        Assert.AreEqual("80.970", fields[6]); // BitSize "F3"
+        Assert.AreEqual("01/06/2023", fields[5]);
+        Assert.AreEqual("80.970", fields[6]);
         Assert.AreEqual("LWD", fields[7]);
         Assert.AreEqual("TestCo", fields[8]);
         Assert.AreEqual("Export test log run", fields[9]);
