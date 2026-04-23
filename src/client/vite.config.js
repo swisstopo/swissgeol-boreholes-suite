3ed2fb8225fe0238da8e27af1c921dd51a7acdbb
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2026-04-23 20:45:24 +0200

    Setup vitest

--- src/client/vite.config.js
+++ src/client/vite.config.js
@@ -1,8 +1,8 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 import svgr from "vite-plugin-svgr";
 import viteTsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   base: "/",
@@ -47,5 +47,10 @@ export default defineConfig({
       "Referrer-Policy": "strict-origin-when-cross-origin",
       "Strict-Transport-Security": "max-age=31536000; includeSubDomains",
     },
+  },
+  test: {
+    include: ["src/**/*.test.{ts,tsx}"],
+    environment: "node",
+    globals: false,
   },
 });
