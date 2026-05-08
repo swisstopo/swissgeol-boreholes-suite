d7fc466f6b4b5c570d5e90e4bef64ad19df2625d
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2026-05-08 15:21:15 +0200

    Rename workgroup columns

---
+++ src/api/Migrations/20260508132004_RenameWorkgroupColumns.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameWorkgroupColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "settings_wgp",
+                schema: "bdms",
+                table: "workgroups",
+                newName: "settings");
+
+            migrationBuilder.RenameColumn(
+                name: "name_wgp",
+                schema: "bdms",
+                table: "workgroups",
+                newName: "name");
+
+            migrationBuilder.RenameColumn(
+                name: "disabled_wgp",
+                schema: "bdms",
+                table: "workgroups",
+                newName: "disabled");
+
+            migrationBuilder.RenameColumn(
+                name: "created_wgp",
+                schema: "bdms",
+                table: "workgroups",
+                newName: "creation");
+
+            migrationBuilder.RenameColumn(
+                name: "id_wgp",
+                schema: "bdms",
+                table: "workgroups",
+                newName: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "settings",
+                schema: "bdms",
+                table: "workgroups",
+                newName: "settings_wgp");
+
+            migrationBuilder.RenameColumn(
+                name: "name",
+                schema: "bdms",
+                table: "workgroups",
+                newName: "name_wgp");
+
+            migrationBuilder.RenameColumn(
+                name: "disabled",
+                schema: "bdms",
+                table: "workgroups",
+                newName: "disabled_wgp");
+
+            migrationBuilder.RenameColumn(
+                name: "creation",
+                schema: "bdms",
+                table: "workgroups",
+                newName: "created_wgp");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                schema: "bdms",
+                table: "workgroups",
+                newName: "id_wgp");
+        }
+    }
+}
