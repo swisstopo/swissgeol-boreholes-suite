56e05aa5023efae736920b3e25b50c2ef2a5117f
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2026-05-12 11:42:09 +0200

    Clean up codelists

---
+++ src/api/Migrations/20260512092501_CleanupSrsAndLogCodelists.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDMS.Migrations;
+
+/// <inheritdoc />
+public partial class CleanupSrsAndLogCodelists : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        // Remove unused legacy GeoDIN SRS codelists
+        migrationBuilder.Sql(@"
+            DELETE FROM bdms.codelist
+            WHERE id_cli=20104003;
+
+            DELETE FROM bdms.codelist
+            WHERE id_cli=20104004;
+
+            DELETE FROM bdms.codelist
+            WHERE id_cli=20104005;
+        ");
+
+        // Add missing codes for LOG tool type.
+        migrationBuilder.Sql(@"
+            UPDATE bdms.codelist
+            SET code_cli='OTHER'
+            WHERE id_cli=100003055;
+
+            UPDATE bdms.codelist
+            SET code_cli='UNSPEC'
+            WHERE id_cli=100003056;
+        ");
+    }
+}
