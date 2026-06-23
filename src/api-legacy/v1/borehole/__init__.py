6b10ecad38f544abd3c3e5d186cf5540c6c0300b
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2026-06-23 13:16:10 +0200

    Remove legacy borehole API

--- src/api-legacy/v1/borehole/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Actions
-from bms.v1.borehole.delete import DeleteBorehole
-from bms.v1.borehole.deletelist import DeleteBoreholes
-from bms.v1.borehole.patch import PatchBorehole
-from bms.v1.borehole.multipatch import MultiPatchBorehole
