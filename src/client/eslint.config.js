df740f850011cc7f25a5536a0fa7d0763be7b840
MiraGeowerkstatt <mira.bleuler@geowerkstatt.ch>
2026-09-16 11:12:41 +0200

    Modernize eslint config

--- src/client/eslint.config.js
+++ src/client/eslint.config.js
@@ -1,50 +1,41 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import prettier from "eslint-plugin-prettier";
-import cypress from "eslint-plugin-cypress";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
+import cypress from "eslint-plugin-cypress/flat";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import tanstackQuery from "@tanstack/eslint-plugin-query";
 import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
 import noHardcodedColors from "./eslint-rules/no-hardcoded-colors.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
 
-export default defineConfig([globalIgnores(["**/dist", "tsconfig.json", "eslint.config.mjs", "**/cypress/downloads",  "**/dev","**/.vscode", "server.cjs"]),
+export default defineConfig([globalIgnores(["**/dist", "tsconfig.json", "eslint.config.js", "**/cypress/downloads",  "**/dev","**/.vscode", "server.cjs"]),
   {
-    extends: fixupConfigRules(compat.extends(
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:react/jsx-runtime",
-        "plugin:prettier/recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:cypress/recommended",
-        "plugin:jsx-a11y/recommended",
-        "plugin:@tanstack/query/recommended",
-    )),
+    extends: [
+        js.configs.recommended,
+        react.configs.flat.recommended,
+        react.configs.flat["jsx-runtime"],
+        prettierRecommended,
+        ...tseslint.configs.recommended,
+        cypress.configs.recommended,
+        jsxA11y.flatConfigs.recommended,
+        ...tanstackQuery.configs["flat/recommended"],
+    ],
     plugins: {
-        "@typescript-eslint": fixupPluginRules(typescriptEslint),
         "react-refresh": reactRefresh,
-        prettier: fixupPluginRules(prettier),
-        cypress: fixupPluginRules(cypress),
     },
     languageOptions: {
         globals: {
             ...globals.browser,
         },
-        parser: tsParser,
+        parser: tseslint.parser,
         ecmaVersion: "latest",
         sourceType: "module",
     },
@@ -77,7 +68,7 @@ export default defineConfig([globalIgnores(["**/dist", "tsconfig.json", "eslint.
     // Type-aware linting.
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
-      parser: tsParser,
+      parser: tseslint.parser,
       parserOptions: {
         projectService: true,
         tsconfigRootDir: __dirname,
